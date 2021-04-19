2f9dbf3b322c2c9fc9008fea3e97685941b73959
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-04-19 06:19:53 -0400

    converting fbm to HLSL

---
+++ generative/fbm.hlsl
@@ -0,0 +1,69 @@
+#include "snoise.hlsl"
+
+/*
+author: Patricio Gonzalez Vivo
+description: Fractal Brownian Motion
+use: fbm(<float2|float3> pos)
+options:
+  FBM_OCTAVES: numbers of octaves. Default is 4.
+  FBM_NOISE_FNC(POS_UV): noise function to use Default 'snoise(POS_UV)' (simplex noise)
+  FBM_VALUE_INITIAL: initial value. Default is 0.
+  FBM_SCALE_SCALAR: scalar. Defualt is 2.
+  FBM_AMPLITUD_INITIAL: initial amplitud value. Default is 0.5
+  FBM_AMPLITUD_SCALAR: amplitud scalar. Default is 0.5
+*/
+
+#ifndef FBM_OCTAVES
+#define FBM_OCTAVES 4
+#endif
+
+#ifndef FBM_NOISE_FNC
+#define FBM_NOISE_FNC(POS_UV) snoise(POS_UV)
+#endif
+
+#ifndef FBM_VALUE_INITIAL
+#define FBM_VALUE_INITIAL 0.0
+#endif
+
+#ifndef FBM_SCALE_SCALAR
+#define FBM_SCALE_SCALAR 2.0
+#endif
+
+#ifndef FBM_AMPLITUD_INITIAL
+#define FBM_AMPLITUD_INITIAL 0.5
+#endif
+
+#ifndef FBM_AMPLITUD_SCALAR
+#define FBM_AMPLITUD_SCALAR 0.5
+#endif
+
+#ifndef FNC_FBM
+#define FNC_FBM
+float fbm(in float2 st) {
+    // Initial values
+    float value = FBM_VALUE_INITIAL;
+    float amplitud = FBM_AMPLITUD_INITIAL;
+
+    // Loop of octaves
+    for (int i = 0; i < FBM_OCTAVES; i++) {
+        value += amplitud * FBM_NOISE_FNC(st);
+        st *= FBM_SCALE_SCALAR;
+        amplitud *= FBM_AMPLITUD_SCALAR;
+    }
+    return value;
+}
+
+float fbm(in float3 pos) {
+    // Initial values
+    float value = FBM_VALUE_INITIAL;
+    float amplitud = FBM_AMPLITUD_INITIAL;
+
+    // Loop of octaves
+    for (int i = 0; i < FBM_OCTAVES; i++) {
+        value += amplitud * FBM_NOISE_FNC(pos);
+        pos *= FBM_SCALE_SCALAR;
+        amplitud *= FBM_AMPLITUD_SCALAR;
+    }
+    return value;
+}
+#endif
