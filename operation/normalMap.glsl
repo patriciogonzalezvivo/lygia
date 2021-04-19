38a30ea8de392b92248b895550a672fb4b89f97a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-04-19 06:14:02 -0400

    adding more math conversions to hlsl

--- operation/normalMap.glsl
+++ operation/normalMap.glsl
@@ -23,21 +23,21 @@ license: |
 #ifndef FNC_NORMALMAP
 #define FNC_NORMALMAP
 vec3 normalMap(sampler2D tex, vec2 st, vec2 pixel) {
-  float center = NORMALMAP_SAMPLER_FNC(st);
+    float center = NORMALMAP_SAMPLER_FNC(st);
 
-  float topLeft    = NORMALMAP_SAMPLER_FNC(st - pixel);
+    float topLeft    = NORMALMAP_SAMPLER_FNC(st - pixel);
 
-  float left = NORMALMAP_SAMPLER_FNC(st - vec2(pixel.x, .0));
-  float bottomLeft = NORMALMAP_SAMPLER_FNC(st + vec2(-pixel.x, pixel.y));
-  float top    = NORMALMAP_SAMPLER_FNC(st - vec2(.0, pixel.y));
-  float bottom = NORMALMAP_SAMPLER_FNC(st + vec2(.0, pixel.y));
-  float topRight = NORMALMAP_SAMPLER_FNC(st + vec2(pixel.x, -pixel.y));
-  float right    = NORMALMAP_SAMPLER_FNC(st + vec2(pixel.x, .0));
-  float bottomRight = NORMALMAP_SAMPLER_FNC(st + pixel);
-  
-  float dX = topRight + 2. * right + bottomRight - topLeft - 2. * left - bottomLeft;
-  float dY = bottomLeft + 2. * bottom + bottomRight - topLeft - 2. * top - topRight;
+    float left = NORMALMAP_SAMPLER_FNC(st - vec2(pixel.x, .0));
+    float bottomLeft = NORMALMAP_SAMPLER_FNC(st + vec2(-pixel.x, pixel.y));
+    float top    = NORMALMAP_SAMPLER_FNC(st - vec2(.0, pixel.y));
+    float bottom = NORMALMAP_SAMPLER_FNC(st + vec2(.0, pixel.y));
+    float topRight = NORMALMAP_SAMPLER_FNC(st + vec2(pixel.x, -pixel.y));
+    float right    = NORMALMAP_SAMPLER_FNC(st + vec2(pixel.x, .0));
+    float bottomRight = NORMALMAP_SAMPLER_FNC(st + pixel);
+    
+    float dX = topRight + 2. * right + bottomRight - topLeft - 2. * left - bottomLeft;
+    float dY = bottomLeft + 2. * bottom + bottomRight - topLeft - 2. * top - topRight;
 
-  return normalize(vec3(dX, dY, NORMALMAP_Z) );
+    return normalize(vec3(dX, dY, NORMALMAP_Z) );
 }
 #endif
