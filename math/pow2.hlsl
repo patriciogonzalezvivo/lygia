2de03d4c697c63fd6a2fb3bca45da79cccca66a1
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-04-19 06:14:02 -0400

    adding more math conversions to hlsl

---
+++ math/pow2.hlsl
@@ -0,0 +1,29 @@
+/*
+author: Patricio Gonzalez Vivo
+description: power of 2
+use: pow2(<float|float2|float3|float4> x)
+license: |
+  Copyright (c) 2017 Patricio Gonzalez Vivo.
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.    
+*/
+
+#ifndef FNC_POW2
+#define FNC_POW2
+float pow2(in float x) {
+  return x * x;
+}
+
+float2 pow2(in float2 x) {
+  return x * x;
+}
+
+float3 pow2(in float3 x) {
+  return x * x;
+}
+
+float4 pow2(in float4 x) {
+  return x * x;
+}
+#endif
---
+++ math/rotate4dY.hlsl
@@ -0,0 +1,22 @@
+/*
+author: Patricio Gonzalez Vivo
+description: returns a 4x4 rotation matrix
+use: rotate4dY(<float> radians)
+license: |
+  Copyright (c) 2017 Patricio Gonzalez Vivo.
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.    
+
+*/
+
+#ifndef FNC_ROTATE4DY
+#define FNC_ROTATE4DY
+float4x4 rotate4dY(in float theta){
+    return float4x4(
+        float4(cos(theta),0.,-sin(theta),0),
+        float4(0.,1.,0.,0.),
+        float4(sin(theta),0.,cos(theta),0.),
+        float4(0.,0.,0.,1.));
+}
+#endif
