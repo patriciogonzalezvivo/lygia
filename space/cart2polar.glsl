540bde37ebec570ec0a581c468f69a10e6cfe57c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-04-30 05:21:05 -0400

    changing name for polar/cart conversion

--- space/cart2polar.glsl
+++ space/cart2polar.glsl
@@ -1,7 +1,7 @@
 /*
 author: Ivan Dianov
-description: Polar to Decart transformation.
-use: polarToDecart(<vec2> polar)
+description: cartesian to polar transformation.
+use: cart2polar(<vec2> st)
 license: |
   Copyright (c) 2021 Ivan Dianov.
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -9,13 +9,11 @@ license: |
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#ifndef FNC_POLARTODECART
-#define FNC_POLARTODECART
-vec2 polarToDecart(in vec2 polar) {
-    float alpha = polar.x;
-    float R = polar.y;
-    float x = sin(alpha) * R;
-    float y = cos(alpha) * R;
-    return vec2(x, y);
+#ifndef FNC_CART2POLAR
+#define FNC_CART2POLAR
+vec2 cart2polar(in vec2 st) {
+    float angles = atan(st.y, st.x);
+    float radius = length(st);
+    return vec2(angles, radius);
 }
 #endif
