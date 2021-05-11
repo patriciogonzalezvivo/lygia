785f58fe818e5745ee2818d06dc7945bba360ad9
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-05-11 05:39:36 -0400

    solving WEBGL 1.0 loop issues

--- filter/bokeh.glsl
+++ filter/bokeh.glsl
@@ -41,6 +41,41 @@ float getBlurSize(float depth, float focusPoint, float focusScale) {
     return abs(coc) * BOKEH_BLUR_SIZE;
 }
 
+#ifdef PLATFORM_WEBGL
+
+vec3 bokeh(sampler2D tex,sampler2D texDepth,vec2 texCoord,float focusPoint,float focusScale){
+    float pct=0.;
+    
+    float centerDepth = BOKEH_DEPTH_FNC(texCoord);
+    float centerSize = getBlurSize(centerDepth, focusPoint, focusScale);
+    vec2 pixelSize = 1.0/u_resolution.xy;
+    vec3 color = BOKEH_COLOR_FNC(texCoord).rgb;
+    
+    float total = 1.0;
+    float radius = BOKEH_RAD_SCALE;
+    for (float angle = 0.0 ; angle < 60.; angle += GOLDEN_ANGLE){
+        if (radius >= BOKEH_BLUR_SIZE)
+            break;
+
+        vec2 tc = texCoord + vec2(cos(angle), sin(angle)) * pixelSize * radius;
+        float sampleDepth = BOKEH_DEPTH_FNC(tc);
+        float sampleSize = getBlurSize(sampleDepth, focusPoint, focusScale);
+        if (sampleDepth > centerDepth)
+            sampleSize=clamp(sampleSize, 0.0, centerSize*2.0);
+        pct = smoothstep(radius-0.5, radius+0.5, sampleSize);
+        vec3 sampleColor = BOKEH_COLOR_FNC(tc).rgb;
+        #ifdef BOKEH_DEBUG
+        sampleColor = heatmap(pct*0.5+(angle/BOKEH_BLUR_SIZE)*0.1);
+        #endif
+        color += mix(color/total, sampleColor, pct);
+        total += 1.0;
+        radius += BOKEH_RAD_SCALE/radius;
+    }
+    return color/=total;
+}
+
+#else
+
 vec3 bokeh(sampler2D tex, sampler2D texDepth, vec2 texCoord, float focusPoint, float focusScale) {
     float pct = 0.0;
     float ang = 0.0;
@@ -69,5 +104,7 @@ vec3 bokeh(sampler2D tex, sampler2D texDepth, vec2 texCoord, float focusPoint, f
     }
     return color /= tot;
 }
+
+#endif
 
 #endif
