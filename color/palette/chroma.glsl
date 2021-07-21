2e2f5f143a9584af5bd64568637e796cb28c9579
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-07-21 10:53:48 -0400

    adding curl, chroma palette and unpack

---
+++ color/palette/chroma.glsl
@@ -0,0 +1,25 @@
+/*
+author: Patricio Gonzalez Vivo
+description: Simpler chroma spectrum 
+use: chroma(<float> value)
+license: |
+  Copyright (c) 2021 Patricio Gonzalez Vivo.
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.    
+*/
+
+#ifndef FNC_CHROMA
+#define FNC_CHROMA
+
+vec3 chroma(float x) {
+    float delta = 0.5;
+    vec3 color = vec3(1.0);
+    float freq = x * -3.1415;
+    color.r = sin(freq - delta);
+    color.g = sin(freq);
+    color.b = sin(freq + delta);
+    return pow(color, vec3(4.0));
+}
+
+#endif
---
+++ generative/curl.glsl
@@ -0,0 +1,56 @@
+#include "snoise.glsl"
+
+/*
+author: Isaac Cohen
+description: https://github.com/cabbibo/glsl-curl-noise/blob/master/curl.glsl
+use: curl(<vec3|vec4> pos)
+license: NONE
+*/
+
+
+#ifndef FNC_CURL
+#define FNC_CURL
+
+vec3 curl( vec3 p ){
+    const float e = .1;
+    vec3 dx = vec3( e   , 0.0 , 0.0 );
+    vec3 dy = vec3( 0.0 , e   , 0.0 );
+    vec3 dz = vec3( 0.0 , 0.0 , e   );
+
+    vec3 p_x0 = snoise3( p - dx );
+    vec3 p_x1 = snoise3( p + dx );
+    vec3 p_y0 = snoise3( p - dy );
+    vec3 p_y1 = snoise3( p + dy );
+    vec3 p_z0 = snoise3( p - dz );
+    vec3 p_z1 = snoise3( p + dz );
+
+    float x = p_y1.z - p_y0.z - p_z1.y + p_z0.y;
+    float y = p_z1.x - p_z0.x - p_x1.z + p_x0.z;
+    float z = p_x1.y - p_x0.y - p_y1.x + p_y0.x;
+
+    const float divisor = 1.0 / ( 2.0 * e );
+    return normalize( vec3( x , y , z ) * divisor );
+}
+
+vec3 curl( vec4 p ){
+    const float e = .1;
+    vec4 dx = vec4( e   , 0.0 , 0.0 , 1.0 );
+    vec4 dy = vec4( 0.0 , e   , 0.0 , 1.0 );
+    vec4 dz = vec4( 0.0 , 0.0 , e   , 1.0 );
+
+    vec3 p_x0 = snoise3( p - dx );
+    vec3 p_x1 = snoise3( p + dx );
+    vec3 p_y0 = snoise3( p - dy );
+    vec3 p_y1 = snoise3( p + dy );
+    vec3 p_z0 = snoise3( p - dz );
+    vec3 p_z1 = snoise3( p + dz );
+
+    float x = p_y1.z - p_y0.z - p_z1.y + p_z0.y;
+    float y = p_z1.x - p_z0.x - p_x1.z + p_x0.z;
+    float z = p_x1.y - p_x0.y - p_y1.x + p_y0.x;
+
+    const float divisor = 1.0 / ( 2.0 * e );
+    return normalize( vec3( x , y , z ) * divisor );
+}
+
+#endif
