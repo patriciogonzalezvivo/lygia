08c15274e00f8c532bef2b19b0127444aff0eabb
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-10-15 09:14:51 -0400

    add lineSDF

---
+++ sdf/lineSDF.hlsl
@@ -0,0 +1,19 @@
+/*\
+author: Inigo Quiles
+description: Segment SDF
+use: lineSDF(<float2> st, <float2> A, <float2> B)
+license: |
+    The MIT License
+    Copyright © 2020 Inigo Quilez
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef FNC_LINESDF
+#define FNC_LINESDF
+float lineSDF( in float2 st, in float2 a, in float2 b ) {
+    float2 b_to_a = b - a;
+    float2 to_a = st - a;
+    float h = saturate(dot(to_a, b_to_a)/dot(b_to_a, b_to_a));
+    return length(to_a - h * b_to_a );
+}
+#endif
