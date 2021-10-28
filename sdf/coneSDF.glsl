6c38c16dc6b5565db9aba105c3738eda2225839f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-10-28 12:36:03 -0400

    removing rounded cone

--- sdf/coneSDF.glsl
+++ sdf/coneSDF.glsl
@@ -57,28 +57,4 @@ float coneSDF( in vec3 p, in float r1, float r2, float h ) {
     return dot(q, vec2(a,b) ) - r1;
 }
 
-// Round
-float coneSDF(vec3 p, vec3 a, vec3 b, float r1, float r2) {
-    // sampling independent computations (only depend on shape)
-    vec3  ba = b - a;
-    float l2 = dot(ba,ba);
-    float rr = r1 - r2;
-    float a2 = l2 - rr*rr;
-    float il2 = 1.0/l2;
-    
-    // sampling dependant computations
-    vec3 pa = p - a;
-    float y = dot(pa,ba);
-    float z = y - l2;
-    float x2 = dot( pa*l2 - ba*y );
-    float y2 = y*y*l2;
-    float z2 = z*z*l2;
-
-    // single square root!
-    float k = sign(rr)*rr*rr*x2;
-    if( sign(z)*a2*z2 > k ) return  sqrt(x2 + z2)        *il2 - r2;
-    if( sign(y)*a2*y2 < k ) return  sqrt(x2 + y2)        *il2 - r1;
-                            return (sqrt(x2*a2*il2)+y*rr)*il2 - r1;
-}
-
 #endif
