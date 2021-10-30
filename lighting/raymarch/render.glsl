108515705ede747db0791bac1b6b04a033952d7a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-10-30 05:49:14 -0400

    tweaks here and there

--- lighting/raymarch/render.glsl
+++ lighting/raymarch/render.glsl
@@ -8,6 +8,12 @@
 author:  Inigo Quiles
 description: raymarching renderer
 use: <vec4> raymarchRender( in <vec3> ro, in <vec3> rd ) 
+options:
+    - RAYMARCH_MATERIAL_FNC(RGB) vec3(RGB)
+    - #define RAYMARCH_BACKGROUND vec3(0.0)
+    - RAYMARCH_AMBIENT vec3(1.0)
+    - LIGHT_COLOR     vec3(0.5)
+    - LIGHT_POSITION  vec3(0.0, 10.0, -50.0)
 license: |
     The MIT License
     Copyright © 2013 Inigo Quilez
@@ -41,68 +47,55 @@ license: |
 
 #ifndef RAYMARCH_BACKGROUND
 #define RAYMARCH_BACKGROUND vec3(0.0)
-// #define RAYMARCH_BACKGROUND ( vec3(0.7, 0.9, 1.0) +rd.y*0.8 )
+#endif
+
+#ifndef RAYMARCH_MATERIAL_FNC
+#define RAYMARCH_MATERIAL_FNC raymarchRender
 #endif
 
 #ifndef FNC_RAYMARCHRENDER
 #define FNC_RAYMARCHRENDER
+
+vec3 raymarchRender(vec3 rd, vec3 pos, vec3 nor, vec3 alb) {
+    if ( alb.r + alb.g + alb.b > 0.0 ) 
+        return alb;
+
+    vec3 color = alb;
+    vec3 ref = reflect( rd, nor );
+    float occ = raymarchAO( pos, nor );
+    vec3  lig = normalize( LIGHT_POSITION );
+    vec3  hal = normalize( lig-rd );
+    float amb = clamp( 0.5+0.5*nor.y, 0.0, 1.0 );
+    float dif = clamp( dot( nor, lig ), 0.0, 1.0 );
+    float bac = clamp( dot( nor, normalize(vec3(-lig.x,0.0,-lig.z))), 0.0, 1.0 )*clamp( 1.0-pos.y,0.0,1.0);
+    float dom = smoothstep( -0.1, 0.1, ref.y );
+    float fre = pow( clamp(1.0+dot(nor,rd),0.0,1.0), 2.0 );
+    
+    dif *= raymarchSoftShadow( pos, lig, 0.02, 2.5 );
+    dom *= raymarchSoftShadow( pos, ref, 0.02, 2.5 );
+
+    vec3 lin = vec3(0.0);
+    lin += 1.30 * dif * LIGHT_COLOR;
+    lin += 0.40 * amb * occ * RAYMARCH_AMBIENT;
+    lin += 0.50 * dom * occ * RAYMARCH_AMBIENT;
+    lin += 0.50 * bac * occ * 0.25;
+    lin += 0.25 * fre * occ;
+
+    return color * lin;
+}
 
 vec4 raymarchRender( in vec3 ro, in vec3 rd ) { 
     vec3 col = vec3(0.0);
     col = RAYMARCH_BACKGROUND;
 
     vec4 res = raymarchCast(ro, rd);
-    vec3 m = res.rgb;
     float t = res.a;
 
-    if ( m.r + m.g + m.b > 0.0 ) 
+    // if ( res.r + res.g + res.b > 0.0 ) 
     {
-        vec3 pos = ro + t*rd;
+        vec3 pos = ro + t * rd;
         vec3 nor = raymarchNormal( pos );
-        vec3 ref = reflect( rd, nor );
-        col = m;
-        
-        #if defined(RAYMARCH_FLOOR)
-        if ( m.y < 0.0) {
-            col = RAYMARCH_FLOOR;
-        }
-        #endif
-
-        // lighitng        
-        float occ = raymarchAO( pos, nor );
-        vec3  lig = normalize( LIGHT_POSITION );
-        vec3  hal = normalize( lig-rd );
-        float amb = clamp( 0.5+0.5*nor.y, 0.0, 1.0 );
-        float dif = clamp( dot( nor, lig ), 0.0, 1.0 );
-        float bac = clamp( dot( nor, normalize(vec3(-lig.x,0.0,-lig.z))), 0.0, 1.0 )*clamp( 1.0-pos.y,0.0,1.0);
-        float dom = smoothstep( -0.1, 0.1, ref.y );
-        float fre = pow( clamp(1.0+dot(nor,rd),0.0,1.0), 2.0 );
-        
-        dif *= raymarchSoftShadow( pos, lig, 0.02, 2.5 );
-        dom *= raymarchSoftShadow( pos, ref, 0.02, 2.5 );
-
-        vec3 lin = vec3(0.0);
-        lin += 1.30 * dif * LIGHT_COLOR;
-        lin += 0.40 * amb * occ * RAYMARCH_AMBIENT;
-        lin += 0.50 * dom * occ * RAYMARCH_AMBIENT;
-        lin += 0.50 * bac * occ * 0.25;
-        lin += 0.25 * fre * occ;
-        col = col*lin;
-
-        // float spe = pow( clamp( dot( nor, hal ), 0.0, 1.0 ),16.0)*
-        //             dif *
-        //             (0.04 + 0.96*pow( clamp(1.0+dot(hal,rd),0.0,1.0), 5.0 ));
-        // col += 10.00*spe*vec3(1.00,0.90,0.70);
-
-        // col = mix( col, vec3(0.8,0.9,1.0), 1.0-exp( -0.0002*t*t*t ) );
-        // col = vec3(1.) * amb;
-        // col = vec3(1.) * dif;
-        // col = vec3(1.) * bac;
-        // col = vec3(1.) * dom;
-        // col = vec3(1.) * fre;
-        // col = vec3(1.) * spe;
-        // col = ref;
-        // col = nor;
+        col = RAYMARCH_MATERIAL_FNC(rd, pos, nor, res.rgb);
     }
 
     return vec4( saturate(col), t );
