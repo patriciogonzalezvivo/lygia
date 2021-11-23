7fe3230c7a8436ebe24f614d67504240293660f7
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-11-23 16:06:20 -0500

    adding platonics

---
+++ sdf/icosahedronSDF.glsl
@@ -0,0 +1,41 @@
+#include "../math/const.glsl"
+
+/*
+description: generate the SDF of a icosahedron
+use: <float> icosahedronSDF( in <vec3> pos, in <float> size ) 
+license: |
+    The MIT License
+    Copyright © 2013 Inigo Quilez
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    https://www.youtube.com/c/InigoQuilez
+    https://iquilezles.org/
+
+    A list of useful distance function to simple primitives. All
+    these functions (except for ellipsoid) return an exact
+    euclidean distance, meaning they produce a better SDF than
+    what you'd get if you were constructing them from boolean
+    operations (such as cutting an infinite cylinder with two planes).
+
+    List of other 3D SDFs:
+       https://www.shadertoy.com/playlist/43cXRl
+    and
+       http://iquilezles.org/www/articles/distfunctions/distfunctions.htm
+*/
+
+#ifndef FNC_ICOSAHEDRONSDF
+#define FNC_ICOSAHEDRONSDF
+
+float icosahedronSDF(vec3 p, float radius) {
+    float q = 2.61803398875; // Golden Ratio + 1 = (sqrt(5)+3)/2;
+    vec3 n1 = normalize(vec3(q, 1,0));
+    vec3 n2 = vec3(0.57735026919);  // = sqrt(3)/3);
+
+    p = abs(p / radius);
+    float a = dot(p, n1.xyz);
+    float b = dot(p, n1.zxy);
+    float c = dot(p, n1.yzx);
+    float d = dot(p, n2) - n1.x;
+    return max(max(max(a,b),c)-n1.x,d) * radius;
+}
+
+#endif
