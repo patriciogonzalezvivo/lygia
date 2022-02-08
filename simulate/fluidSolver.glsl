3ae1d6508ad8db815bf551ab85f532869ef0b222
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-02-08 09:49:14 -0500

    improve fluid solver

--- simulate/fluidSolver.glsl
+++ simulate/fluidSolver.glsl
@@ -5,11 +5,12 @@ author: Patricio Gonzalez Vivo
 description: Simple single pass fluid simlation from the book GPU Pro 2, "Simple and Fast Fluids"
 use: <vec2> fluidSolver(<sampler2D> tex, <vec2> st, <vec2> pixel, <vec2> force)
 options:
-    - FLUIDSOLVER_DT
-    - FLUIDSOLVER_VORTICITY
-    - FLUIDSOLVER_VISCOSITY
+    - FLUIDSOLVER_DT: Default 0.15
+    - FLUIDSOLVER_DX: Defailt 1.0
+    - FLUIDSOLVER_VORTICITY: lower value for vorticity threshold means higher viscosity and vice versa (max .3)
+    - FLUIDSOLVER_VISCOSITY: Default: 0.16
+    - FLUIDSOLVER_VELOCITY_DECAY: Default: 5e-6
     - FLUIDSOLVER_SAMPLER_FNC
-    - FLUIDSOLVER_PACK_FNC
 license: |
   Copyright (c) 2022 Patricio Gonzalez Vivo.
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -21,72 +22,87 @@ license: |
 #define FLUIDSOLVER_DT 0.15
 #endif
 
-// lower value for vorticity threshold means higher viscosity
-// and vice versa (max .3). Setting it to 0. disables it.
-#ifndef FLUIDSOLVER_VORTICITY
-#define FLUIDSOLVER_VORTICITY 0.0015
+#ifndef FLUIDSOLVER_DX
+#define FLUIDSOLVER_DX 1.0
 #endif
-
 
 // higher this threshold, lower the viscosity (max .8)
 #ifndef FLUIDSOLVER_VISCOSITY
 #define FLUIDSOLVER_VISCOSITY .16
 #endif
 
-// #define FLUIDSOLVER_SAMPLER_FNC(UV) (texture2D(tex, UV)) 
-#ifndef FLUIDSOLVER_SAMPLER_FNC
-#define FLUIDSOLVER_SAMPLER_FNC(UV) (texture2D(tex, UV) * vec4(vec2(2.0), 3.0, 2.0) - vec4(vec2(1.0), 0.0, 1.0))
-#endif
+// #ifndef FLUIDSOLVER_VELOCITY_DECAY
+// #define FLUIDSOLVER_VELOCITY_DECAY 5e-6
+// #endif 
 
-#ifndef FLUIDSOLVER_PACK_FNC
-#define FLUIDSOLVER_PACK_FNC(COLOR) saturate(COLOR * vec4(vec2(0.5), 1./3., 0.5) + vec4(vec2(0.5), 0.0, 0.5))
+#ifndef FLUIDSOLVER_SAMPLER_FNC
+#define FLUIDSOLVER_SAMPLER_FNC(UV) texture2D(tex, UV)
 #endif
 
 #ifndef FNC_FLUIDSOLVER
 #define FNC_FLUIDSOLVER
 
-vec4 fluidSolver(sampler2D tex, vec2 st, vec2 pixel, vec2 force) {
-    float k = .2;
-    float s = k/FLUIDSOLVER_DT;
-    
-    vec4 fluidData = FLUIDSOLVER_SAMPLER_FNC(st);
-    vec4 fr = FLUIDSOLVER_SAMPLER_FNC(st + vec2(pixel.x, 0.));
-    vec4 fl = FLUIDSOLVER_SAMPLER_FNC(st - vec2(pixel.x, 0.));
-    vec4 ft = FLUIDSOLVER_SAMPLER_FNC(st + vec2(0., pixel.y));
-    vec4 fd = FLUIDSOLVER_SAMPLER_FNC(st - vec2(0., pixel.y));
+vec4 fluidSolver_sampler(sampler2D tex, vec2 st) {
+    vec4 data = FLUIDSOLVER_SAMPLER_FNC(st);
+    // XY velocity  -1.0 to 1.0
+    // Z density     0.0 to 1.0
+    // W vorticity   0.0 to 1.0
+    data.xyw = data.xyw * 2.0 - 1.0;
+    return data;
+}
 
-    vec3 ddx = (fr - fl).xyz * 0.5;
-    vec3 ddy = (ft - fd).xyz * 0.5;
-    float divergence = ddx.x + ddy.y;
-    vec2 densityDiff = vec2(ddx.z, ddy.z);
+vec4 fluidSolver(sampler2D tex, vec2 st, vec2 pixel, vec2 force) {
+    const float k = .2;
+    const float s = k/FLUIDSOLVER_DT;
+    const float dx = FLUIDSOLVER_DX;
     
-    // Solving for density
-    fluidData.z -= FLUIDSOLVER_DT * dot(vec3(densityDiff, divergence), fluidData.xyz);
+    // Data
+    vec4 d = fluidSolver_sampler(tex, st);
+    vec4 dR = fluidSolver_sampler(tex, st + vec2(pixel.x, 0.));
+    vec4 dL = fluidSolver_sampler(tex, st - vec2(pixel.x, 0.));
+    vec4 dT = fluidSolver_sampler(tex, st + vec2(0., pixel.y));
+    vec4 dB = fluidSolver_sampler(tex, st - vec2(0., pixel.y));
+
+    // Delta Data
+    vec4 ddx = (dR - dL).xyzw; // delta data on X
+    vec4 ddy = (dT - dB).xyzw; // delta data on Y
+    float divergence = (ddx.x + ddy.y) * 0.5;
+    divergence = (ddx.x + ddy.y) / (2.0 * dx * dx);
+
+    // Solving for density with one jacobi iteration 
+    float a = 1.0 / (dx * dx);
+    d.z = 1.0 / ( -4.0 * a ) * ( divergence - a * (dT.z + dR.z + dB.z + dL.z));
     
+    #ifdef FLUIDSOLVER_VISCOSITY
     // Solving for velocity
-    vec2 laplacian = fr.xy + fl.xy + ft.xy + fd.xy - 4.*fluidData.xy;
+    vec2 laplacian = dR.xy + dL.xy + dT.xy + dB.xy - 4.0 * d.xy;
     vec2 viscosityForce = FLUIDSOLVER_VISCOSITY * laplacian;
-    
     // Semi-lagrangian advection
-    vec2 densityInvariance = s * densityDiff;
-    vec2 was = st - FLUIDSOLVER_DT * fluidData.xy * pixel;
-    fluidData.xyw = FLUIDSOLVER_SAMPLER_FNC(was).xyw;
-    fluidData.xy += FLUIDSOLVER_DT*(viscosityForce - densityInvariance + clamp(force, -1., 1.) * 10.0);
+    vec2 densityInvariance = s * vec2(ddx.z, ddy.z);
+    vec2 was = st - FLUIDSOLVER_DT * d.xy * pixel;
+    d.xyw = fluidSolver_sampler(tex, was).xyw;
+    d.xy += FLUIDSOLVER_DT * (viscosityForce - densityInvariance + force);
+    #endif
     
-    // velocity decay
-    fluidData.xy = max(vec2(0.), abs(fluidData.xy) - 5e-6)*sign(fluidData.xy);
+    #ifdef FLUIDSOLVER_VELOCITY_DECAY
+    d.xy = max(vec2(0.), abs(d.xy) - FLUIDSOLVER_VELOCITY_DECAY)*sign(d.xy);
+    #endif
     
     // Vorticity confinement
-    fluidData.w = (fd.x - ft.x + fr.y - fl.y); // curl stored in the w channel
-    vec2 vorticity = vec2(abs(ft.w) - abs(fd.w), abs(fl.w) - abs(fr.w));
-    vorticity *= FLUIDSOLVER_VORTICITY/(length(vorticity) + 1e-5) * fluidData.w;
-    fluidData.xy += vorticity;
+    #ifdef FLUIDSOLVER_VORTICITY
+    d.w = (dB.x - dT.x + dR.y - dL.y); // curl stored in the w channel
+    vec2 vorticity = vec2(abs(dT.w) - abs(dB.w), abs(dL.w) - abs(dR.w));
+    vorticity *= FLUIDSOLVER_VORTICITY/(length(vorticity) + 1e-5) * d.w;
+    d.xy += vorticity;
+    #endif
 
     // Boundary conditions
-    fluidData.x *= smoothstep(0.5, 0.49,abs(st.x - 0.5));
-    fluidData.y *= smoothstep(0.5, 0.48,abs(st.y - 0.5));
-    
-    return FLUIDSOLVER_PACK_FNC(fluidData);
+    d.xy *= smoothstep(0.5, 0.49,abs(st - 0.5));
+
+    // Pack XY, Z and W data
+    d.xy = clamp(d.xy, -0.999, 0.999) * 0.5 + 0.5;
+    d.zw = clamp(d.zw, 0.0, 1.0);
+    return d;
 }
 
 #endif
