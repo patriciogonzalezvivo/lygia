669f98cb3a5e7909fb0656b0e782ee443c5b09db
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-09-22 14:57:35 -0400

    adding dither HLSL

---
+++ color/dither/interleavedGradientNoise.hlsl
@@ -0,0 +1,44 @@
+/*
+original_author: Patricio Gonzalez Vivo
+description: Jimenez 2014, "Next Generation Post-Processing in Call of Duty" http://advances.realtimerendering.com/s2014/index.html
+use: <float4|float3|float> interleavedGradientNoise(<float4|float3|float> value, <float> time)
+options:
+    - DITHER_INTERLEAVEDGRADIENTNOISE_ANIMATED
+*/
+
+#ifdef DITHER_ANIMATED
+#define DITHER_INTERLEAVEDGRADIENTNOISE_ANIMATED
+#endif
+
+#ifndef DITHER_INTERLEAVEDGRADIENTNOISE
+#define DITHER_INTERLEAVEDGRADIENTNOISE
+
+float interleavedGradientNoise(const in float2 n) {
+    return frac(52.982919 * frac(dot(float2(0.06711, 0.00584), n)));
+}
+
+float ditherInterleavedGradientNoise(float b, float2 fragcoord, const in float time) {
+    #ifdef DITHER_INTERLEAVEDGRADIENTNOISE_ANIMATED
+    fragcoord += 1337.0 * frac(time);
+    #endif
+    float noise = interleavedGradientNoise(fragcoord);
+    // remap from [0..1[ to [-1..1[
+    noise = (noise * 2.0) - 1.0;
+    return b + noise / 255.0;
+}
+
+float3 ditherInterleavedGradientNoise(float3 rgb, float2 fragcoord, const in float time) {
+    #ifdef DITHER_INTERLEAVEDGRADIENTNOISE_ANIMATED
+    fragcoord += 1337.0 * frac(time);
+    #endif
+    float noise = interleavedGradientNoise(fragcoord);
+    // remap from [0..1[ to [-1..1[
+    noise = (noise * 2.0) - 1.0;
+    return rgb.rgb + noise / 255.0;
+}
+
+float4 ditherInterleavedGradientNoise(float4 rgba, float2 fragcoord, const in float time) {
+    return float4(ditherInterleavedGradientNoise(rgba.rgb, fragcoord, time), rgba.a);
+}
+
+#endif
---
+++ color/dither/triangleNoise.hlsl
@@ -0,0 +1,73 @@
+/*
+original_author: Patricio Gonzalez Vivo
+description: Gjøl 2016, "Banding in Games: A Noisy Rant"
+use: 
+ - <float4|float3|float> ditherTriangleNoise(<float4|float3|float> value, <float2> fragcoord, <float> time)
+ - <float4|float3|float> ditherTriangleNoise(<float4|float3|float> value, <float> time)
+*/
+
+#ifdef DITHER_ANIMATED
+#define DITHER_TRIANGLENOISE_ANIMATED
+#endif
+
+#ifdef DITHER_CHROMATIC
+#define DITHER_TRIANGLENOISE_CHROMATIC
+#endif
+
+#ifndef RESOLUTION
+#define RESOLUTION _ScreenParams
+#endif
+
+#ifndef DITHER_TRIANGLENOISE
+#define DITHER_TRIANGLENOISE
+
+float triangleNoise(in float2 n, const in float time) {
+    // triangle noise, in [-1.0..1.0[ range
+    #ifdef DITHER_TRIANGLENOISE_ANIMATED
+    n += float2(0.07 * frac(time));
+    #endif
+    n  = frac(n * float2(5.3987, 5.4421));
+    n += dot(n.yx, n.xy + float2(21.5351, 14.3137));
+
+    float xy = n.x * n.y;
+    // compute in [0..2[ and remap to [-1.0..1.0[
+    return frac(xy * 95.4307) + frac(xy * 75.04961) - 1.0;
+}
+
+float ditherTriangleNoise(const in float b, in float2 fragcoord, const in float time) {
+    fragcoord /= RESOLUTION;
+    return b + triangleNoise(fragcoord, time) / 255.0;
+}
+
+float3 ditherTriangleNoise(const in float3 rgb, in float2 fragcoord, const in float time) {
+    fragcoord /= RESOLUTION;
+
+    #ifdef DITHER_TRIANGLENOISE_CHROMATIC 
+    float3 dither = float3(
+            triangleNoise(fragcoord, time),
+            triangleNoise(fragcoord + 0.1337, time),
+            triangleNoise(fragcoord + 0.3141, time));
+    #else
+    float n = triangleNoise(fragcoord, time);
+    float3 dither = float3(n, n, n);
+    #endif
+            
+    return rgb + dither / 255.0;
+}
+
+float4 ditherTriangleNoise(const in float4 rgba, in float2 fragcoord, const in float time) {
+    fragcoord /= RESOLUTION;
+
+    #ifdef DITHER_TRIANGLENOISE_CHROMATIC 
+    float3 dither = float3(
+            triangleNoise(fragcoord, time),
+            triangleNoise(fragcoord + 0.1337, time),
+            triangleNoise(fragcoord + 0.3141, time));
+    #else
+    float n = triangleNoise(fragcoord, time);
+    float3 dither = float3(n, n, n);
+    #endif
+    return (rgba + float4(dither, dither.x)) / 255.0;
+}
+
+#endif
