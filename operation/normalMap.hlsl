5ffa2c0308d198f89b376f9af01c38b78693d6a5
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-09-28 04:06:09 -0400

    adding sampling derivative

--- operation/normalMap.hlsl
+++ operation/normalMap.hlsl
@@ -1,9 +1,9 @@
-#include "../sample/derivative.glsl"
+#include "../sample/derivative.hlsl"
 
 /*
 original_author: Patricio Gonzalez Vivo
 description: Converts a RGB normal map into normal vectors
-use: normalMap(<sampler2D> texture, <vec2> st, <vec2> pixel)
+use: normalMap(<sampler2D> texture, <float2> st, <float2> pixel)
 options:
     - NORMALMAP_Z: Steepness of z before normalization, defaults to .01
 */
@@ -13,8 +13,8 @@ options:
 
 #ifndef FNC_NORMALMAP
 #define FNC_NORMALMAP
-vec3 normalMap(sampler2D tex, vec2 st, vec2 pixel) {
-    vec2 deltas = sampleDerivative(tex, st, pixel);
-    return normalize( vec3(deltas, NORMALMAP_Z) );
+float3 normalMap(sampler2D tex, float2 st, float2 pixel) {
+    float2 deltas = sampleDerivative(tex, st, pixel);
+    return normalize(float3(deltas.x, deltas.y, NORMALMAP_Z) );
 }
 #endif
---
+++ sample/derivative.glsl
@@ -0,0 +1,48 @@
+/*
+original_author: Patricio Gonzalez Vivo
+description: sample derrivatives
+use: sampleDerivative(<sampler2D> tex, <vec2> st)
+options:
+    - SAMPLER_FNC(TEX, UV): optional depending the target version of GLSL (tex2D(...) or texture(...))
+    - USE_DERIVATIVES
+*/
+
+#ifndef SAMPLER_FNC
+#define SAMPLER_FNC(TEX, UV) texture2D(TEX, UV)
+#endif
+
+#ifndef SAMPLERDERIVATIVE_FNC
+#define SAMPLERDERIVATIVE_FNC(UV) SAMPLER_FNC(tex,UV).r
+#endif
+
+#ifndef FNC_SAMPLEDERIVATIVE
+#define FNC_SAMPLEDERIVATIVE
+vec2 sampleDerivative(in sampler2D tex, in vec2 st, vec2 pixel) { 
+    float p = SAMPLERDERIVATIVE_FNC(st); 
+
+    #if defined(SAMPLEDERRIVATIVE_DD)
+    return -vec2(dFdx(p), dFdy(p));
+
+    #elif defined(SAMPLEDERRIVATIVE_FAST)
+    float h1 = SAMPLERDERIVATIVE_FNC(st + vec2(pixel.x,0.0));
+    float v1 = SAMPLERDERIVATIVE_FNC(st + vec2(0.0,pixel.y));
+    return (p - vec2(h1, v1));
+
+    #else
+    float center      = SAMPLERDERIVATIVE_FNC(st);
+    float topLeft     = SAMPLERDERIVATIVE_FNC(st - pixel);
+    float left        = SAMPLERDERIVATIVE_FNC(st - vec2(pixel.x, .0));
+    float bottomLeft  = SAMPLERDERIVATIVE_FNC(st + vec2(-pixel.x, pixel.y));
+    float top         = SAMPLERDERIVATIVE_FNC(st - vec2(.0, pixel.y));
+    float bottom      = SAMPLERDERIVATIVE_FNC(st + vec2(.0, pixel.y));
+    float topRight    = SAMPLERDERIVATIVE_FNC(st + vec2(pixel.x, -pixel.y));
+    float right       = SAMPLERDERIVATIVE_FNC(st + vec2(pixel.x, .0));
+    float bottomRight = SAMPLERDERIVATIVE_FNC(st + pixel);
+    
+    float dX = topRight + 2. * right + bottomRight - topLeft - 2. * left - bottomLeft;
+    float dY = bottomLeft + 2. * bottom + bottomRight - topLeft - 2. * top - topRight;
+
+    return vec2(dX, dY);
+    #endif
+}
+#endif
