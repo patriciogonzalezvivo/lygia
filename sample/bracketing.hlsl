758ccfd53f053400e0e4fe3dfc73ae104ada9cb1
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-09-28 04:06:09 -0400

    adding sampling derivative

---
+++ sample/bracketing.hlsl
@@ -0,0 +1,95 @@
+#include "../space/rotate.hlsl"
+#include "../space/mod.hlsl"
+
+/*
+original_author: Huw Bowles ( @hdb1 )
+description: 'Bracketing' technique maps a texture to a plane using any arbitrary 2D vector field to give orientatio. From https://www.shadertoy.com/view/NddcDr
+use: sampleBracketing(<sampler2D> texture, <float2> st, <float2> direction [, <float> scale] )
+options:
+    - SAMPLER_FNC(TEX, UV): optional depending the target version of GLSL (texture2D(...) or texture(...))
+    - SAMPLEBRACKETING_TYPE:
+    - SAMPLEBRACKETING_SAMPLER_FNC(UV):
+    - SAMPLEBRACKETING_ANGLE_DELTA:
+    - SAMPLEBRACKETING_REPLACE_DIVERGENCE
+license: Copyright Huw Bowles May 2022 on MIT license
+*/
+
+#ifndef SAMPLER_FNC
+#define SAMPLER_FNC(TEX, UV) tex2D(TEX, UV)
+#endif
+
+// Parameter for bracketing - bracket size in radians. Large values create noticeable linear structure,
+// small values prone to simply replicating the issues with the brute force approach. In my use cases it
+// was quick and easy to find a sweet spot.
+#ifndef SAMPLEBRACKETING_ANGLE_DELTA
+#define SAMPLEBRACKETING_ANGLE_DELTA PI / 20.0
+#endif
+
+#ifndef SAMPLEBRACKETING_TYPE
+#define SAMPLEBRACKETING_TYPE vec4
+#endif
+
+#ifndef SAMPLEBRACKETING_SAMPLER_FNC
+#define SAMPLEBRACKETING_SAMPLER_FNC(UV) SAMPLER_FNC(tex, UV)
+#endif
+
+#ifndef FNC_SAMPLEBRACKETING
+#define FNC_SAMPLEBRACKETING
+
+// Vector field direction is used to drive UV coordinate frame, but instead
+// of directly taking the vector directly, take two samples of the texture
+// using coordinate frames at snapped angles, and then blend them based on
+// the angle of the original vector.
+void sampleBracketing(float2 dir, out float2 vAxis0, out float2 vAxis1, out float blendAlpha) {
+    // Heading angle of the original vector field direction
+    float angle = atan(dir.y, dir.x) + 2.0*PI;
+
+    float AngleDelta = SAMPLEBRACKETING_ANGLE_DELTA;
+
+    // Snap to a first canonical direction by subtracting fractional angle
+    float fractional = mod(angle, AngleDelta);
+    float angle0 = angle - fractional;
+    
+    // Compute one V axis of UV frame. Given angle0 is snapped, this could come from LUT, but would
+    // need testing on target platform to verify that a LUT is faster.
+    vAxis0 = float2(cos(angle0), sin(angle0));
+
+    // Compute the next V axis by rotating by the snap angle size
+
+    float2x2 RotateByAngleDelta = float2x2(cos(AngleDelta), sin(AngleDelta), -sin(AngleDelta), cos(AngleDelta));
+    vAxis1 = RotateByAngleDelta * vAxis0;
+
+    // Blend to get final result, based on how close the vector was to the first snapped angle
+    blendAlpha = fractional / AngleDelta;
+}
+
+SAMPLEBRACKETING_TYPE sampleBracketing(sampler2D tex, float2 st, float2 dir, float scale) {
+    float2 vAxis0 = float2(0.0);
+    float2 vAxis1 = float2(0.0);
+    float blendAlpha = 0.0;
+
+    sampleBracketing(dir, vAxis0, vAxis1, blendAlpha);
+    
+    // Compute the function for the two canonical directions
+    float2 uv0 = scale * rotate(st, vAxis0);
+    float2 uv1 = scale * rotate(st, vAxis1);
+    
+    // Now sample function/texture
+    SAMPLEBRACKETING_TYPE sample0 = SAMPLEBRACKETING_SAMPLER_FNC(uv0);
+    SAMPLEBRACKETING_TYPE sample1 = SAMPLEBRACKETING_SAMPLER_FNC(uv1);
+
+    // Blend to get final result, based on how close the vector was to the first snapped angle
+    SAMPLEBRACKETING_TYPE result = lerp( sample0, sample1, blendAlpha);
+
+#ifdef SAMPLEBRACKETING_REPLACE_DIVERGENCE
+    float strength = smoothstep(0.0, 0.2, dot(dir, dir)*6.0);
+    result = lerp(   SAMPLEBRACKETING_SAMPLER_FNC(scale * rotate(st, -float2(cos(0.5),sin(0.5)))), 
+                    result, 
+                    strength);
+#endif
+    
+    return result;
+}
+
+SAMPLEBRACKETING_TYPE sampleBracketing(sampler2D tex, float2 st, float2 dir) { return sampleBracketing(tex, st, dir, 1.0); }
+#endif
---
+++ sample/derivative.hlsl
@@ -0,0 +1,48 @@
+/*
+original_author: Patricio Gonzalez Vivo
+description: sample derrivatives
+use: sampleDerivative(<sampler2D> tex, <float2> st)
+options:
+    - SAMPLER_FNC(TEX, UV): optional depending the target version of GLSL (tex2D(...) or texture(...))
+    - USE_DERIVATIVES
+*/
+
+#ifndef SAMPLER_FNC
+#define SAMPLER_FNC(TEX, UV) tex2D(TEX, UV)
+#endif
+
+#ifndef SAMPLERDERIVATIVE_FNC
+#define SAMPLERDERIVATIVE_FNC(UV) SAMPLER_FNC(tex,UV).r
+#endif
+
+#ifndef FNC_SAMPLEDERIVATIVE
+#define FNC_SAMPLEDERIVATIVE
+float2 sampleDerivative(in sampler2D tex, in float2 st, float2 pixel) { 
+    float p = SAMPLERDERIVATIVE_FNC(st); 
+
+    #if defined(SAMPLEDERRIVATIVE_DD)
+    return -float2(ddx(p), ddy(p));
+
+    #elif defined(SAMPLEDERRIVATIVE_FAST)
+    float h1 = SAMPLERDERIVATIVE_FNC(st + float2(pixel.x,0.0));
+    float v1 = SAMPLERDERIVATIVE_FNC(st + float2(0.0,pixel.y));
+    return (p - float2(h1, v1));
+
+    #else
+    float center      = SAMPLERDERIVATIVE_FNC(st);
+    float topLeft     = SAMPLERDERIVATIVE_FNC(st - pixel);
+    float left        = SAMPLERDERIVATIVE_FNC(st - float2(pixel.x, .0));
+    float bottomLeft  = SAMPLERDERIVATIVE_FNC(st + float2(-pixel.x, pixel.y));
+    float top         = SAMPLERDERIVATIVE_FNC(st - float2(.0, pixel.y));
+    float bottom      = SAMPLERDERIVATIVE_FNC(st + float2(.0, pixel.y));
+    float topRight    = SAMPLERDERIVATIVE_FNC(st + float2(pixel.x, -pixel.y));
+    float right       = SAMPLERDERIVATIVE_FNC(st + float2(pixel.x, .0));
+    float bottomRight = SAMPLERDERIVATIVE_FNC(st + pixel);
+    
+    float dX = topRight + 2. * right + bottomRight - topLeft - 2. * left - bottomLeft;
+    float dY = bottomLeft + 2. * bottom + bottomRight - topLeft - 2. * top - topRight;
+
+    return float2(dX, dY);
+    #endif
+}
+#endif
