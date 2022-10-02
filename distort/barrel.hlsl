4798ef727ae4681a0d0b41f40509e9f67742355a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-10-02 13:59:23 -0400

    distort

---
+++ distort/barrel.hlsl
@@ -0,0 +1,73 @@
+#include "../math/lengthSq.hlsl"
+
+/*
+original_author: Patricio Gonzalez Vivo
+description: Barrel distortion
+use: barrel(sampler2D tex, <float2> st, [, <float2|float> sdf])
+options:
+    - SAMPLER_FNC(TEX, UV): optional depending the target version of GLSL (texture2D(...) or texture(...))
+    - BARREL_DISTANCE: function used to shape the distortion, defaults to radial shape with lengthSq
+    - BARREL_TYPE: return type, defaults to vec3
+    - BARREL_SAMPLER_FNC: function used to sample the input texture, defaults to texture2D(tex, POS_UV).rgb
+    - BARREL_OCT_1: one octave of distortion
+    - BARREL_OCT_2: two octaves of distortion
+    - BARREL_OCT_3: three octaves of distortion
+*/
+
+#ifndef SAMPLER_FNC
+#define SAMPLER_FNC(TEX, UV) tex2D(TEX, UV)
+#endif
+
+#ifndef BARREL_DISTANCE
+#define BARREL_DISTANCE dist
+#endif
+
+#ifndef BARREL_TYPE
+#define BARREL_TYPE float3
+#endif
+
+#ifndef BARREL_SAMPLER_FNC
+#define BARREL_SAMPLER_FNC(POS_UV) SAMPLER_FNC(tex, POS_UV).rgb
+#endif
+
+#ifndef FNC_BARREL
+#define FNC_BARREL
+float2 barrel(float2 st, float amt, float dist) {
+    return st + (st-.5) * (BARREL_DISTANCE) * amt;
+}
+
+float2 barrel(float2 st, float amt) {
+    return barrel(st, amt, lengthSq(st-.5));
+}
+
+BARREL_TYPE barrel(in sampler2D tex, in float2 st, float offset) {
+    BARREL_TYPE a1 = BARREL_SAMPLER_FNC( barrel(st, .0, offset));
+    BARREL_TYPE a2 = BARREL_SAMPLER_FNC( barrel(st, .2, offset));
+    BARREL_TYPE a3 = BARREL_SAMPLER_FNC( barrel(st, .4, offset));
+    BARREL_TYPE a4 = BARREL_SAMPLER_FNC( barrel(st, .6, offset));
+#ifdef BARREL_OCT_1
+    return (a1+a2+a3+a4)/4.;
+#endif
+    BARREL_TYPE a5 = BARREL_SAMPLER_FNC( barrel(st, .8, offset) );
+    BARREL_TYPE a6 = BARREL_SAMPLER_FNC( barrel(st, 1.0, offset) );
+    BARREL_TYPE a7 = BARREL_SAMPLER_FNC( barrel(st, 1.2, offset) );
+    BARREL_TYPE a8 = BARREL_SAMPLER_FNC( barrel(st, 1.4, offset) );
+#ifdef BARREL_OCT_2
+    return (a1+a2+a3+a4+a5+a6+a7+a8)/8.;
+#endif
+    BARREL_TYPE a9 = BARREL_SAMPLER_FNC( barrel(st, 1.6, offset));
+    BARREL_TYPE a10 = BARREL_SAMPLER_FNC( barrel(st, 1.8, offset));
+    BARREL_TYPE a11 = BARREL_SAMPLER_FNC( barrel(st, 2.0, offset));
+    BARREL_TYPE a12 = BARREL_SAMPLER_FNC( barrel(st, 2.2, offset));
+    return (a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12)/12.;
+}
+
+BARREL_TYPE barrel(in sampler2D tex, in float2 st, in float2 offset) {
+    return barrel(tex, st, dot(float2(.5, .5), offset));
+}
+
+BARREL_TYPE barrel(in sampler2D tex, in float2 st) {
+    return barrel(tex, st, lengthSq(st-.5));
+}
+
+#endif
--- lighting/iridescence.hlsl
+++ lighting/iridescence.hlsl
@@ -1,24 +1,24 @@
-#include "../color/palette/hue.glsl"
+#include "../color/palette/hue.hlsl"
 
 /*
 original_author: Paniq (https://www.shadertoy.com/view/Ms33zj)
 description: based on the picture in http://home.hiroshima-u.ac.jp/kin/publications/TVC01/examples.pdf
-use: <vec3> iridescence(<float> angle, <float> thickness)
+use: <float3> iridescence(<float> angle, <float> thickness)
 */
 
 #ifndef FNC_IRIDESCENCE
 #define FNC_IRIDESCENCE
-vec3 iridescence(float cosA, float thickness) {
+float3 iridescence(float cosAngle, float thickness) {
     // typically the dot product of normal with eye ray
-    float NxV = cosA;
+    float NxV = cosAngle;
     
     // energy of spectral colors
     float lum = 0.05064;
     // basic energy of light
     float luma = 0.01070;
     // tint of the final color
-    //vec3 tint = vec3(0.7333,0.89804,0.94902);
-    vec3 tint = vec3(0.49639,0.78252,0.88723);
+    //float3 tint = float3(0.7333,0.89804,0.94902);
+    float3 tint = float3(0.49639,0.78252,0.88723);
     // interference rate at minimum angle
     float interf0 = 2.4;
     // phase shift rate at minimum angle
@@ -29,21 +29,18 @@ vec3 iridescence(float cosA, float thickness) {
     float phase1 = phase0;
     // fresnel (most likely completely wrong)
     float f = (1.0 - NxV) * (1.0 - NxV);
-    float interf = mix(interf0, interf1, f);
-    float phase = mix(phase0, phase1, f);
+    float interf = lerp(interf0, interf1, f);
+    float phase = lerp(phase0, phase1, f);
     float dp = (NxV - 1.0) * 0.5;
     
     // film hue
-    vec3 hue = 
-    		// fade in higher frequency at the right end
-        	mix(
-                hue(thickness * interf0 + dp, thickness * phase0),
-    			hue(thickness * interf1 + 0.1 + dp, thickness * phase1),
-                f);
+    float3 h = lerp(hue(thickness * interf0 + dp, thickness * phase0),
+    			    hue(thickness * interf1 + 0.1 + dp, thickness * phase1),
+                    f);
     
-    vec3 film = hue * lum + vec3(0.49639,0.78252,0.88723) * luma;
+    float3 film = h * lum + float3(0.49639,0.78252,0.88723) * luma;
     
-    return vec3((film * 3.0 + pow(f, 12.0))) * tint;
+    return float3((film * 3.0 + pow(f, 12.0))) * tint;
 }
 
 #endif
