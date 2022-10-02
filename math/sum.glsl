293bd65f4a6d643b23a2579b4a2e6989cea33534
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-10-02 15:45:03 -0400

    fast seamless

---
+++ math/sum.glsl
@@ -0,0 +1,14 @@
+/*
+original_author: Patricio Gonzalez Vivo
+description: Sum elements of a vector
+use: <float> sum(<vec2|vec3|vec4> value)
+*/
+
+#ifndef FNC_SUM
+#define FNC_SUM
+
+float sum( vec2 v ) { return v.x+v.y; }
+float sum( vec3 v ) { return v.x+v.y+v.z; }
+float sum( vec4 v ) { return v.x+v.y+v.z+v.w; }
+
+#endif
