b22570671c9442e23deeff08f2d806ffa6a702ef
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-10-04 08:06:02 -0400

    untile

--- sample/untile.glsl
+++ sample/untile.glsl
@@ -5,36 +5,37 @@
 /*
 original_author: Inigo Quiles
 description: Avoiding texture repetition by using Voronoise: a small texture can be used to generate infinite variety instead of tiled repetition. More info:  https://iquilezles.org/articles/texturerepetition/
-use: sampleSeamless(<sampler2D> texture, <vec2> st, <float> noTiling)
+use: sampleUNTILE(<sampler2D> texture, <vec2> st, <float> noTiling)
 options:
     - SAMPLER_FNC(TEX, UV)
-    - SAMPLESEAMLESS_TYPE
-    - SAMPLESEAMLESS_SAMPLER_FNC(UV)
+    - SAMPLEUNTILE_TYPE
+    - SAMPLEUNTILE_SAMPLER_FNC(UV)
 */
+
 #ifndef SAMPLER_FNC
 #define SAMPLER_FNC(TEX, UV) texture2D(TEX, UV)
 #endif
 
-#ifndef SAMPLESEAMLESS_TYPE
-#define SAMPLESEAMLESS_TYPE vec4
+#ifndef SAMPLEUNTILE_TYPE
+#define SAMPLEUNTILE_TYPE vec4
 #endif
 
-#ifndef SAMPLESEAMLESS_SAMPLER_FNC
+#ifndef SAMPLEUNTILE_SAMPLER_FNC
 #ifdef PLATFORM_WEBGL
-#define SAMPLESEAMLESS_SAMPLER_FNC(UV) textureGrad(tex, UV, ddx, ddy)
+#define SAMPLEUNTILE_SAMPLER_FNC(UV) textureGrad(tex, UV, ddx, ddy)
 #else
-#define SAMPLESEAMLESS_SAMPLER_FNC(UV) SAMPLER_FNC(tex, UV)
+#define SAMPLEUNTILE_SAMPLER_FNC(UV) SAMPLER_FNC(tex, UV)
 #endif
 #endif
 
-#ifndef SAMPLESEAMLESS_RANDOM_FNC 
-#define SAMPLESEAMLESS_RANDOM_FNC(XYZ) random4(XYZ) 
+#ifndef SAMPLEUNTILE_RANDOM_FNC 
+#define SAMPLEUNTILE_RANDOM_FNC(XYZ) random4(XYZ) 
 #endif
 
-#ifndef FNC_SAMPLESEAMLESS
-#define FNC_SAMPLESEAMLESS
+#ifndef FNC_SAMPLEUNTILE
+#define FNC_SAMPLEUNTILE
 
-SAMPLESEAMLESS_TYPE sampleSeamless(sampler2D tex, in vec2 st, float v) {
+SAMPLEUNTILE_TYPE sampleUntile(sampler2D tex, in vec2 st, float v) {
         
     #ifdef PLATFORM_WEBGL
     // derivatives (for correct mipmapping)
@@ -43,7 +44,7 @@ SAMPLESEAMLESS_TYPE sampleSeamless(sampler2D tex, in vec2 st, float v) {
     #endif
 
     #ifdef SAMPLESSEAMLESS_FAST
-    float k = SAMPLESEAMLESS_SAMPLER_FNC(0.005*st ).x; // cheap (cache friendly) lookup
+    float k = SAMPLEUNTILE_SAMPLER_FNC(0.005*st ).x; // cheap (cache friendly) lookup
     
     float l = k*8.0;
     float f = fract(l);
@@ -60,8 +61,8 @@ SAMPLESEAMLESS_TYPE sampleSeamless(sampler2D tex, in vec2 st, float v) {
     vec2 offa = sin(vec2(3.0,7.0)*ia); // can replace with any other hash
     vec2 offb = sin(vec2(3.0,7.0)*ib); // can replace with any other hash
 
-    SAMPLESEAMLESS_TYPE cola = SAMPLESEAMLESS_SAMPLER_FNC( st + v * offa );
-    SAMPLESEAMLESS_TYPE colb = SAMPLESEAMLESS_SAMPLER_FNC( st + v * offb );
+    SAMPLEUNTILE_TYPE cola = SAMPLEUNTILE_SAMPLER_FNC( st + v * offa );
+    SAMPLEUNTILE_TYPE colb = SAMPLEUNTILE_SAMPLER_FNC( st + v * offb );
     return mix( cola, colb, smoothstep(0.2, 0.8, f - 0.1 * sum(cola-colb) ) );
 
     #else 
@@ -71,17 +72,17 @@ SAMPLESEAMLESS_TYPE sampleSeamless(sampler2D tex, in vec2 st, float v) {
     vec2 p = floor( st );
     vec2 f = fract( st );
     
-    SAMPLESEAMLESS_TYPE va = SAMPLESEAMLESS_TYPE(0.0);
+    SAMPLEUNTILE_TYPE va = SAMPLEUNTILE_TYPE(0.0);
     float w1 = 0.0;
     float w2 = 0.0;
     vec2 g = vec2(-1.0, -1.0);
     for( g.y = -1.0; g.y <= 1.0; g.y++ )
     for( g.x = -1.0; g.x <= 1.0; g.x++ ) {
-        vec4 o = SAMPLESEAMLESS_RANDOM_FNC( p + g );
+        vec4 o = SAMPLEUNTILE_RANDOM_FNC( p + g );
         vec2 r = g - f + o.xy;
         float d = dot(r,r);
         float w = exp(-5.0*d );
-        SAMPLESEAMLESS_TYPE c = SAMPLESEAMLESS_SAMPLER_FNC(st + v*o.zw); 
+        SAMPLEUNTILE_TYPE c = SAMPLEUNTILE_SAMPLER_FNC(st + v*o.zw); 
         va += w*c;
         w1 += w;
         w2 += w*w;
@@ -92,7 +93,7 @@ SAMPLESEAMLESS_TYPE sampleSeamless(sampler2D tex, in vec2 st, float v) {
 
     // contrast preserving average
     float mean = 0.3;// textureGrad( samp, uv, ddx*16.0, ddy*16.0 ).x;
-    SAMPLESEAMLESS_TYPE res = mean + (va-w1*mean)/sqrt(w2);
+    SAMPLEUNTILE_TYPE res = mean + (va-w1*mean)/sqrt(w2);
     return mix( va/w1, res, v );
     #endif
 }
