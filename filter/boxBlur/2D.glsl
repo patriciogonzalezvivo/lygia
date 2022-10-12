8a84a59008e57f98b1106b140080f039cba9d987
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-10-12 05:07:05 -0400

    gaussian webgl

--- filter/boxBlur/2D.glsl
+++ filter/boxBlur/2D.glsl
@@ -35,21 +35,21 @@ BOXBLUR2D_TYPE boxBlur2D(in sampler2D tex, in vec2 st, in vec2 pixel, const int 
     BOXBLUR2D_TYPE color = BOXBLUR2D_TYPE(0.);
 
     #ifndef BOXBLUR2D_KERNELSIZE
-    
+
     #if defined(PLATFORM_WEBGL)
     #define BOXBLUR2D_KERNELSIZE 20
-    float f_kernelSize = float(kernelSize);
+    float kernelSizef = float(kernelSize);
     #else
     #define BOXBLUR2D_KERNELSIZE kernelSize
-    float f_kernelSize = float(BOXBLUR2D_KERNELSIZE);
+    float kernelSizef = float(BOXBLUR2D_KERNELSIZE);
     #endif
 
     #else
-    float f_kernelSize = float(BOXBLUR2D_KERNELSIZE);
+    float kernelSizef = float(BOXBLUR2D_KERNELSIZE);
     #endif
 
     float accumWeight = 0.;
-    float kernelSize2 = f_kernelSize * f_kernelSize;
+    float kernelSize2 = kernelSizef * kernelSizef;
     float weight = 1. / kernelSize2;
 
     for (int j = 0; j < BOXBLUR2D_KERNELSIZE; j++) {
@@ -57,13 +57,13 @@ BOXBLUR2D_TYPE boxBlur2D(in sampler2D tex, in vec2 st, in vec2 pixel, const int 
         if (j >= kernelSize)
             break;
         #endif
-        float y = -.5 * (f_kernelSize - 1.) + float(j);
+        float y = -.5 * (kernelSizef - 1.) + float(j);
         for (int i = 0; i < BOXBLUR2D_KERNELSIZE; i++) {
             #if defined(PLATFORM_WEBGL)
             if (i >= kernelSize)
                 break;
             #endif
-            float x = -.5 * (f_kernelSize - 1.) + float(i);
+            float x = -.5 * (kernelSizef - 1.) + float(i);
             color += BOXBLUR2D_SAMPLER_FNC(st + vec2(x, y) * pixel) * weight;
         }
     }
