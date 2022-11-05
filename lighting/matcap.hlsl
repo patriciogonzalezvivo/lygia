e7896973d4bbe0b5c118d1c66419597a374e4a73
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-11-05 05:07:27 -0400

    adding debugcube and spheremapping

--- lighting/matcap.hlsl
+++ lighting/matcap.hlsl
@@ -1,14 +1,20 @@
+#include "sphereMap.glsl"
+
 /*
 original_author: Patricio Gonzalez Vivo
 description: matcap
-use: <float2> matcap(<float3> eye, <float3> normal) 
+use: 
+    - <float2> matcap(<float3> normal, <float3> eye) 
+    - <float2> matcap(<sampler2D> tex, <float3> normal, <float3> eye) 
 */
 
 #ifndef FNC_MATCAP
 #define FNC_MATCAP
-float2 matcap(float3 eye, float3 normal) {
-    float3 reflected = reflect(eye, normal);
-    float m = 2.8284271247461903 * sqrt( reflected.z+1.0 );
-    return reflected.xy / m + 0.5;
-}
+// float2 matcap(float3 normal, float3 eye) {
+//     float3 reflected = reflect(eye, normal);
+//     float m = 2.8284271247461903 * sqrt( reflected.z+1.0 );
+//     return reflected.xy / m + 0.5;
+// }
+
+#define matcap sphereMap
 #endif
--- lighting/sphereMap.hlsl
+++ lighting/sphereMap.hlsl
@@ -2,40 +2,34 @@
 original_author: Patricio Gonzalez Vivo
 description: given a Spherical Map texture and a normal direction returns the right pixel
 use: spheremap(<sampler2D> texture, <float3> normal)
-options:
-  SPHEREMAP_EYETOPOINT: where the eye is looking
 */
 
-#ifndef SPHEREMAP_EYETOPOINT
-#define SPHEREMAP_EYETOPOINT float3(0.,0.,1.)
+#ifndef SAMPLER_FNC
+#define SAMPLER_FNC(TEX, UV) texture2D(TEX, UV)
 #endif
-
 
 #ifndef SPHEREMAP_TYPE
 #define SPHEREMAP_TYPE float4
 #endif
 
 #ifndef SPHEREMAP_SAMPLER_FNC
-#define SPHEREMAP_SAMPLER_FNC(POS_UV) texture2D(tex, POS_UV)
+#define SPHEREMAP_SAMPLER_FNC(POS_UV) SAMPLER_FNC(tex, POS_UV)
 #endif
 
 #ifndef FNC_SPHEREMAP
 #define FNC_SPHEREMAP
-SPHEREMAP_TYPE sphereMap (in sampler2D tex, in float3 normal, in float3 eye) {
-  float3 r = reflect( eye, normal );
-	r.z += 1.;
-  float m = 2. * length(r);
-  vec2 uv = r.xy / m + .5;
-  return SPHEREMAP_SAMPLER_FNC(uv);
+float2 sphereMap(float3 normal, float3 eye) {
+    // float3 reflected = reflect(eye, normal);
+    // float m = 2.8284271247461903 * sqrt( reflected.z+1.0 );
+    // return reflected.xy / m + 0.5;
+    float3 r = reflect(eye, normal);
+    r.z += 1.;
+    float m = 2. * length(r);
+    float2 uv = r.xy / m + .5;
 }
 
-#ifdef FNC_TEXTURE2D
-SPHEREMAP_TYPE sphereMap (in SamplerVideo tex, in float3 normal, in float3 eye) {
-  float3 r = reflect( eye, normal );
-	r.z += 1.;
-  float m = 2. * length(r);
-  vec2 uv = r.xy / m + .5;
-  return SPHEREMAP_SAMPLER_FNC(uv);
+
+SPHEREMAP_TYPE sphereMap (in sampler2D tex, in float3 normal, in float3 eye) {
+    return SPHEREMAP_SAMPLER_FNC( sphereMap(normal, eye) );
 }
-#endif
 #endif
