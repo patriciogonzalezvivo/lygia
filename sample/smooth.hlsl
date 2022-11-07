3f0ce7401d4bf61c060b2acc8ebfef5bb7b7365c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-11-07 14:13:40 -0500

    fixing sampleSmooth

--- sample/smooth.hlsl
+++ sample/smooth.hlsl
@@ -1,22 +1,30 @@
+#include "../math/cubic.glsl"
+#include "../math/quintic.glsl"
+
 /*
 original_author: Inigo Quiles
 description: avoid the ugly artifacts of bilinear texture filtering. You can find more information here https://iquilezles.org/articles/texture
 use:> <float4 sampleSmooth(<sampler2D> tex, <float2> st, <float2> texResolution);
 options:
     - SAMPLER_FNC(TEX, UV)
+    - SAMPLESMOOTH_POLYNOMIAL: cubic or quartic
 */
 
 #ifndef SAMPLER_FNC
-#define SAMPLER_FNC(TEX, UV) tex2D(TEX, UV)
+#define SAMPLER_FNC(TEX, UV) texture2D(TEX, UV)
+#endif
+
+#ifndef SAMPLESMOOTH_POLYNOMIAL
+#define SAMPLESMOOTH_POLYNOMIAL cubic
 #endif
 
 #ifndef FNC_SAMPLESMOOTH
 #define FNC_SAMPLESMOOTH
 float4 sampleSmooth(sampler2D tex, float2 st, float2 texResolution) {
     st *= texResolution + 0.5;
-    float2 ist = floor( st );
     float2 fst = frac( st );
-    st = ist + fst*fst*(3.0-2.0*fst); // fuv*fuv*fuv*(fuv*(fuv*6.0-15.0)+10.0);
+    st = floor( st );
+    st += SAMPLESMOOTH_POLYNOMIAL(fst);
     st = (st - 0.5) / texResolution;
     return SAMPLER_FNC( tex, st );
 }
