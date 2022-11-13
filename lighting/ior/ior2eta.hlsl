b672ccbc4663e655cacde6ce80a3a1c6e2c9abaa
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-11-13 03:37:10 -0500

    adding ior calc

---
+++ lighting/ior/ior2eta.hlsl
@@ -0,0 +1,12 @@
+/*
+original_author: Patricio Gonzalez Vivo
+description: index of refraction to ratio of index of refraction
+use: <float|float3|float4> ior2eta(<float|float3|float4> ior)
+*/
+
+#ifndef FNC_IOR2ETA
+#define FNC_IOR2ETA
+float ior2eta( const float ior ) { return 1.0/ior; }
+float3 ior2eta( const float3 ior ) { return 1.0/ior; }
+float4 ior2eta( const float4 ior ) { return float4(1.0/ior.rgb, ior.a); }
+#endif
---
+++ lighting/ior/ior2f0.glsl
@@ -0,0 +1,14 @@
+#include "../math/pow2.glsl"
+
+/*
+original_author: Patricio Gonzalez Vivo
+description: index of refraction to reflectance at 0 degree https://handlespixels.wordpress.com/tag/f0-reflectance/
+use: <float|vec3|vec4> ior2f0(<float|vec3|vec4> ior)
+*/
+
+#ifndef FNC_IOR2F0
+#define FNC_IOR2F0
+float ior2f0(const float ior) { return pow2(ior - 1.0) / pow2(ior + 1.0); }
+vec3 ior2f0(const vec3 ior) { return pow2(ior - 1.0) / pow2(ior + 1.0); }
+vec4 ior2f0(const vec4 ior) { return vec4(pow2(ior.rgb - 1.0) / pow2(ior.rgb + 1.0), ior.a); }
+#endif
