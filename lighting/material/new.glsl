2e390fc05e48cd797b3582e79d0c9cb4fe5846fc
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-11-14 06:43:40 -0500

    cleaning

--- lighting/material/new.glsl
+++ lighting/material/new.glsl
@@ -60,11 +60,16 @@ void materialNew(out Material _mat) {
     #if defined(SCENE_BACK_SURFACE) && defined(RESOLUTION)
     vec4 back_surface       = SAMPLER_FNC(SCENE_BACK_SURFACE, gl_FragCoord.xy / RESOLUTION);
     _mat.normal_back        = back_surface.xyz;
+    #if defined(SHADING_MODEL_SUBSURFACE)
     _mat.thickness          = saturate(gl_FragCoord.z - back_surface.a);
-    #elif defined(SCENE_BACK_SURFACE)
+    #endif
+    #else
+    #if defined(SCENE_BACK_SURFACE)
     _mat.normal_back        = -_mat.normal;
-    #elif defined(SHADING_MODEL_SUBSURFACE)
+    #endif
+    #if defined(SHADING_MODEL_SUBSURFACE)
     _mat.thickness          = 0.5;
+    #endif
     #endif
 
     // PBR Properties
@@ -73,10 +78,7 @@ void materialNew(out Material _mat) {
     _mat.roughness          = materialRoughness();
     _mat.metallic           = materialMetallic();
 
-#if defined(SHADING_MODEL_TRANSPARENT)
-    _mat.ior                = vec3(IOR_GLASS_RGB); // Index of Refraction
-#endif
-
+    _mat.ior                = vec3(IOR_GLASS_RGB);      // Index of Refraction
     _mat.f0                 = vec3(0.04, 0.04, 0.04);   // reflectance at 0 degree
 
     // Shade
@@ -101,9 +103,7 @@ void materialNew(out Material _mat) {
 #if defined(MATERIAL_SUBSURFACE_COLOR)
     #if defined(SHADING_MODEL_SUBSURFACE)
     _mat.subsurfaceColor    = vec3(1.0, 1.0, 1.0);
-    #else#if defined(SCENE_BACK_SURFACE)
-    vec3    normal_back;    // world normal of the back surface of the model
-    #endif
+    #else
     _mat.subsurfaceColor    = vec3(0.0, 0.0, 0.0);
     #endif
 #endif
