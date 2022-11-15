633b3c75e51055bce0fcb0640c32a136718d593b
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-11-15 13:39:06 -0500

    adding sampleEquirect to have textureCube like sampling from regular images

---
+++ sample/equirect.hlsl
@@ -0,0 +1,45 @@
+#include "../space/xyz2equirect.hlsl"
+#include "../generative/random.hlsl"
+#include "../generative/srandom.hlsl"
+
+/*
+original_author: Patricio Gonzalez Vivo
+description: sample an equirect texture as it was a cubemap
+use: sampleEquirect(<sampler2D> texture, <float3> dir)
+options:
+    - SAMPLER_FNC(TEX, UV): optional depending the target version of GLSL (texture2D(...) or texture(...))
+    - SAMPLEEQUIRET_ITERATIONS:
+*/
+
+#ifndef SAMPLER_FNC
+#define SAMPLER_FNC(TEX, UV) tex2D(TEX, UV)
+#endif
+
+#ifndef FNC_SAMPLEEQUIRECT
+#define FNC_SAMPLEEQUIRECT
+float4 sampleEquirect(sampler2D tex, float3 dir) { return SAMPLER_FNC(tex, xyz2equirect(dir)); }
+
+float4 sampleEquirect(sampler2D tex, float3 dir, float lod) { 
+    
+    #if defined(SAMPLEEQUIRET_ITERATIONS)
+    float4 acc = float4(0.0, 0.0, 0.0, 0.0);
+    float2 st = xyz2equirect(dir);
+    float2x2 rot = float2x2(cos(GOLDEN_ANGLE), sin(GOLDEN_ANGLE), -sin(GOLDEN_ANGLE), cos(GOLDEN_ANGLE));
+    float r = 1.;
+    float2 vangle = float2(0.0, lod * 0.01);
+    for (int i = 0; i < SAMPLEEQUIRET_ITERATIONS; i++) {
+        vangle = mul(rot, vangle);
+        r++;
+        float4 col = SAMPLER_FNC(tex, st + random( float3(st, r) ) * vangle );
+        acc += col * col;
+    }
+    return float4(acc.rgb/acc.a, 1.0); 
+
+    #else
+    dir += srandom3( dir ) * 0.01 * lod;
+    return SAMPLER_FNC(tex, xyz2equirect(dir));
+
+    #endif
+}
+
+#endif
