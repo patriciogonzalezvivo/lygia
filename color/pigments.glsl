eeff8c908926b3c40a584f4d10f5645e6697aa40
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-11-24 14:00:11 -0500

    try new lut approach

---
+++ color/pigments.glsl
@@ -0,0 +1,51 @@
+#ifndef CADMIUM_YELLOW
+#define CADMIUM_YELLOW vec3(0.996, 0.925, 0.000)
+#endif 
+
+#ifndef HANSA_YELLOW
+#define HANSA_YELLOW vec3(0.988, 0.827, 0.000)
+#endif 
+
+#ifndef CADMIUM_ORANGE
+#define CADMIUM_ORANGE vec3(1.000, 0.412, 0.000)
+#endif 
+
+#ifndef CADMIUM_RED
+#define CADMIUM_RED vec3(1.000, 0.153, 0.008)
+#endif 
+
+#ifndef QUINACRIDONE_MAGENTA
+#define QUINACRIDONE_MAGENTA vec3(0.502, 0.008, 0.180)
+#endif 
+
+#ifndef COBALT_VIOLET
+#define COBALT_VIOLET vec3(0.306, 0.000, 0.259)
+#endif 
+
+#ifndef ULTRAMARINE_BLUE
+#define ULTRAMARINE_BLUE vec3(0.098, 0.000, 0.349)
+#endif 
+
+#ifndef COBALTE_BLUE
+#define COBALTE_BLUE vec3(0.000, 0.129, 0.522)
+#endif 
+
+#ifndef PHTHALO_BLUE
+#define PHTHALO_BLUE vec3(0.051, 0.106, 0.267)
+#endif 
+
+#ifndef PHTHALO_GREEN
+#define PHTHALO_GREEN vec3(0.000, 0.235, 0.196)
+#endif 
+
+#ifndef PERMANENT_GREEN
+#define PERMANENT_GREEN vec3(0.027, 0.427, 0.086)
+#endif 
+
+#ifndef SAP_GREEN
+#define SAP_GREEN vec3(0.420, 0.580, 0.016)
+#endif 
+
+#ifndef BURNT_SIENNA
+#define BURNT_SIENNA vec3(0.482, 0.282, 0.000)
+#endif 
---
+++ color/pigments.hlsl
@@ -0,0 +1,51 @@
+#ifndef CADMIUM_YELLOW
+#define CADMIUM_YELLOW float3(0.996, 0.925, 0.000)
+#endif 
+
+#ifndef HANSA_YELLOW
+#define HANSA_YELLOW float3(0.988, 0.827, 0.000)
+#endif 
+
+#ifndef CADMIUM_ORANGE
+#define CADMIUM_ORANGE float3(1.000, 0.412, 0.000)
+#endif 
+
+#ifndef CADMIUM_RED
+#define CADMIUM_RED float3(1.000, 0.153, 0.008)
+#endif 
+
+#ifndef QUINACRIDONE_MAGENTA
+#define QUINACRIDONE_MAGENTA float3(0.502, 0.008, 0.180)
+#endif 
+
+#ifndef COBALT_VIOLET
+#define COBALT_VIOLET float3(0.306, 0.000, 0.259)
+#endif 
+
+#ifndef ULTRAMARINE_BLUE
+#define ULTRAMARINE_BLUE float3(0.098, 0.000, 0.349)
+#endif 
+
+#ifndef COBALTE_BLUE
+#define COBALTE_BLUE float3(0.000, 0.129, 0.522)
+#endif 
+
+#ifndef PHTHALO_BLUE
+#define PHTHALO_BLUE float3(0.051, 0.106, 0.267)
+#endif 
+
+#ifndef PHTHALO_BLUE
+#define PHTHALO_BLUE float3(0.000, 0.235, 0.196)
+#endif 
+
+#ifndef PERMANENT_GREEN
+#define PERMANENT_GREEN float3(0.027, 0.427, 0.086)
+#endif 
+
+#ifndef SAP_GREEN
+#define SAP_GREEN float3(0.420, 0.580, 0.016)
+#endif 
+
+#ifndef BURNT_SIENNA
+#define BURNT_SIENNA float3(0.482, 0.282, 0.000)
+#endif 
