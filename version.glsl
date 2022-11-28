90362cca5324fe827b16505d19d7b569e52dc1c4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-11-28 04:48:23 -0500

    v1.1.0

---
+++ version.glsl
@@ -0,0 +1,11 @@
+#ifndef LYGIA_VERSION_MAYOR
+#define LYGIA_VERSION_MAYOR 1
+#endif
+
+#ifndef LYGIA_VERSION_MINOR
+#define LYGIA_VERSION_MINOR 1
+#endif
+
+#ifndef LYGIA_VERSION_PATCH
+#define LYGIA_VERSION_PATCH 0
+#endif
