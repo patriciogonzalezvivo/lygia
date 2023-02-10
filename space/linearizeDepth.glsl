81a8d88539c97a4b0b9e999e441618319f95e531
scarletsky <scarletsky1025@gmail.com>
2023-02-10 16:12:52 +0800

    fix: linearizeDepth wrong

--- space/linearizeDepth.glsl
+++ space/linearizeDepth.glsl
@@ -17,7 +17,7 @@ license: |
 
 float linearizeDepth(float depth, float near, float far) {
     depth = 2.0 * depth - 1.0;
-    return (2.0 * near) / (far + near - depth * (far - near));
+    return (2.0 * near * far) / (far + near - depth * (far - near));
 }
 
 #if defined(CAMERA_NEAR_CLIP) && defined(CAMERA_FAR_CLIP)
