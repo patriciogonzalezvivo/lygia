d057af9c9eb201d6f5f1033bb7a434ab0077f76a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-02-14 04:59:55 -0500

    decimate

--- math/decimate.glsl
+++ math/decimate.glsl
@@ -4,8 +4,8 @@ description: decimate a value with an specific presicion
 use: decimate(<float|vec2|vec3|vec4> value, <float|vec2|vec3|vec4> presicion)
 */
 
-#ifndef FNC_DECIMATION
-#define FNC_DECIMATION
+#ifndef FNC_DECIMATE
+#define FNC_DECIMATE
 
 #define decimate(value, presicion) (floor(value * presicion)/presicion)
 
