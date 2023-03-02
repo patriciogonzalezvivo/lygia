1b9ca23d99274bbb254e7eafcacb56d6cc3edc8f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-03-02 05:17:54 -0500

    cleaning

--- math/saturate.glsl
+++ math/saturate.glsl
@@ -7,8 +7,4 @@ use: saturation(<float|vec2|vec3|vec4> value)
 #ifndef FNC_SATURATE
 #define FNC_SATURATE
 #define saturate(x) clamp(x, 0.0, 1.0)
-// float saturate( const in float x){ return clamp(x, 0.0, 1.0); }
-// vec2  saturate( const in vec2 x ){ return clamp(x, 0.0, 1.0); }
-// vec3  saturate( const in vec3 x ){ return clamp(x, 0.0, 1.0); }
-// vec4  saturate( const in vec4 x ){ return clamp(x, 0.0, 1.0); }
 #endif
