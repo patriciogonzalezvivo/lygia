3409c24d7e401231fa0a5a632d77500c9ae3e517
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-03-21 03:59:19 -0400

    color spaces

--- color/space/rgb2lms.hlsl
+++ color/space/rgb2lms.hlsl
@@ -1,17 +1,28 @@
 /*
 original_author: Patricio Gonzalez Vivo  
-description: convert rgb to LMS. LMS (long, medium, short), is a color space which represents the response of the three types of cones of the human eye, named for their responsivity (sensitivity) peaks at long, medium, and short wavelengths. https://en.wikipedia.org/wiki/LMS_color_space
+description: |
+    Convert rgb to LMS. LMS (long, medium, short), is a color space which represents the response of the three types of cones of the human eye, named for their responsivity (sensitivity) peaks at long, medium, and short wavelengths. 
+    Refs https://en.wikipedia.org/wiki/LMS_color_space https://arxiv.org/pdf/1711.10662
 use: <float3|float4> rgb2lms(<float3|float4> rgb)
 */
 
 #ifndef FNC_RGB2LMS
 #define FNC_RGB2LMS
 float3 rgb2lms(float3 rgb) {
-    return float3(
-        (17.8824 * rgb.r) + (43.5161 * rgb.g) + (4.11935 * rgb.b),
-        (3.45565 * rgb.r) + (27.1554 * rgb.g) + (3.86714 * rgb.b),
-        (0.0299566 * rgb.r) + (0.184309 * rgb.g) + (1.46709 * rgb.b)
+
+    // float3x3 rgb2lms_mat = float3x3(
+    //     3.90405e-1, 5.49941e-1, 8.92632e-3,
+    //     7.08416e-2, 9.63172e-1, 1.35775e-3,
+    //     2.31082e-2, 1.28021e-1, 9.36245e-1
+    // );
+
+    float3x3 rgb2lms_mat = float3x3(
+        17.8824,  3.45565,  0.0299566,
+        43.5161, 27.1554,   0.184309,
+        4.11935,  0.184309, 1.46709
     );
+
+    return mul(rgb2lms_mat, rgb);
 }
 float4 rgb2lms(float4 rgb) { return float4( rgb2lms(rgb.rgb), rgb.a); }
 #endif
--- color/vibrance.wgsl
+++ color/vibrance.wgsl
@@ -1,6 +1,8 @@
+#include "luma.wgsl"
+
 fn vibrance(color : vec3<f32>, v:f32) -> vec3<f32> {
-    let average = (color.r + color.g + color.b) * 0.333333;
-    let mx = max(color.r, max(color.g, color.b));
-    let amt = (mx - average) * (-v * 3.0);
-    return mix(color, vec3<f32>(mx), amt );
+    let max_color = max(color.r, max(color.g, color.b));
+    let min_color = min(color.r, min(color.g, color.b));
+    let sat = max_color - min_color;
+    return mix(vec3<f32>( luma(color) ), color, 1.0 + (v * 1.0 - (sign(v) * sat)));
 }
