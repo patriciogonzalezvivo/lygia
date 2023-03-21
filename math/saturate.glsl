2ffa02accbc7a81ab6560c58f5878f73f1242ca6
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-03-21 04:03:06 -0400

    double check saturate

--- math/saturate.glsl
+++ math/saturate.glsl
@@ -4,7 +4,7 @@ description: clamp a value between 0 and 1
 use: saturation(<float|vec2|vec3|vec4> value)
 */
 
-#ifndef FNC_SATURATE
+#if !defined(FNC_SATURATE) && !defined(saturate)
 #define FNC_SATURATE
 #define saturate(x) clamp(x, 0.0, 1.0)
 #endif
