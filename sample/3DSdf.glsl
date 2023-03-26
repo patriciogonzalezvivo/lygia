14d0f0d243c1d8a9e25addb9e30a30f51fb30d53
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-03-26 11:53:40 -0400

    better documetnation for 3D sdf

--- sample/3DSdf.glsl
+++ sample/3DSdf.glsl
@@ -3,20 +3,33 @@
 #include "../sdf/boxSDF.glsl"
 #include "../sdf/opIntersection.glsl"
 
+/*
+Author: Patricio Gonzalez Vivo
+description: Use a 2D texture as to encode a 3D SDF (Signed Distance Field) function
+use: <vecSAMPLE3DSDF_TYPE4> sample3DSdf(in <sampler2D> lut, in <vec3> xyz) 
+options:
+    - SAMPLER_FNC(TEX, UV): optional depending the target version of GLSL (texture2D(...) or texture(...))
+    - SAMPLE_2DCUBE_CELL_SIZE
+    - SAMPLE_2DCUBE_CELLS_PER_SIDE: default 8
+    - SAMPLE_2DCUBE_FNC
+    - SAMPLE3DSDF_TYPE: defaults to float
+    - SAMPLE3DSDF_FNC(TEX, POS): defaults to sample2DCube(TEX, POS).r
+*/
+
 #ifndef SAMPLE3DSDF_TYPE
 #define SAMPLE3DSDF_TYPE float
 #endif
 
-#ifndef SAMPLE3DSDF_COMPONENT
-#define SAMPLE3DSDF_COMPONENT r
+#ifndef SAMPLE3DSDF_FNC
+#define SAMPLE3DSDF_FNC(TEX, POS) sample2DCube(TEX, POS).r
 #endif
 
 #ifndef FNC_SAMPLE3DSDF
 #define FNC_SAMPLE3DSDF
 SAMPLE3DSDF_TYPE sample3DSdf(sampler2D tex, vec3 pos) {
-    pos += .5;
-    pos = scale(pos, .5);
-    SAMPLE3DSDF_TYPE sdf = (sample2DCube(tex, pos).SAMPLE3DSDF_COMPONENT * 2.0 - 1.0) * 1.5;
-    return opIntersection( boxSDF(pos - 0.5, vec3(.5)), sdf);
+    pos += 0.5;
+    pos = scale(pos, 0.5);
+    SAMPLE3DSDF_TYPE sdf = (SAMPLE3DSDF_FNC(tex, pos) * 2.0 - 1.0) * 1.5;
+    return opIntersection( boxSDF(pos - 0.5, vec3(0.5)), sdf);
 }
 #endif
