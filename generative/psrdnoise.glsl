a8a4a03906b64b20865bad98b1aec77c395eb94a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-04-01 05:06:12 -0400

    less verbose license for Stefan Gustavson and Ian McEwan

--- generative/psrdnoise.glsl
+++ generative/psrdnoise.glsl
@@ -20,10 +20,9 @@ Options:
     - PSRDNOISE_PERLIN_GRID
     - PSRDNOISE_FAST_ROTATION
 license: |
-    Copyright 2021 Stefan Gustavson and Ian McEwan
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    Copyright 2021-2023 by Stefan Gustavson and Ian McEwan.
+    Published under the terms of the MIT license:
+    https://opensource.org/license/mit/
 */
 
 #ifndef FNC_PSRFNOISE
