9d62abde15f087512e527def1079cf5ca698ffc9
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-05-11 09:45:06 -0400

    adding example

--- sample/dither.glsl
+++ sample/dither.glsl
@@ -5,10 +5,10 @@
 original_author: Patricio Gonzalez Vivo
 description: sample a texture and dither using a 8x8 Bayer matrix
 use: <vec3> ditherBayer(<sampler2D> tex, <vec2> st, <vec2> resolution)
-
 options:
     - DITHERBAKER_LUT(COLOR): function that returns a vec3 with the color to use for the dithering
-
+examples:
+    - /shaders/color_dither_bayer.frag
 */
 
 #ifndef FNC_SAMPLEDITHER
