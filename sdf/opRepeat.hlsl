df2db43e99ffa4848cfc9f665a726faef8187915
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-07-04 06:47:27 -0400

    changing opRepeat

--- sdf/opRepeat.hlsl
+++ sdf/opRepeat.hlsl
@@ -6,22 +6,22 @@ description: repite operation of one 2D SDFs
 use: <vec4> opElongate( in <float3> p, in <float3> h )
 */
 
-#ifndef FNC_OPREPITE
-#define FNC_OPREPITE
+#ifndef FNC_OPREPEAT
+#define FNC_OPREPEAT
 
-float2 opRepite( in float2 p, in float s ) {
+float2 opRepeat( in float2 p, in float s ) {
     return mod(p+s*0.5,s)-s*0.5;
 }
 
-float3 opRepite( in float3 p, in float3 c ) {
+float3 opRepeat( in float3 p, in float3 c ) {
     return mod(p+0.5*c,c)-0.5*c;
 }
 
-float2 opRepite( in float2 p, in float2 lima, in float2 limb, in float s ) {
+float2 opRepeat( in float2 p, in float2 lima, in float2 limb, in float s ) {
     return p-s*clamp(floor(p/s), lima, limb);
 }
 
-float3 opRepite( in float3 p, in float3 lima, in float3 limb, in float s ) {
+float3 opRepeat( in float3 p, in float3 lima, in float3 limb, in float s ) {
     return p-s*clamp(floor(p/s), lima, limb);
 }
 
