8ec4d14e7facda83574f992afa7625ba260ff8a0
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-07-28 15:41:47 -0400

    update function

--- sdf/supershape.glsl
+++ sdf/supershape.glsl
@@ -11,8 +11,8 @@ use: supershapeSDF(<vec2> st, <float> size s, <float> a, <float> b, <float> n1, 
 #define FNC_SUPERSHAPESDF
 float supershapeSDF( in vec2 st, in float s, in float a, in float b, in float n1, in float n2, in float n3, in float m ) {
   vec2 q;
-  float d = spherical( st ).x;
-  float theta = spherical( st ).y;
+  float d = cart2polar( st ).y;
+  float theta = cart2polar( st ).x;
   float r = superFormula(theta, a, b, n1, n2, n3, m);
   q.x = s * r * cos(theta);
   q.y = s * r * sin(theta);
