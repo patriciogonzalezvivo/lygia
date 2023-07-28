e658c538ae5ad5984961d000c27d7852c46b8734
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-07-28 16:16:43 -0400

    Update supershapeSDF.glsl
    add notes about parameters

--- sdf/supershapeSDF.glsl
+++ sdf/supershapeSDF.glsl
@@ -3,8 +3,15 @@
 
 /*
 author: Kathy McGuiness
+supershape_author: Paul Bourke
 description: Returns 2D supershape
 use: supershapeSDF(<vec2> st, <float> size s, <float> a, <float> b, <float> n1, <float> n2, <float> n3, <float> m)
+Notes about parameters:
+*m determines number of sides/branches; m = 0 yields a circle
+*a!=b results in a assymetrical shape
+*n1=n2=n3<1, the shape is "pinched"
+*n1>n2,n3, the shape is "bloated"
+*n1!=n2!=n3, the shape is assymetrical
 */
 
 #ifndef FNC_SUPERSHAPESDF
