69ea65f5454f26b5826d1116c64279996b88027e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-07-29 08:31:02 -0400

    cleaning

--- space/cart2polar.glsl
+++ space/cart2polar.glsl
@@ -1,18 +1,21 @@
 /*
-original_author: Ivan Dianov
+original_author: [Ivan Dianov, Kathy McGuiness]
 description: cartesian to polar transformation.
-use: cart2polar(<vec2> st)
+use: <vec2|vec3> cart2polar(<vec2|vec3> st)
 */
 
 #ifndef FNC_CART2POLAR
 #define FNC_CART2POLAR
+
 vec2 cart2polar(in vec2 st) {
     return vec2(atan(st.y, st.x), length(st));
 }
+
 vec3 cart2polar( in vec3 st ) {
    float r = length(st);
    float theta = atan(length(st.xy), st.z);
    float phi = atan(st.y, st.x);
    return vec3(r, theta, phi);
 }
+
 #endif
--- space/cart2polar.hlsl
+++ space/cart2polar.hlsl
@@ -1,16 +1,17 @@
 /*
-original_author: Ivan Dianov
+original_author: [Ivan Dianov, Kathy McGuiness]
 description: cartesian to polar transformation.
-use: cart2polar(<float2> st)
+use: <float2|float3> cart2polar(<float2|float3> st)
 */
 
 #ifndef FNC_CART2POLAR
 #define FNC_CART2POLAR
+
 float2 cart2polar(in float2 st) {
     return float2(atan2(st.y, st.x), length(st));
 }
-float3 cart2polar( in float3 st) 
-{
+
+float3 cart2polar( in float3 st) {
    float r = length(st);
    float theta = atan( length(st.xy), st.z);
    float phi = atan(st.y, st.x);
