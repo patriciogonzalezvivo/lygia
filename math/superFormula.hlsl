b03d2580d9afe5f82dac01e686468200abaa91f4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-07-29 07:47:02 -0400

    pruning superShaperSDF

--- math/superFormula.hlsl
+++ math/superFormula.hlsl
@@ -1,21 +1,23 @@
 /*
 original_author: Daniel Shiffman
-description: Returns r
+description: |
+    [Superformula](https://en.wikipedia.org/wiki/Superformula) is a generalization of the superellipse and was proposed by Johan Gielis around 2000.
+    Gielis suggested that the formula can be used to describe many complex shapes and curves that are found in nature.
+    https://www.youtube.com/watch?v=ksRoh-10lak
 use: superFormula(in <float> theta, in <float> a, in <float> b, in <float> n1, in <float> n2, in <float> n3, in <float> m)
-a, b, n1, n2, n3, m parameters
 */
 
 #ifndef FNC_SUPERFORMULA
 #define FNC_SUPERFORMULA
 float superFormula(in float theta, in float a, in float b, in float n1, in float n2, in float n3, in float m) {
-  float t1 = abs((1.0/a) * cos(m * theta / 4.0));
-  t1 = pow(t1, n2);
-  
-  float t2 = abs((1.0/b) * sin(m * theta / 4.0));
-  t2 = pow(t2, n3);
-  
-  float t3 = t1 + t2;
-  float r = pow(t3, -1.0 / n1);
-  return r;
+    float t1 = abs((1.0/a) * cos(m * theta / 4.0));
+    t1 = pow(t1, n2);
+
+    float t2 = abs((1.0/b) * sin(m * theta / 4.0));
+    t2 = pow(t2, n3);
+
+    float t3 = t1 + t2;
+    float r = pow(t3, -1.0 / n1);
+    return r;
 }
 #endif
