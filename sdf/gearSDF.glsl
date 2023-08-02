f5dfb4e7ceffc18cb8a839039b940db047f3a173
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-08-02 13:14:26 -0400

    Incorporate hyperbolic tan function into gearSDF

--- sdf/gearSDF.glsl
+++ sdf/gearSDF.glsl
@@ -12,17 +12,15 @@ use: gearSDF(<vec2> st, <float> b, <int> n_spokes)
 
 #ifndef FNC_GEARSDF
 #define FNC_GEARSDF
-float hyperbolicTan( float theta ) {
+float gearSDF( vec2 st, float b, int N) {
     float e = 2.71828182845904523536;
-    float l = pow(e, 2.0 * theta);
-    return (l - 1.0) / (l + 1.0);
-}
-float gearSDF( vec2 st, float b, int N ) {
     st = st * 2.0 - 1.0;
     float s = map(b, 1.0, 15.0, 0.066, 0.5);
     float d = length(st) - s;
-    float theta = atan(st.y, st.x);
-    float r = (1.0/b)*(hyperbolicTan(b * sin(float(N)*theta)));
+    float omega = b * sin(float(N)*atan(st.y, st.x));
+    float l = pow(e, 2.0 * omega);
+    float hyperTan = (l - 1.0) / (l + 1.0);
+    float r = (1.0/b)*hyperTan;
     return d + min(d, r);
 }
 #endif
