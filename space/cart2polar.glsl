5e0281f6f2b7d9e650a2b18549eb10c05f0be927
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-08-03 15:08:40 -0400

    adding mod2 and improving cart2polar

--- space/cart2polar.glsl
+++ space/cart2polar.glsl
@@ -12,10 +12,10 @@ vec2 cart2polar(in vec2 st) {
 }
 
 vec3 cart2polar( in vec3 st ) {
-   float r = length(st);
-   float theta = atan(length(st.xy), st.z);
-   float phi = atan(st.y, st.x);
-   return vec3(r, theta, phi);
+    float r = length(st);
+    float theta = acos(st.z/r);
+    float phi = atan(st.y, st.x);
+    return vec3(r, theta, phi);
 }
 
 #endif
