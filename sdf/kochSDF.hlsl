aa46ada267707a391628522f59888d5ab87e11a5
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-08-05 09:12:28 -0400

    update original author and fix loop

--- sdf/kochSDF.hlsl
+++ sdf/kochSDF.hlsl
@@ -1,7 +1,7 @@
 #include "../math/const.hlsl"
 
 /*
-original_author: The Art of Code
+original_author: Martijn Steinrucken
 description: Returns a Koch curve SDF   
 use: <float2> kochSDF(<float2> st, <int> iterations)
 */
@@ -9,41 +9,24 @@ use: <float2> kochSDF(<float2> st, <int> iterations)
 #ifndef FNC_KOCHSDF
 #define FNC_KOCHSDF
 float2 kochSDF( float2 st, int N ) {
-    float angle = (5.0/6.0)*PI;
     st = st * 2.0 - 1.0;
     st.x = abs(st.x); 
     st.y += tan(angle)*0.5;
-    float2 n = float2(sin(angle), cos(angle));
+    float2 n = float2(sin((5.0/6.0)*PI), cos((5.0/6.0)*PI));
     float d = dot(st- vec2(0.5,0.0), n);
     st -= n * max(0.0, d) * 2.0;
     n = float2(sin((2.0/3.0)*PI), cos((2.0/3.0)*PI));
     float scale = 1.0;
-    st.x += 0.5; 
-    if (N == 1){
+    st.x += 0.5;
+    #if defined(PLATFORM_WEBGL)
+    if (int i = 0; i < N; i++){
+    #endif
         st *= 3.0;
         scale *= 3.0;
         st.x -= 1.5; 
         st.x = abs(st.x) - 0.5;;
         d = dot(st, n);
         st -= n * min(0.0, d) * 2.0;
-    } else if (N == 2){
-        for (int i = 0; i < 2; i++) {
-            st *= 3.0;
-            scale *= 3.0;
-            st.x -= 1.5; 
-            st.x = abs(st.x) - 0.5;;
-            d = dot(st, n);
-            st -= n * min(0.0, d) * 2.0;
-       }
-    } else if (N == 3){
-        for (int i = 0; i < 3; i++) {
-            st *= 3.0;
-            scale *= 3.0;
-            st.x -= 1.5; 
-            st.x = abs(st.x) - 0.5;;
-            d = dot(st, n);
-            st -= n * min(0.0, d) * 2.0;
-        }
     }
     return st / scale;
 }
