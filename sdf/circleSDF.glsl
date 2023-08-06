862add5d2db5ce77f02aa94f636fd3c31db71cee
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-08-06 11:08:12 -0400

    consistency on 2D shapes

--- sdf/circleSDF.glsl
+++ sdf/circleSDF.glsl
@@ -3,7 +3,8 @@ original_author: Patricio Gonzalez Vivo
 description: Returns a circle-shaped SDF.
 use: circleSDF(vec2 st[, vec2 center])
 options:
-    CIRCLESDF_FNC(POS_UV) : function used to calculate the SDF, defaults to GLSL length function, use lengthSq for a different slope
+    - CENTER_2D : vec2, defaults to vec2(.5)
+    - CIRCLESDF_FNC(POS_UV) : function used to calculate the SDF, defaults to GLSL length function, use lengthSq for a different slope
 */
 
 #ifndef CIRCLESDF_FNC
--- sdf/gearSDF.hlsl
+++ sdf/gearSDF.hlsl
@@ -7,22 +7,36 @@ description: |
     Some notes about the parameters:
         * b determines the length and roundness of the spokes
         * n is the number of spokes 
-use: gearSDF(<float2> st, <float> b, <int> n_spokes)
+use: 
+    - gearSDF(<float2> st, <float2> center, <float> b, <int> n_spokes);
+    - gearSDF(<float2> st, <float> b, <int> n_spokes);
+options:
+    - CENTER_2D: <float2> the center of the gear
 examples:
     - https://gist.githubusercontent.com/kfahn22/84ded9666e6037fdb62376ccffb6582e/raw/14bd8fb7911b0dc5aec635357693e21fdda512bc/gear.frag
 */
 
 #ifndef FNC_GEARSDF
 #define FNC_GEARSDF
-float gearSDF( float2 st, float b, int N ) 
-    float e = 2.71828182845904523536;
-    st = st * 2.0 - 1.0;
+float gearSDF( float2 st, float2 center, float b, int N) {
+    const float e = 2.71828182845904523536;
+    st += 0.5;
+    st -= center;
+    st *= 3.0;
     float s = map(b, 1.0, 15.0, 0.066, 0.5);
     float d = length(st) - s;
-    float omega = b * sin(float(N)*atan(st.y, st.x));
+    float omega = b * sin(float(N) * atan(st.y, st.x));
     float l = pow(e, 2.0 * omega);
     float hyperTan = (l - 1.0) / (l + 1.0);
-    float r = (1.0/b)*hyperTan;
-    return d + min(d, r);
+    float r = (1.0/b) * hyperTan;
+    return (d + min(d, r));
+}
+
+float gearSDF(float2 st, float b, int N) {
+    #ifdef CENTER_2D
+    return gearSDF(st, CENTER_2D, b, N);
+    #else 
+    return gearSDF(st, float2(0.5, 0.5), b, N);
+    #endif
 }
 #endif
