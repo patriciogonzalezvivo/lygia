30ec9198ab90569d5283387545a917c16faf791e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-08-06 14:08:05 -0400

    fixing julia

--- sdf/juliaSDF.glsl
+++ sdf/juliaSDF.glsl
@@ -1,6 +1,7 @@
 #include "../math/pow2.glsl"
 
 /*
+original_author: Kathy McGuiness
 description: |
     Returns the Juia set SDF
     For more information about the Julia set, check [this article](https://en.wikipedia.org/wiki/Julia_set)
@@ -15,18 +16,27 @@ use: juliaSDF(<vec2> st, <vec2> c, <float> r)
 
 #ifndef FNC_JULIASDF
 #define FNC_JULIASDF
-float juliaSDF( vec2 st, vec2 c, float r) {
-    st = st * 2.0 - 1.0;
+float juliaSDF( vec2 st, vec2 center, vec2 c, float r) {
+    st -= 0.5;
+    st *= 2.0;
     vec2 z = vec2(0.0) - (st) * r;
     float n = 0.0;
     const int I = 500;
-    for (int i = I; i > 0; i --) { 
+    for (int i = I; i > 0; i--) { 
         if ( length(z) > 4.0 ) { 
-        n = float(i)/float(I); 
-        break;
+            n = float(i)/float(I); 
+            break;
         } 
         z = vec2( (pow2(z.x) - pow2(z.y)) + c.x, (2.0*z.x*z.y) + c.y ); 
     } 
     return n;
+}
+
+float juliaSDF( vec2 st, vec2 c, float r) {
+    #ifdef CENTER_2D
+        return juliaSDF(st, CENTER_2D, c, r); 
+    #else 
+        return juliaSDF(st, vec2(0.5), c, r); 
+    #endif
 }
 #endif
