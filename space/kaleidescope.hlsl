319052ae9bbd3e6d61566f7b19bd268472c3262b
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-08-07 15:40:46 -0400

    Replace trig with constants

--- space/kaleidescope.hlsl
+++ space/kaleidescope.hlsl
@@ -13,15 +13,16 @@ use:
 float2 kaleidescope( float2 st ) {
     st = st * 2.0 - 1.0;
     st = abs(st); 
-    st.y += tan((5.0/6.0)*PI)*0.5;
-    float2 n = float2(cos((5.0/6.0)*PI), sin((5.0/6.0)*PI));
+    st.y += -0.288;
+    float2 n = float2(-0.866, 0.5);
     float d = dot(st- 0.5, n);
     st -= n * max(0.0, d) * 2.0;
-    st.y -= tan((2.0/3.0)*PI) * 0.25; 
-    n = float2(cos((2.0/3.0)*PI), sin((2.0/3.0)*PI));
+    st.y -= -0.433; 
+    n = float2(-0.5, 0.866);
     d = dot(st, n);
     st -= n * min(0.0, d) * 2.0;
-    st.y -= tan((1.0/6.0)*PI) * 0.5; 
+    st.y -= 0.288; 
     return st;
+}
 }
 #endif
