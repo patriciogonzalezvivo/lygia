63a967711493d428eaef0740a1ff89dd3b83b005
Guido Schmidt <git@guidoschmidt.cc>
2023-08-12 11:43:04 +0200

    FIX/sample/marchingSquares: sanitize centerOf macro parameters

--- sample/marchingSquares.glsl
+++ sample/marchingSquares.glsl
@@ -22,7 +22,7 @@ float defaultSampleFunction(in sampler2D tex, in vec2 uv) {
 #define FNC_SAMPLEMARCHINGSQUARES(TEX, UV) defaultSampleFunction(TEX, UV)
 #endif
 
-#define centerOf(p1, p2, v1, v2) mix(p1, p2, 0.5)
+#define centerOf(p1, p2) mix(p1, p2, 0.5)
 
 float sampleOutline(in vec2 p, in vec2 cellUv, in vec2 a, in vec2 b, in bool straight) {
     float lineStrength = (straight ? 1.3333 : 1.0);
@@ -110,94 +110,94 @@ vec2 sampleMarchinSquares(in vec2 uv, in sampler2D tex, in float cellSize, in fl
     }
     // Distance field cuts through the cell vertically
     if (v_bl < thr && v_br >= thr && v_tl < thr && v_tr >= thr) {
-        a = centerOf(p_tl, p_tr, v_tl, v_tr);
-        b = centerOf(p_bl, p_br, v_bl, v_br);
+        a = centerOf(p_tl, p_tr);
+        b = centerOf(p_bl, p_br);
         ms.r = sampleTile(uv, gridUVCells, a, b, 0);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     if (v_bl >= thr && v_br < thr && v_tl >= thr && v_tr < thr) {
-        a = centerOf(p_tl, p_tr, v_tl, v_tr);
-        b = centerOf(p_bl, p_br, v_bl, v_br);
+        a = centerOf(p_tl, p_tr);
+        b = centerOf(p_bl, p_br);
         ms.r = sampleTile(uv, gridUVCells, a, b, 1);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through the cell horizontally
     if ((v_tl < thr && v_tr < thr && v_bl >= thr && v_br >= thr) ||
     (v_tl >= thr && v_tr >= thr && v_bl < thr && v_br < thr)) {
-        a = centerOf(p_bl, p_tl, v_tl, v_bl);
-        b = centerOf(p_tr, p_br, v_tr, v_br);
+        a = centerOf(p_bl, p_tl);
+        b = centerOf(p_tr, p_br);
         ms.r = sampleTile(uv, gridUVCells, a, b, 2);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     if (v_tl < thr && v_tr < thr && v_bl >= thr && v_br >= thr) {
-        a = centerOf(p_bl, p_tl, v_tl, v_bl);
-        b = centerOf(p_tr, p_br, v_tr, v_br);
+        a = centerOf(p_bl, p_tl);
+        b = centerOf(p_tr, p_br);
         ms.r = sampleTile(uv, gridUVCells, a, b, 3);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through bottom left corner
     if (v_bl < thr && v_tl >= thr && v_tr >= thr && v_br >= thr) {
-        a = centerOf(p_br, p_bl, v_br, v_bl);
-        b = centerOf(p_bl, p_tl, v_bl, v_tl);
+        a = centerOf(p_br, p_bl);
+        b = centerOf(p_bl, p_tl);
         ms.r = sampleTile(uv, gridUVCells, a, b, 4);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     if (v_bl >= thr && v_tl < thr && v_tr < thr && v_br < thr) {
-        a = centerOf(p_br, p_bl, v_br, v_bl);
-        b = centerOf(p_bl, p_tl, v_bl, v_tl);
+        a = centerOf(p_br, p_bl);
+        b = centerOf(p_bl, p_tl);
         ms.r = sampleTile(uv, gridUVCells, a, b, 5);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through bottom right corner
     if (v_br < thr && v_bl >= thr && v_tl >= thr && v_tr >= thr) {
-        a = centerOf(p_tr, p_br, v_tr, v_br);
-        b = centerOf(p_br, p_bl, v_br, v_bl);
+        a = centerOf(p_tr, p_br);
+        b = centerOf(p_br, p_bl);
         ms.r = sampleTile(uv, gridUVCells, a, b, 6);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     if (v_br >= thr && v_bl < thr && v_tl < thr && v_tr < thr) {
-        a = centerOf(p_tr, p_br, v_tr, v_br);
-        b = centerOf(p_br, p_bl, v_br, v_bl);
+        a = centerOf(p_tr, p_br);
+        b = centerOf(p_br, p_bl);
         ms.r = sampleTile(uv, gridUVCells, a, b, 7);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through top left corner
     if (v_tl < thr && v_tr >= thr && v_bl >= thr && v_br >= thr) {
-        a = centerOf(p_bl, p_tl, v_bl, v_tl);
-        b = centerOf(p_tl, p_tr, v_tl, v_tr);
+        a = centerOf(p_bl, p_tl);
+        b = centerOf(p_tl, p_tr);
         ms.r =  sampleTile(uv, gridUVCells, a, b, 8);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     if (v_tl >= thr && v_tr < thr && v_bl < thr && v_br < thr) {
-        a = centerOf(p_bl, p_tl, v_bl, v_tl);
-        b = centerOf(p_tl, p_tr, v_tl, v_tr);
+        a = centerOf(p_bl, p_tl);
+        b = centerOf(p_tl, p_tr);
         ms.r = sampleTile(uv, gridUVCells, a, b, 9);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through top right corner
     if (v_tr < thr && v_tl >= thr && v_bl >= thr && v_br >= thr) {
-        a = centerOf(p_tl, p_tr, v_tl, v_tr);
-        b = centerOf(p_tr, p_br, v_tr, v_br);
+        a = centerOf(p_tl, p_tr);
+        b = centerOf(p_tr, p_br);
         ms.r = sampleTile(uv, gridUVCells, a, b, 10);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     if (v_tr >= thr && v_tl < thr && v_bl < thr && v_br < thr) {
-        a = centerOf(p_tl, p_tr, v_tl, v_tr);
-        b = centerOf(p_tr, p_br, v_tr, v_br);
+        a = centerOf(p_tl, p_tr);
+        b = centerOf(p_tr, p_br);
         ms.r = sampleTile(uv, gridUVCells, a, b, 11);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through top right and bottom left corner
     if ((v_tl >= thr && v_tr < thr && v_bl < thr && v_br >= thr)) {
-        a = centerOf(p_tl, p_tr, v_tl, v_tr);
-        b = centerOf(p_tr, p_br, v_tr, v_br);
+        a = centerOf(p_tl, p_tr);
+        b = centerOf(p_tr, p_br);
         ms.r = sampleTile(uv, gridUVCells, a, b, 12);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through top left and bottom right corner
     if ((v_tl < thr && v_tr >= thr && v_bl >= thr && v_br < thr)) {
-        a = centerOf(p_tl, p_bl, v_tl, v_bl);
-        b = centerOf(p_tl, p_tr, v_tl, v_tr);
+        a = centerOf(p_tl, p_bl);
+        b = centerOf(p_tl, p_tr);
         ms.r = sampleTile(uv, gridUVCells, a, b, 13);
         ms.g = sampleOutline(uv, gridUVCells, a, b, false);
     }
