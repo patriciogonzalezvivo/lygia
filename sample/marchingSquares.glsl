c5ecdc45ac5540b50d569bf1809c41d9fc0f5766
Guido Schmidt <git@guidoschmidt.cc>
2023-08-12 11:56:57 +0200

    FIX/marchingSquares: prefix sampling functions

--- sample/marchingSquares.glsl
+++ sample/marchingSquares.glsl
@@ -8,6 +8,9 @@ description: |
     The Marching Squares algorithm generates contour lines (isolines) of a given input for a
     given grid and a threshhold. Is uses a lookup of 16 cases to decide if a cell is inside, outside or
     a specific corner.
+    **References:**
+    - [urbanspr1nter.github.io](https://urbanspr1nter.github.io/marchingsquares/)
+    - []()
 use: <vec3> sampleMarchingSquares(in <vec2> uv, in <sampler2D> tex, in <float> cellSize, in <float> threshold, in <vec2> resolution) 
 options:
     - FNC_SAMPLEMARCHINGSQUARES(TEX, UV): optional sampling function
@@ -24,7 +27,7 @@ float defaultSampleFunction(in sampler2D tex, in vec2 uv) {
 
 #define centerOf(p1, p2) mix(p1, p2, 0.5)
 
-float sampleOutline(in vec2 p, in vec2 cellUv, in vec2 a, in vec2 b, in bool straight) {
+float sampleMarchingSquares_outline(in vec2 p, in vec2 cellUv, in vec2 a, in vec2 b, in bool straight) {
     float lineStrength = (straight ? 1.3333 : 1.0);
     vec2 pa = p - a;
     vec2 ba = b - a;
@@ -32,7 +35,7 @@ float sampleOutline(in vec2 p, in vec2 cellUv, in vec2 a, in vec2 b, in bool str
     return 1.0 - step(lineStrength, length(line));
 }
 
-float sampleTile(in vec2 p, in vec2 cellUv, in vec2 a, in vec2 b, in int tile) {
+float sampleMarchingSquares_tile(in vec2 p, in vec2 cellUv, in vec2 a, in vec2 b, in int tile) {
     vec2 pa = p - a;
     vec2 ba = b - a;
     vec2 line = pa - ba * dot(pa, ba) / dot(ba, ba);
@@ -71,8 +74,6 @@ float sampleTile(in vec2 p, in vec2 cellUv, in vec2 a, in vec2 b, in int tile) {
           return shape13_0 - shape13_1;
         default:
           return 0.0;
-        // @DEBUG: enable the smoothstep in order to show a line for tiles
-        //return smoothstep(0.4, 0.0, length(line));
     }
 }
 
@@ -112,94 +113,94 @@ vec2 sampleMarchinSquares(in sampler2D tex, in vec2 uv, in vec2 resolution, in f
     if (v_bl < thr && v_br >= thr && v_tl < thr && v_tr >= thr) {
         a = centerOf(p_tl, p_tr);
         b = centerOf(p_bl, p_br);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 0);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 0);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     if (v_bl >= thr && v_br < thr && v_tl >= thr && v_tr < thr) {
         a = centerOf(p_tl, p_tr);
         b = centerOf(p_bl, p_br);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 1);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 1);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through the cell horizontally
     if ((v_tl < thr && v_tr < thr && v_bl >= thr && v_br >= thr) ||
     (v_tl >= thr && v_tr >= thr && v_bl < thr && v_br < thr)) {
         a = centerOf(p_bl, p_tl);
         b = centerOf(p_tr, p_br);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 2);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 2);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     if (v_tl < thr && v_tr < thr && v_bl >= thr && v_br >= thr) {
         a = centerOf(p_bl, p_tl);
         b = centerOf(p_tr, p_br);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 3);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 3);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through bottom left corner
     if (v_bl < thr && v_tl >= thr && v_tr >= thr && v_br >= thr) {
         a = centerOf(p_br, p_bl);
         b = centerOf(p_bl, p_tl);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 4);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 4);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     if (v_bl >= thr && v_tl < thr && v_tr < thr && v_br < thr) {
         a = centerOf(p_br, p_bl);
         b = centerOf(p_bl, p_tl);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 5);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 5);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through bottom right corner
     if (v_br < thr && v_bl >= thr && v_tl >= thr && v_tr >= thr) {
         a = centerOf(p_tr, p_br);
         b = centerOf(p_br, p_bl);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 6);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 6);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     if (v_br >= thr && v_bl < thr && v_tl < thr && v_tr < thr) {
         a = centerOf(p_tr, p_br);
         b = centerOf(p_br, p_bl);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 7);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 7);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through top left corner
     if (v_tl < thr && v_tr >= thr && v_bl >= thr && v_br >= thr) {
         a = centerOf(p_bl, p_tl);
         b = centerOf(p_tl, p_tr);
-        ms.r =  sampleTile(uv, gridUVCells, a, b, 8);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r =  sampleMarchingSquares_tile(uv, gridUVCells, a, b, 8);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     if (v_tl >= thr && v_tr < thr && v_bl < thr && v_br < thr) {
         a = centerOf(p_bl, p_tl);
         b = centerOf(p_tl, p_tr);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 9);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 9);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through top right corner
     if (v_tr < thr && v_tl >= thr && v_bl >= thr && v_br >= thr) {
         a = centerOf(p_tl, p_tr);
         b = centerOf(p_tr, p_br);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 10);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 10);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     if (v_tr >= thr && v_tl < thr && v_bl < thr && v_br < thr) {
         a = centerOf(p_tl, p_tr);
         b = centerOf(p_tr, p_br);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 11);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 11);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through top right and bottom left corner
     if ((v_tl >= thr && v_tr < thr && v_bl < thr && v_br >= thr)) {
         a = centerOf(p_tl, p_tr);
         b = centerOf(p_tr, p_br);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 12);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 12);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     // Distance field cuts through top left and bottom right corner
     if ((v_tl < thr && v_tr >= thr && v_bl >= thr && v_br < thr)) {
         a = centerOf(p_tl, p_bl);
         b = centerOf(p_tl, p_tr);
-        ms.r = sampleTile(uv, gridUVCells, a, b, 13);
-        ms.g = sampleOutline(uv, gridUVCells, a, b, false);
+        ms.r = sampleMarchingSquares_tile(uv, gridUVCells, a, b, 13);
+        ms.g = sampleMarchingSquares_outline(uv, gridUVCells, a, b, false);
     }
     return ms;
 }
