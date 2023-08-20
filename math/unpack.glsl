b9d4be924f8b1be9374a85776ff78396a3a2b331
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-08-20 04:00:45 -0400

    packing and unpacking

--- math/unpack.glsl
+++ math/unpack.glsl
@@ -8,6 +8,16 @@ use: <float> unpack(<vec3> value [, <float> base])
 #define UNPACK_FNC unpack256
 #endif 
 
+// https://github.com/mrdoob/three.js/blob/acdda10d5896aa10abdf33e971951dbf7bd8f074/src/renderers/shaders/ShaderChunk/packing.glsl
+#ifndef CONST_PACKING
+#define CONST_PACKING
+const float PackUpscale = 256. / 255.; // fraction -> 0..1 (including 1)
+const float UnpackDownscale = 255. / 256.; // 0..1 -> fraction (excluding 1)
+const vec3 PackFactors = vec3( 256. * 256. * 256., 256. * 256.,  256. );
+const vec4 UnpackFactors = UnpackDownscale / vec4( PackFactors, 1. );
+const float ShiftRight8 = 1. / 256.;
+#endif
+
 #ifndef FNC_UNPACK
 #define FNC_UNPACK
 
@@ -50,4 +60,10 @@ float unpack(vec3 value, float base) {
 float unpack(vec3 value) {
     return UNPACK_FNC(value);
 }
+
+// https://github.com/mrdoob/three.js/blob/acdda10d5896aa10abdf33e971951dbf7bd8f074/src/renderers/shaders/ShaderChunk/packing.glsl
+float unpack( const in vec4 v ) {
+	return dot( v, UnpackFactors );
+}
+
 #endif
