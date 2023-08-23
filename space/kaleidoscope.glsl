0134e2af9c2a8f041ed32d662df0f59a29bb71eb
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-08-23 19:25:11 -0400

    update function

---
+++ space/kaleidoscope.glsl
@@ -0,0 +1,25 @@
+/*
+original_author:  Martijn Steinrucken
+description: It mirrors uvs in a kaleisdoscope pattern.  Based on [Koch Snowflake tutorial](https://www.shadertoy.com/view/tdcGDj) 
+use: <vec2> kaleidoscope(<vec2> st)
+*/
+
+#ifndef FNC_KALEIDOSCOPE
+#define FNC_KALEIDOSCOPE
+vec2 kaleidoscope(vec2 st){
+    #ifdef CENTER_2D
+    st -= CENTER_2D;
+    #else
+    st -= vec2(0.5);
+    #endif
+    float r3 = sqrt(3.);
+    st.x = abs(st.x);  
+    vec2 dir = vec2(1.0, -r3)*0.5;
+    float d = dot(st, dir);  
+    st -= dir * max(0.0, d) * 2.0; 
+    st.x = abs(st.x);
+    dir = vec2(r3, -1.0)*0.5;
+    st -= dir * min(0., dot(st, dir)) * 2.0;
+    return st;
+}
+#endif
---
+++ space/kaleidoscope.hlsl
@@ -0,0 +1,25 @@
+/*
+original_author:  Martijn Steinrucken
+description: It mirrors uvs in a kaleisdoscope pattern.  Based on [Koch Snowflake tutorial](https://www.shadertoy.com/view/tdcGDj) 
+use: <float2> kaleidoscope(<float2> st)
+*/
+
+#ifndef FNC_KALEIDOSCOPE
+#define FNC_KALEIDOSCOPE
+float2 kaleidoscope(float2 st){
+    #ifdef CENTER_2D
+    st -= CENTER_2D;
+    #else
+    st -= float2(0.5, 0.5);
+    #endif
+    float r3 = sqrt(3.);
+    st.x = abs(st.x);  
+    float2 dir = float2(1.0, -r3)*0.5;
+    float d = dot(st, dir);  
+    st -= dir * max(0.0, d) * 2.0; 
+    st.x = abs(st.x);
+    dir = float2(r3, -1.0)*0.5;
+    st -= dir * min(0., dot(st, dir)) * 2.0;
+    return st;
+}
+#endif
