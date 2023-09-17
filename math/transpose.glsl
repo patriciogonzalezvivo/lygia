079e21f481aaee93ae03b8ebe3df50edda89a6a9
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-09-17 13:00:19 -0400

    pyramid

--- math/transpose.glsl
+++ math/transpose.glsl
@@ -8,28 +8,19 @@ use: <mat3|mat4> transpose(in <mat3|mat4> m)
 #define FNC_TRANSPOSE
 
 mat3 transpose(in mat3 m) {
-    vec3 i0 = m[0];
-    vec3 i1 = m[1];
-    vec3 i2 = m[2];
-
     return mat3(
-                    vec3(i0.x, i1.x, i2.x),
-                    vec3(i0.y, i1.y, i2.y),
-                    vec3(i0.z, i1.z, i2.z)
+                    m[0][0], m[1][0], m[2][0],
+                    m[0][1], m[1][1], m[2][1],
+                    m[0][2], m[1][2], m[2][2]
                 );
 }
 
 mat4 transpose(in mat4 m) {
-    vec4 i0 = m[0];
-    vec4 i1 = m[1];
-    vec4 i2 = m[2];
-    vec4 i3 = m[3];
-
     return mat4(
-                    vec4(i0.x, i1.x, i2.x, i3.x),
-                    vec4(i0.y, i1.y, i2.y, i3.y),
-                    vec4(i0.z, i1.z, i2.z, i3.z),
-                    vec4(i0.w, i1.w, i2.w, i3.w)
+                    vec4(m[0][0], m[1][0], m[2][0], m[3][0]),
+                    vec4(m[0][1], m[1][1], m[2][1], m[3][1]),
+                    vec4(m[0][2], m[1][2], m[2][2], m[3][2]),
+                    vec4(m[0][3], m[1][3], m[2][3], m[3][3])
                 );
 }
 
