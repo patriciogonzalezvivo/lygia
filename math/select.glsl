7df431db1dba18a2bccedcc67945e532f2f3a66c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-09-28 11:09:40 -0400

    compatibility for WGSL select

---
+++ math/select.glsl
@@ -0,0 +1,15 @@
+/*
+original_author: Patricio Gonzalez Vivo
+description: |
+    Returns A when cond is true, and B otherwise. This is in part to bring a compatibility layer with WGSL 
+use: <float|vec2|vec3|vec4> select(<float|vec2|vec3|vec4> A, <float|vec2|vec3|vec4> B, <bool> cond)
+*/
+
+#ifndef FNC_SELECT
+#define FNC_SELECT
+int select(int A, int B, bool cond) { return cond ? A : B; }
+float select(float A, float B, bool cond) { return cond ? A : B; }
+vec2 select(vec2 A, vec2 B, bool cond) { return cond ? A : B; }
+vec3 select(vec3 A, vec3 B, bool cond) { return cond ? A : B; }
+vec4 select(vec4 A, vec4 B, bool cond) { return cond ? A : B; }
+#endif
