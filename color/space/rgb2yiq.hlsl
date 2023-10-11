0a54043090b1e0cadd5314abd69940397f68e7f4
kevinarbor <kevinarbor@fb.com>
2023-10-11 09:35:15 -0700

    removed unterminated define in rgb2yiq.hlsl

--- color/space/rgb2yiq.hlsl
+++ color/space/rgb2yiq.hlsl
@@ -8,9 +8,6 @@ use: <float3|float4> rgb2yiq(<float3|float4> color)
 
 #ifndef FNC_RGB2YIQ
 #define FNC_RGB2YIQ
-
-#ifndef FNC_RGB2YIQ
-#define FNC_RGB2YIQ
 const float3x3 rgb2yiq_mat = float3x3(
     0.300,  0.5900,  0.1100, 
     0.599, -0.2773, -0.3217, 
