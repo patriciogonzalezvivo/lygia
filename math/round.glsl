b12a11029d15bec80b1bf5f89dc4f813d1f66746
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-10-11 05:03:15 -0400

    bilinear for bartlett, and round function

---
+++ math/round.glsl
@@ -0,0 +1,13 @@
+/*
+original_author: Patricio Gonzalez Vivo
+description: round a value to the nearest integer
+use: round(<float|vec2|vec3|vec4> value)
+*/
+
+#ifndef FNC_ROUND
+#define FNC_ROUND
+float round(float x) { return sign(x)*floor(abs(x)+0.5); }
+vec2 round(vec2 x) { return sign(x)*floor(abs(x)+0.5); }
+vec3 round(vec3 x) { return sign(x)*floor(abs(x)+0.5); }
+vec4 round(vec4 x) { return sign(x)*floor(abs(x)+0.5); }
+#endif
