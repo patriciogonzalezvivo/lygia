b394373a4ba06da26d19041e5f053a42807acb51
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-11-18 14:28:55 -0500

    adding stars to atmosphere

--- math/mod2.glsl
+++ math/mod2.glsl
@@ -8,6 +8,12 @@ use: <vec2> mod2(<vec2> x, <vec2> size)
 
 #ifndef FNC_MOD2
 #define FNC_MOD2
+vec2 mod2(inout vec2 p, float s) {
+    vec2 c = floor((p + s*0.5)/s);
+    p = mod(p + s*0.5,s) - s*0.5;
+    return c;
+}
+
 vec2 mod2(inout vec2 p, vec2 s) {
     vec2 c = floor((p + s*0.5)/s);
     p = mod(p + s*0.5,s) - s*0.5;
