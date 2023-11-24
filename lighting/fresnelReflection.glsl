ea9a635c5edae1239c34f6e5c4e9af1b89056a87
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-11-24 18:23:06 -0500

    more efficient

--- lighting/fresnelReflection.glsl
+++ lighting/fresnelReflection.glsl
@@ -3,11 +3,12 @@
 
 /*
 contributors: Patricio Gonzalez Vivo
-description: resolve fresnel coeficient
+description: |
+    Resolve fresnel coeficient and apply it to a reflection. It can apply iridescence to 
+    using a formula based on https://www.alanzucconi.com/2017/07/25/the-mathematics-of-thin-film-interference/
 use: 
     - <vec3> fresnelReflection(<vec3> R, <vec3> f0, <float> NoV)
-    - <vec3> fresnelIridescentReflection(<vec3> R, <vec3> f0, <float> NoV, <float> thickness, <float> ior0, <float> ior1, <float> ior2, <float> roughness)
-    - <vec3> fresnelIridescentReflection(<vec3> normal, <vec3> view, <float> thickness, <float> ior1, <float> ior2)
+    - <vec3> fresnelIridescentReflection(<vec3> normal, <vec3> view, <vec3> f0, <vec3> ior1, <vec3> ior2, <float> thickness, <float> roughness)
     - <vec3> fresnelReflection(<Material> _M)
 */
 
@@ -16,68 +17,58 @@ use:
 
 vec3 fresnelReflection(const in vec3 R, const in vec3 f0, const in float NoV) {
     vec3 frsnl = fresnel(f0, NoV);
-
     vec3 reflectColor = vec3(0.0);
-
     #if defined(FRESNEL_REFLECTION_FNC)
     reflection = FRESNEL_REFLECTION_FNC(R);
-
     #else
     reflectColor = envMap(R, 1.0, 0.001);
-
     #endif
-
     return reflectColor * frsnl;
 }
 
-vec3 fresnelIridescentReflection(vec3 R, vec3 f0, float NoV, float thickness, float ior0, float ior1, float ior2, float roughness) {
-    vec3 frsnl = fresnelIridescent(f0, NoV, thickness, ior0, ior1, ior2, roughness);
-
-    vec3 reflectColor = vec3(0.0);
-
-    #if defined(FRESNEL_REFLECTION_FNC)
-    reflection = FRESNEL_REFLECTION_FNC(R);
-
-    #else
-    reflectColor = envMap(R, roughness, 0.00001);
-
-    #endif
-
-    return reflectColor * frsnl;
-}
-
-vec3 fresnelIridescentReflection(vec3 normal, vec3 view, float thickness, float ior1, float ior2) {
-    
-    // Schlick approximation
+vec3 fresnelIridescentReflection(vec3 normal, vec3 view, float f0, float ior1, float ior2, float thickness, float roughness) {
     float cos0 = -dot(view, normal);
-    float f0 = (ior2-1.)/(ior2+1.);
-    float R = schlick(f0 * f0, 1.0, cos0);
-    float T = 1.0-R;
-
-    const vec3 RGB = vec3(612.,549.,464.);
-
-    // Formula based on:
-    // https://www.alanzucconi.com/2017/07/25/the-mathematics-of-thin-film-interference/
+    float Fr = fresnel(f0, cos0);
+    float T = 1.0 - Fr;
+    const vec3 RGB = vec3(612.0, 549.0, 464.0);
     float a = ior1/ior2;
-    float cosi2 = sqrt(1. - a*a*(1. - cos0*cos0));
-    vec3 shift = 4.*PI*(thickness/RGB)*ior2*cosi2 + HALF_PI;
-    
-    // Intensity of each color channel after interference
-    vec3 irid = R*( 1. + T*( T + 2.*cos(shift) ) );
-    
-    // Reflection and background
-    vec3 ref = envMap(reflect(view, normal), 0.0, 0.0);
-
-    // Specular reflection
+    float cosi2 = sqrt(1.0 - a * a * (1.0 - cos0*cos0));
+    vec3 shift = 4.0 * PI * (thickness/RGB) * ior2 * cosi2 + HALF_PI;
+    vec3 irid = Fr * ( 1.0 + T * ( T + 2.0 * cos(shift) ) );
+    vec3 ref = envMap(reflect(view, normal), roughness, 0.0);
     return (ref + pow5(ref)) * irid;
 }
 
+vec3 fresnelIridescentReflection(vec3 normal, vec3 view, vec3 f0, vec3 ior1, vec3 ior2, float thickness, float roughness) {
+    float cos0 = -dot(view, normal);
+    
+    vec3 Fr = fresnel(f0, cos0);
+    vec3 T = 1.0 - Fr;
+
+    const vec3 RGB = vec3(612.0, 549.0, 464.0);
+
+    vec3 a = ior1/ior2;
+    vec3 cosi2 = sqrt(1.0 - a * a * (1.0 - cos0*cos0));
+    vec3 shift = 4.0 * PI * (thickness/RGB) * ior2 * cosi2 + HALF_PI;
+    vec3 irid = Fr * ( 1.0 + T * ( T + 2.0 * cos(shift) ) );
+    vec3 ref = envMap(reflect(view, normal), roughness, 0.0);
+    return (ref + pow5(ref)) * irid;
+}
+
+vec3 fresnelIridescentReflection(vec3 normal, vec3 view, float ior1, float ior2, float thickness, float roughness) {
+    float F0 = (ior2-1.)/(ior2+1.);
+    return fresnelIridescentReflection(normal, view, F0 * F0, ior1, ior2, thickness, roughness);
+}
+
+vec3 fresnelIridescentReflection(vec3 normal, vec3 view, vec3 ior1, vec3 ior2, float thickness, float roughness) {
+    vec3 F0 = (ior2-1.)/(ior2+1.);
+    return fresnelIridescentReflection(normal, view, F0 * F0, ior1, ior2, thickness, roughness);
+}
 
 #ifdef STR_MATERIAL
 vec3 fresnelReflection(const in Material _M) {
     #if defined(SHADING_MODEL_IRIDESCENCE)
-    return fresnelIridescentReflection(_M.R, _M.f0, _M.NoV, _M.thickness, IOR_AIR, _M.ior.g, IOR_AIR, _M.roughness);
-    // return fresnelIridescentReflection(_M.normal, -_M.V, _M.thickness, IOR_AIR, _M.ior.g) * (1.0-_M.roughness);
+    return fresnelIridescentReflection(_M.normal, -_M.V, _M.f0, vec3(IOR_AIR), _M.ior, _M.thickness, _M.roughness);
     #else
     return fresnelReflection(_M.R, _M.f0, _M.NoV) * (1.0-_M.roughness);
     #endif
