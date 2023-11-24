39b270d8d174f146ab429ba2158d11e9d7df231a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-11-24 18:23:06 -0500

    more efficient

--- lighting/fresnel.glsl
+++ lighting/fresnel.glsl
@@ -14,10 +14,6 @@ use:
 #ifndef FNC_FRESNEL
 #define FNC_FRESNEL
 
-vec3 fresnelIridescent(vec3 f0, float NoV, float thickness, float ior0, float ior1, float ior2, float roughness) {
-    return f0 + (max(vec3(1.0-roughness), f0) - f0) * thinFilmReflectance(NoV, thickness, ior0, ior1, ior2);
-}
-
 vec3 fresnel(vec3 f0, vec3 normal, vec3 view) {
    return schlick(f0, 1.0, dot(view, normal));
 }
