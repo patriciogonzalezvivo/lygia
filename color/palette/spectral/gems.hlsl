789ab543f37c915bbe24b20dfdb44fbd01ff78d6
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-12-07 05:00:34 -0500

    fixing exit character

--- color/palette/spectral/gems.hlsl
+++ color/palette/spectral/gems.hlsl
@@ -2,7 +2,8 @@
 
 /*
 contributors: ["Jos Stam", "Alias Systems"]
-description: From Chap 8: "Simulating Diffraction" from GPU Gems https://developer.nvidia.com/gpugems/gpugems/part-i-natural-effects/chapter-8-simulating-diffraction
+description: |
+	From Chap 8 "Simulating Diffraction" from GPU Gems https://developer.nvidia.com/gpugems/gpugems/part-i-natural-effects/chapter-8-simulating-diffraction
 use: <float3> spectral_gems(<float> x)
 */
 
