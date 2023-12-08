1478622184e9f26c279a4cc2e4516fed845e6c0f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-12-08 16:20:22 -0500

    color checker

---
+++ color/checker.glsl
@@ -0,0 +1,419 @@
+
+// MACBETH COLOR PALETTE
+//
+// Values from:
+//  - http://en.wikipedia.org/wiki/ColorChecker
+//  - http://kurtmunger.com/color_checkerid277.html
+
+#ifndef DARK_SKIN
+//Dark skin		3 YR 3.7/3.2	0.400 0.350 10.1	#735244
+#define DARK_SKIN vec3(0.454, 0.311, 0.255)
+#endif
+
+#ifndef LIGHT_SKIN
+//Light skin	2.2 YR 6.47/4.1	0.377 0.345 35.8	#c29682
+#define LIGHT_SKIN vec3(0.773, 0.563, 0.497)
+#endif
+
+#ifndef BLUE_SKY
+//Blue sky		4.3 PB 4.95/5.5	0.247 0.251 19.3	#627a9d
+#define BLUE_SKY vec3(0.356, 0.472, 0.609)
+#endif
+
+#ifndef FOLIAGE
+//Foliage		6.7 GY 4.2/4.1	0.337 0.422 13.3	#576c43
+#define FOLIAGE vec3(0.359, 0.425, 0.250)
+#endif
+
+#ifndef BLUE_FLOWER
+//Blue flower	9.7 PB 5.47/6.7	0.265 0.240 24.3	#8580b1
+#define BLUE_FLOWER vec3(0.514, 0.498, 0.685)
+#endif
+
+#ifndef BLUISH_GREEN
+//Bluish green	2.5 BG 7/6		0.261 0.343 43.1	#67bdaa
+#define BLUISH_GREEN vec3(0.374, 0.740, 0.673)
+#endif
+
+#ifndef ORANGE
+//Orange		5 YR 6/11		0.506 0.407 30.1	#d67e2c
+#define ORANGE vec3(0.879, 0.487, 0.189)
+#endif
+
+#ifndef PURPLISH_BLUE
+//Purplish blue	7.5 PB 4/10.7	0.211 0.175 12.0	#505ba6
+#define PURPLISH_BLUE vec3(0.269, 0.351, 0.656)
+#endif
+
+#ifndef MODERATE_RED
+//Moderate red	2.5 R 5/10		0.453 0.306 19.8	#c15a63
+#define MODERATE_RED vec3(0.774, 0.313, 0.373)
+#endif
+
+#ifndef PURPLE
+//Purple		5 P 3/7			0.285 0.202 6.6		#5e3c6c
+#define PURPLE vec3(0.364, 0.227, 0.407)
+#endif
+
+#ifndef YELLOW_GREEN
+//Yellow green	5 GY 7.1/9.1	0.380 0.489 44.3	#9dbc40
+#define YELLOW_GREEN vec3(0.612, 0.734, 0.228)
+#endif
+
+#ifndef ORANGE_YELLOW
+//Orange Yellow	10 YR 7/10.5	0.473 0.438 43.1	#e0a32e
+#define ORANGE_YELLOW vec3(0.892, 0.633, 0.153)
+#endif
+
+#ifndef BLUE
+//Blue			7.5 PB 2.9/12.7	0.187 0.129 6.1		#383d96
+#define BLUE vec3(0.156, 0.241, 0.570)
+#endif
+
+#ifndef GREEN
+//Green			0.25 G 5.4/9.6	0.305 0.478 23.4	#469449
+#define GREEN vec3(0.238, 0.577, 0.275)
+#endif
+
+#ifndef RED
+//Red			5 R 4/12		0.539 0.313 12.0	#af363c
+#define RED vec3(0.699, 0.210, 0.222)
+#endif
+
+#ifndef YELLOW
+//Yellow		5 Y 8/11.1		0.448 0.470 59.1	#e7c71f
+#define YELLOW vec3(0.927, 0.782, 0.057)
+#endif
+
+#ifndef MAGENTA
+//Magenta		2.5 RP 5/12		0.364 0.233 19.8	#bb5695
+#define MAGENTA vec3(0.750, 0.309, 0.574)
+#endif
+
+#ifndef CYAN
+//Cyan			5 B 5/8			0.196 0.252 19.8	#0885a1
+#define CYAN vec3(0.000, 0.521, 0.648)
+#endif
+
+#ifndef WHITE
+//White			N 9.5/			0.310 0.316 90.0	#f3f3f2
+#define WHITE vec3(0.945, 0.948, 0.923)
+#endif
+
+#ifndef NEUTRAL_80
+//Neutral 8		N 8/			0.310 0.316 59.1	#c8c8c8
+#define NEUTRAL_80 vec3(0.789, 0.793, 0.788)
+#endif
+
+#ifndef NEUTRAL_65
+//Neutral 6.5	N 6.5/			0.310 0.316 36.2	#a0a0a0
+#define NEUTRAL_65 vec3(0.632, 0.640, 0.638)
+#endif
+
+#ifndef NEUTRAL_50
+//Neutral 5		N 5/			0.310 0.316 19.8	#7a7a79
+#define NEUTRAL_50 vec3(0.473, 0.474, 0.473)
+#endif
+
+#ifndef NEUTRAL_35
+//Neutral 3.5	N 3.5/			0.310 0.316 9.0		#555555
+#define NEUTRAL_35 vec3(0.324, 0.330, 0.331)
+#endif
+
+#ifndef BLACK
+//Black			N 2/			0.310 0.316 3.1		#343434
+#define BLACK vec3(0.194, 0.195, 0.197)
+#endif
+
+// SPYDERCHECKR COLOR PALETTE
+// 
+// Values from:
+//  - https://www.northlight-images.co.uk/datacolor-spydercheckr-colour-test-card-review/
+
+// A1
+// Lab: 61.35,  34.81,  18.38
+// 0.071, 0.107
+
+// A2
+// Lab: 75.50 ,  5.84,  50.42
+// 0.071, 0.264
+
+// A3
+// Lab: 66.82,	-25.1,	23.47
+// 0.071, 0.421
+
+// A4
+// Lab: 60.53,	-22.6, -20.40 
+// 0.071, 0.579
+
+// A5
+// Lab: 59.66,	-2.03, -28.46 
+// 0.071, 0.736
+
+// A6
+// Lab: 59.15,	30.83,  -5.72 
+// 0.071, 0.893
+
+// B1
+// Lab: 82.68,	 5.03,	 3.02
+// 0.175, 0.107
+
+// B2
+// Lab: 82.25,	-2.42,	 3.78
+// 0.175, 0.264
+
+// B3
+// Lab: 82.29,	 2.20,	-2.04
+// 0.175, 0.421
+
+// B4
+// Lab: 24.89,	 4.43,	 0.78
+// 0.175, 0.579
+
+// B5
+// Lab: 25.16,	-3.88,	 2.13
+// 0.175, 0.736
+
+// B6
+// Lab: 26.13,	 2.61,	-5.03
+// 0.175, 0.893
+
+// C1
+// Lab: 85.42,	 9.41,	14.49
+// 0.279, 0.107
+
+// C2
+// Lab: 74.28,	 9.05,	27.21
+// 0.279, 0.264
+
+// C3
+// Lab: 64.57,	12.39,	37.24
+// 0.279, 0.421
+
+// C4
+// Lab: 44.49,	17.23,	26.24
+// 0.279, 0.579
+
+// C5
+// Lab: 25.29,	 7.95,	 8.87
+// 0.279, 0.736
+
+// C6
+// Lab: 22.67,	 2.11,	-1.10
+// 0.279, 0.893
+
+// D1
+// Lab: 92.72,	 1.89,	 2.76
+// 0.384, 0.107
+
+// D2
+// Lab: 88.85,	 1.59,	 2.27
+// 0.384, 0.264
+
+// D3
+// Lab: 73.42,	 0.99,	 1.89
+// 0.384, 0.421
+
+// D4
+// Lab: 57.15,	 0.57,	 1.19
+// 0.384, 0.579
+
+// D5
+// Lab: 41.57,	 0.24,	 1.45
+// 0.384, 0.736
+
+// D6
+// Lab: 25.65,	 1.24,	 0.05
+// 0.384, 0.893
+
+#ifndef CARD_WHITE
+// E1
+// Lab: 96.04,	 2.16,	 2.60 
+// 0.616, 0.107
+// sRGB: 249, 242, 238
+#define CARD_WHITE vec3(0.976, 0.949, 0.933)
+#endif
+
+#ifndef GRAY_20
+// E2
+// Lab: 80.44,	 1.17,	 2.05 
+// 0.616, 0.264
+// sRGB: 202, 198, 195
+#define GRAY_20 vec3(0.792, 0.777, 0.765)
+#endif
+
+#ifndef GRAY_40
+// E3
+// Lab: 65.52,	 0.69,	 1.86 
+// 0.616, 0.421
+// sRGB: 161, 157, 154
+#define GRAY_40 vec3(0.631, 0.616, 0.604)
+#endif
+
+#ifndef GRAY_60
+// E4
+// Lab: 49.62,	 0.58,	 1.56 
+// 0.616, 0.579
+// sRGB: 122, 118, 116
+#define GRAY_60 vec3(0.478, 0.463, 0.455)
+#endif
+
+#ifndef GRAY_80
+// E5
+// Lab: 33.55,	 0.35,	 1.40 
+// 0.616, 0.736
+// sRGB: 80, 80, 78
+#define GRAY_80 vec3(0.314, 0.314, 0.306)
+#endif
+
+#ifndef CARD_BLACK
+// E6
+// Lab: 16.91,	 1.43,	-0.81 
+// 0.616, 0.893
+// sRGB: 43, 41, 43
+#define CARD_BLACK vec3(0.169, 0.161, 0.169)
+#endif
+
+#ifndef PRIMARY_CYAN
+// F1
+// Lab: 47.12, -32.50, -28.75
+// 0.721, 0.107
+// sRGB: 0, 127, 159
+#define PRIMARY_CYAN vec3(0.000, 0.498, 0.623)
+#endif
+
+#ifndef PRIMARY_MAGENTA
+// F2
+// Lab: 50.49,	53.45, -13.55 
+//  0.721, 0.264
+// sRGB: 192, 75, 145
+#define PRIMARY_MAGENTA vec3(0.753, 0.294, 0.569)
+#endif
+
+#ifndef PRIMARY_YELLOW
+// F3
+// Lab: 83.61,	 3.36,	87.02
+// 0.721, 0.421
+// sRGB: 245, 205, 0
+#define PRIMARY_YELLOW vec3(0.961, 0.804, 0.000)
+#endif 
+
+#ifndef PRIMARY_RED
+// F4
+// Lab: 41.05,	60.75,	31.17
+// 0.721, 0.579
+// sRGB: 186, 26, 51
+#define PRIMARY_RED vec3(0.729, 0.102, 0.200)
+#endif
+
+#ifndef PRIMARY_GREEN
+// F5
+// Lab: 54.14, -40.80,	34.75 
+// 0.721, 0.736
+// sRGB: 57, 146, 64
+#define PRIMARY_GREEN vec3(0.224, 0.573, 0.251)
+#endif
+
+#ifndef PRIMARY_BLUE
+// F6
+// Lab: 24.75,	13.78, -49.48 
+// 0.721, 0.893
+// sRGB: 25, 55, 135
+#define PRIMARY_BLUE vec3(0.098, 0.216, 0.529)
+#endif
+
+#ifndef PRIMARY_ORANGE
+// G1
+// Lab: 60.94,	38.21,	61.31 
+// 0.825, 0.107
+// sRGB: 222, 118, 32
+#define PRIMARY_ORANGE vec3(0.871, 0.463, 0.125)
+#endif
+
+#ifndef BLUEPRINT
+// G2
+// Lab: 37.80,	 7.30, -43.04
+// 0.825, 0.26
+// sRGB: 58, 89, 160
+#define BLUEPRINT vec3(0.227, 0.349, 0.627)
+#endif
+
+#ifndef PINK
+// G3
+// Lab: 49.81,	48.50,	15.76 
+// 0.825, 0.421
+// sRGB: 195, 79, 95
+#define PINK vec3(0.765, 0.310, 0.373)
+#endif
+
+#ifndef VIOLET
+// G4
+// Lab: 28.88,	19.36, -24.48
+// 0.825, 0.57
+// sRGB: 83, 58, 106
+#define VIOLET vec3(0.325, 0.227, 0.416)
+#endif
+
+#ifndef APPLE_GREEN
+// G5
+// Lab: 72.45, -23.60,	60.47
+// 0.825, 0.73
+// sRGB: 157, 188, 54
+#define APPLE_GREEN vec3(0.616, 0.737, 0.212)
+#endif
+
+#ifndef SUNFLOWER
+// G6
+// Lab: 71.65,	23.74,	72.28 
+// 0.825, 0.893
+// sRGB: 238, 158, 25
+#define SUNFLOWER vec3(0.933, 0.620, 0.098)
+#endif
+
+#ifndef AQUA
+// H1
+// Lab: 70.19, -31.90,	 1.98
+// 0.929, 0.107
+// sRGB: 98, 187, 166
+#define AQUA vec3(0.384, 0.733, 0.651)
+#endif
+
+#ifndef LAVANDER
+// H2
+// Lab: 54.38,	 8.84, -25.71
+// 0.929, 0.264
+// sRGB: 126, 125, 174
+#define LAVANDER vec3(0.494, 0.490, 0.682)
+#endif
+
+#ifndef EVERGREEN
+// H3
+// Lab: 42.03, -15.80,	22.93
+// 0.929, 0.421
+// sRGB: 82, 106, 60
+#define EVERGREEN vec3(0.322, 0.423, 0.247)
+#endif
+
+#ifndef STEEL_BLUE
+// H4
+// Lab: 48.82,	-5.11, -23.08
+// 0.929, 0.579
+// sRGB: 87, 120, 155
+#define STEEL_BLUE vec3(0.341, 0.467, 0.603)
+#endif
+
+#ifndef CLASSIC_LIGHT_SKIN
+// H5
+// Lab: 65.10,	18.14,	18.68 
+// 0.929, 0.736 
+// sRGB: 197, 145, 125
+#define CLASSIC_LIGHT_SKIN vec3(0.769, 0.557, 0.494)
+#endif
+
+#ifndef CLASSIC_DARK_SKIN
+// H6
+// Lab: 36.13,	14.15,	15.78 
+// 0.929, 0.893 
+// sRGB: 112, 76, 60
+#define CLASSIC_DARK_SKIN vec3(0.439, 0.302, 0.247)
+#endif
