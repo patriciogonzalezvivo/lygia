b912c953f6dc30ffbe0dca4073ae2dcf272d63a9
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-12-18 06:03:19 -0500

    match lookat to glm

--- space/lookAt.hlsl
+++ space/lookAt.hlsl
@@ -17,14 +17,14 @@ float3x3 lookAt(float3 forward, float3 up) {
     return float3x3(xaxis, yaxis, zaxis);
 }
 
-float3x3 lookAt(float3 target, float3 eye, float3 up) {
+float3x3 lookAt(float3 eye, float3 target, float3 up) {
     float3 zaxis = normalize(target - eye);
     float3 xaxis = normalize(cross(zaxis, up));
     float3 yaxis = cross(zaxis, xaxis);
     return float3x3(xaxis, yaxis, zaxis);
 }
 
-float3x3 lookAt(float3 target, float3 eye, float roll) {
+float3x3 lookAt(float3 eye, float3 target, float roll) {
     float3 up = float3(sin(roll), cos(roll), 0.0);
     float3 zaxis = normalize(target - eye);
     float3 xaxis = normalize(cross(zaxis, up));
