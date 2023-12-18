9835b83f5f0f4bb2185ee17e3cc103999f5357f8
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-12-18 06:03:19 -0500

    match lookat to glm

--- space/lookAt.glsl
+++ space/lookAt.glsl
@@ -3,8 +3,8 @@ contributors: Patricio Gonzalez Vivo
 description: create a look up matrix 
 use: 
     - <mat3> lookAt(<vec3> forward, <vec3> up)
-    - <mat3> lookAt(<vec3> target, <vec3> eye, <vec3> up)
-    - <mat3> lookAt(<vec3> target, <vec3> eye, <float> rolle)
+    - <mat3> lookAt(<vec3> eye, <vec3> target, <vec3> up)
+    - <mat3> lookAt(<vec3> eye, <vec3> target, <float> rolle)
 */
 
 #ifndef FNC_LOOKAT
@@ -17,14 +17,14 @@ mat3 lookAt(vec3 forward, vec3 up) {
     return mat3(xaxis, yaxis, zaxis);
 }
 
-mat3 lookAt(vec3 target, vec3 eye, vec3 up) {
+mat3 lookAt(vec3 eye, vec3 target, vec3 up) {
     vec3 zaxis = normalize(target - eye);
     vec3 xaxis = normalize(cross(zaxis, up));
     vec3 yaxis = cross(zaxis, xaxis);
     return mat3(xaxis, yaxis, zaxis);
 }
 
-mat3 lookAt(vec3 target, vec3 eye, float roll) {
+mat3 lookAt(vec3 eye, vec3 target, float roll) {
     vec3 up = vec3(sin(roll), cos(roll), 0.0);
     vec3 zaxis = normalize(target - eye);
     vec3 xaxis = normalize(cross(zaxis, up));
