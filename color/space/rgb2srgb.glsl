c528de7b8395da45a66bbad331f571b0f3278b0e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-12-20 11:06:44 -0500

    fixing const

--- color/space/rgb2srgb.glsl
+++ color/space/rgb2srgb.glsl
@@ -12,9 +12,9 @@ use: <float|vec3\vec4> rgb2srgb(<float|vec3|vec4> srgb)
 
 #ifndef FNC_RGB2SRGB
 #define FNC_RGB2SRGB
-float rgb2srgb(float c) {   return (c <= 0.0031308) ? c * 12.92 : 1.055 * pow(c, 0.4166666666666667) - 0.055; }
-vec3  rgb2srgb(vec3 rgb) {  return saturate(vec3(   rgb2srgb(rgb.r - SRGB_EPSILON), 
+float rgb2srgb(const in float c) {   return (c <= 0.0031308) ? c * 12.92 : 1.055 * pow(c, 0.4166666666666667) - 0.055; }
+vec3  rgb2srgb(const in vec3 rgb) {  return saturate(vec3(   rgb2srgb(rgb.r - SRGB_EPSILON), 
                                                     rgb2srgb(rgb.g - SRGB_EPSILON), 
                                                     rgb2srgb(rgb.b - SRGB_EPSILON))); }
-vec4  rgb2srgb(vec4 rgb) {  return vec4(rgb2srgb(rgb.rgb), rgb.a); }
+vec4  rgb2srgb(const in vec4 rgb) {  return vec4(rgb2srgb(rgb.rgb), rgb.a); }
 #endif
