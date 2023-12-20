fb5768827f8b12b58e6a0ffe7dd637c1b9816db7
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-12-20 12:04:56 -0500

    fixing hsv2rgb

--- color/space/hsv2rgb.glsl
+++ color/space/hsv2rgb.glsl
@@ -3,12 +3,12 @@
 /*
 contributors: Inigo Quiles
 description: |
-    Convert from HSB to linear RGB
+    Convert from HSV to linear RGB
 use: <vec3|vec4> hsv2rgb(<vec3|vec4> hsv)
 */
 
 #ifndef FNC_HSV2RGB
 #define FNC_HSV2RGB
-vec3 hsv2rgb(const in vec3 hsb) { return ((hue2rgb(hsb.x) - 1.0) * hsv.y + 1.0) * hsv.z; }
-vec4 hsv2rgb(const in vec4 hsb) { return vec4(hsv2rgb(hsb.rgb), hsb.a); }
+vec3 hsv2rgb(const in vec3 hsv) { return ((hue2rgb(hsv.x) - 1.0) * hsv.y + 1.0) * hsv.z; }
+vec4 hsv2rgb(const in vec4 hsv) { return vec4(hsv2rgb(hsv.rgb), hsv.a); }
 #endif
