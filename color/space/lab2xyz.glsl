4ee1dcecf74445f8ed100acdb34be66c1ec495f4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-12-21 07:02:13 -0500

    lab and xyY to srgb

--- color/space/lab2xyz.glsl
+++ color/space/lab2xyz.glsl
@@ -1,19 +1,22 @@
 /*
 contributors: Patricio Gonzalez Vivo
-description: Converts a Lab color to XYZ color space.
+description: |
+    Converts a Lab color to XYZ color space.
+    https://en.wikipedia.org/wiki/CIELAB_color_space
 use: rgb2xyz(<vec3|vec4> color)
 */
 
 #ifndef FNC_LAB2XYZ
 #define FNC_LAB2XYZ
 vec3 lab2xyz(const in vec3 c) {
-    vec3 f = vec3(0.0);
-    f.y = (c.x + 16.0) / 116.0;
-    f.x = c.y / 500.0 + f.y;
-    f.z = f.y - c.z / 200.0;
-    vec3 c0 = f * f * f;
-    vec3 c1 = (f - 16.0 / 116.0) / 7.787;
-    return vec3(95.047, 100.000, 108.883) * mix(c0, c1, step(f, vec3(0.206897)));
+    float fy = ( c.x + 16.0 ) / 116.0;
+    float fx = c.y / 500.0 + fy;
+    float fz = fy - c.z / 200.0;
+    return vec3(
+         95.047 * (( fx > 0.206897 ) ? fx * fx * fx : ( fx - 16.0 / 116.0 ) / 7.787),
+        100.000 * (( fy > 0.206897 ) ? fy * fy * fy : ( fy - 16.0 / 116.0 ) / 7.787),
+        108.883 * (( fz > 0.206897 ) ? fz * fz * fz : ( fz - 16.0 / 116.0 ) / 7.787)
+    );
 }
 
 vec4 lab2xyz(in vec4 c) { return vec4(lab2xyz(c.xyz), c.w); }
