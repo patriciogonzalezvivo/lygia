d0bfd511d70ed2fcefdbbf70889c1a4161bf608d
patternspandemic <patternspandemic@live.com>
2024-02-25 09:35:58 -0700

    Fix missing return statemtent for rgb2oklab.

--- color/space/rgb2oklab.hlsl
+++ color/space/rgb2oklab.hlsl
@@ -10,6 +10,6 @@ use: <float3\float4> rgb2oklab(<float3|float4> srgb)
 
 #ifndef FNC_RGB2OKLAB
 #define FNC_RGB2OKLAB
-float3 rgb2oklab(const in float3 rgb) { srgb2oklab( rgb2srgb(rgb) ); }
+float3 rgb2oklab(const in float3 rgb) { return srgb2oklab( rgb2srgb(rgb) ); }
 float4 rgb2oklab(const in float4 rgb) { return float4(rgb2oklab(rgb.rgb), rgb.a); }
 #endif
