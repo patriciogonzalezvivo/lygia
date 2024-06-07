c4a5c27ece7ffd98b85f22126152b9209b454d68
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-06-07 17:00:20 -0400

    adding basic pigments

--- color/palette/pigments.hlsl
+++ color/palette/pigments.hlsl
@@ -1,3 +1,23 @@
+/*
+contributors: Patricio Gonzalez Vivo
+description: | 
+    Basic default color pigments.
+    For specific manufacturer pigments you can use the following:
+    - winsor_oil
+    - rembrandt_oil
+    - gamblin_oil
+    - winsor_acrylics
+    - golden_acrylics
+    - liquitex_acrylics
+    - winsor_gouache
+examples:
+    - https://raw.githubusercontent.com/patriciogonzalezvivo/lygia_examples/main/color_pigments.frag
+*/
+
+#ifndef TITANIUM_WHITE
+#define TITANIUM_WHITE float3(0.973, 0.961, 0.929)
+#endif
+
 #ifndef CADMIUM_YELLOW
 #define CADMIUM_YELLOW float3(0.996, 0.925, 0.000)
 #endif 
@@ -5,6 +25,10 @@
 #ifndef HANSA_YELLOW
 #define HANSA_YELLOW float3(0.988, 0.827, 0.000)
 #endif 
+
+#ifndef YELLOW_OCHRE
+#define YELLOW_OCHRE vec3(0.706, 0.486, 0.188)
+#endif
 
 #ifndef CADMIUM_ORANGE
 #define CADMIUM_ORANGE float3(1.000, 0.412, 0.000)
@@ -49,3 +73,7 @@
 #ifndef BURNT_SIENNA
 #define BURNT_SIENNA float3(0.482, 0.282, 0.000)
 #endif 
+
+#ifndef IVORY_BLACK
+#define IVORY_BLACK float3(0.180, 0.180, 0.180)
+#endif
