3bea5266e2ac3bcb0778300f655f4426d3aec279
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-06-08 10:20:13 -0400

    fixing format

--- color/palette/spectral/gems.glsl
+++ color/palette/spectral/gems.glsl
@@ -3,7 +3,7 @@
 /*
 contributors: ["Jos Stam", "Alias Systems"]
 description: |
-    From Chap 8 "Simulating Diffraction" from GPU Gems https://developer.nvidia.com/gpugems/gpugems/part-i-natural-effects/chapter-8-simulating-diffraction
+    From Chap 8 Simulating Diffraction from GPU Gems https://developer.nvidia.com/gpugems/gpugems/part-i-natural-effects/chapter-8-simulating-diffraction
 use: <vec3> spectral_gems(<float> x)
 examples:
     - https://raw.githubusercontent.com/patriciogonzalezvivo/lygia_examples/main/color_wavelength.frag
