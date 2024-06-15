d763068448c8175281491968ff690d3b13ba364e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-06-15 16:33:19 -0400

    brightnessContrast

--- color/contrastMatrix.hlsl
+++ color/contrastMatrix.hlsl
@@ -2,6 +2,8 @@
 contributors: Patricio Gonzalez Vivo
 description: Generate a matrix to change a the contrast of any color
 use: contrastMatrix(<float> amount)
+examples:
+    - https://raw.githubusercontent.com/patriciogonzalezvivo/lygia_examples/main/color_brightnessContrastMatrix.frag
 license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
