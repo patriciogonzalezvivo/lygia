eb2f0fc13c00b0f8053064caba0cc369077736ed
shadielhajj <shadi.elhajj@gmail.com>
2024-06-27 22:54:41 +0100

    Fixed hlsl implementation of color/palette/hue

--- color/palette/hue.hlsl
+++ color/palette/hue.hlsl
@@ -1,3 +1,5 @@
+#include "../../math/mod.hlsl"
+
 /*
 contributors: Patricio Gonzalez Vivo
 description: 'Physical Hue. Ratio: 1/3 = neon, 1/4 = refracted, 1/5+ = approximate white'
@@ -9,10 +11,10 @@ license:
 
 #ifndef FNC_PALETTE_HUE
 #define FNC_PALETTE_HUE
-float3 hue(float x, float r) { 
-    float3 v = abs( mod(fract(1.0-x) + vec3(0.0,1.0,2.0) * r, 1.0) * 2.0 - 1.0); 
-    return v*v*(3.0-2.0*v);
+
+float3 hue(float x, float r) {
+    float3 v = abs(mod(frac(1.0 - x) + float3(0.0, 1.0, 2.0) * r, 1.0) * 2.0 - 1.0);
+    return v * v * (3.0 - 2.0 * v);
 }
-float3 hue(float _hue) { return hue(_hue, 0.33333); }
 
 #endif
