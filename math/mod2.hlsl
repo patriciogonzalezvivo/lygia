49c4d5fce976a0265028993b205a96d978814cd6
shadielhajj <shadi.elhajj@gmail.com>
2024-07-03 10:26:29 +0100

    Straightforward port of lighting/atmosphere to hlsl.

---
+++ math/mod2.hlsl
@@ -0,0 +1,24 @@
+#include "mod.hlsl"
+
+/*
+contributors: Mercury
+description: |
+    Two dimensional modulus, returns the remainder of a division of two vectors.
+    Found at in Mercury's library https://mercury.sexy/hg_sdf/
+use: <vec2> mod2(<vec2> x, <vec2> size)
+*/
+
+#ifndef FNC_MOD2
+#define FNC_MOD2
+float2 mod2(inout float2 p, float s) {
+    float2 c = floor((p + s * 0.5) / s);
+    p = mod(p + s*0.5,s) - s*0.5;
+    return c;
+}
+
+float2 mod2(inout float2 p, float2 s) {
+    float2 c = floor((p + s * 0.5) / s);
+    p = mod(p + s*0.5,s) - s*0.5;
+    return c;
+}
+#endif
