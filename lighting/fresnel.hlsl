ce4ad4c00423075d32e5163211c7f6d2a27ba179
shadielhajj <shadi.elhajj@gmail.com>
2024-07-04 20:06:36 +0100

    Added roughness-adjueste fresnel function to fresnel.hlsl.

--- lighting/fresnel.hlsl
+++ lighting/fresnel.hlsl
@@ -1,10 +1,12 @@
 #include "common/schlick.hlsl"
+#include "../math/pow5.hlsl"
 
 /*
 contributors: Patricio Gonzalez Vivo
 description: Resolve fresnel coeficient
 use:
     - <float3> fresnel(const <float3> f0, <float> NoV)
+    - <float3> fresnel(const <float3> f0, <float> NoV, float roughness)
 license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
@@ -33,5 +35,12 @@ float fresnel(const in float f0, const in float NoV)
     return schlick(f0, 1.0, NoV);
 }
 
+// Roughness-adjusted fresnel function to attenuate high speculars at glancing angles
+// Very useful when used with filtered environment maps
+// See https://seblagarde.wordpress.com/2011/08/17/hello-world/
+float3 fresnel(float3 f0, float NoV, float roughness)
+{
+    return f0 + (max(1.0 - roughness, f0) - f0) * pow5(1.0-NoV);
+}
 
 #endif
