72e4952b41af346f93e4308cdc991f0204c4a1eb
shadielhajj <shadi.elhajj@gmail.com>
2024-07-04 22:47:47 +0100

    Added support for Material struct in point.hlsl

--- lighting/light/point.hlsl
+++ lighting/light/point.hlsl
@@ -85,4 +85,57 @@ void lightPoint(float3 _diffuseColor, float3 _specularColor, float3 _N, float3 _
     lightPoint(_diffuseColor, _specularColor, _N, _V,  _NoV, _roughness, _f0, 1.0, _diffuse, _specular);
 }
 
+void lightPoint(
+    const in float3 _diffuseColor, const in float3 _specularColor,
+    const in float3 _V,
+    const in float3 _Lp, const in float3 _Ld, const in float3 _Lc, const in float _Li, const in float _Ldist, const in float _Lof,
+    const in float3 _N, const in float _NoV, const in float _NoL, const in float _roughness, const in float _f0,
+    inout float3 _diffuse, inout float3 _specular)
+{
+
+    float dif = diffuse(_Ld, _N, _V, _NoV, _NoL, _roughness); // * INV_PI;
+    float spec = specular(_Ld, _N, _V, _NoV, _NoL, _roughness, _f0);
+
+    float3 lightContribution = _Lc * _Li;
+    if (_Lof > 0.0)
+        lightContribution *= falloff(_Ldist, _Lof);
+
+    _diffuse += max(float3(0.0, 0.0, 0.0), _diffuseColor * lightContribution * dif);
+    _specular += max(float3(0.0, 0.0, 0.0), _specularColor * lightContribution * spec);
+}
+
+#ifdef STR_MATERIAL
+void lightPoint(
+    const in float3 _diffuseColor, const in float3 _specularColor,
+    LightPoint _L, const in Material _mat, 
+    inout float3 _diffuse, inout float3 _specular) 
+    {
+    float dist  = length(_L.position);
+    float3 L      = _L.position/dist;
+
+    float f0    = max(_mat.f0.r, max(_mat.f0.g, _mat.f0.b));
+    float NoL   = dot(_mat.normal, L);
+
+    lightPoint( _diffuseColor, _specularColor, 
+                _mat.V, 
+                _L.position, L, _L.color, _L.intensity, dist, _L.falloff, 
+                _mat.normal, _mat.NoV, NoL, _mat.roughness, f0, 
+                _diffuse, _specular);
+
+    // TODO:
+    // - make sure that the shadow use a perspective projection
+#ifdef SHADING_MODEL_SUBSURFACE
+    float3  h     = normalize(_mat.V + L);
+    float NoH   = saturate(dot(_mat.normal, h));
+    float LoH   = saturate(dot(L, h));
+
+    float scatterVoH = saturate(dot(_mat.V, -L));
+    float forwardScatter = exp2(scatterVoH * _mat.subsurfacePower - _mat.subsurfacePower);
+    float backScatter = saturate(NoL * _mat.thickness + (1.0 - _mat.thickness)) * 0.5;
+    float subsurface = lerp(backScatter, 1.0, forwardScatter) * (1.0 - _mat.thickness);
+    _diffuse += _mat.subsurfaceColor * (subsurface * diffuseLambert());
+#endif
+}
+#endif
+
 #endif
