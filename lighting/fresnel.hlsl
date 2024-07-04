87cb9f51d1e36bad7107b7303d21f048b9068429
shadielhajj <shadi.elhajj@gmail.com>
2024-07-04 20:05:57 +0100

    Clean up fresnel.hlsl and bring it in line with fresnel.glsl

--- lighting/fresnel.hlsl
+++ lighting/fresnel.hlsl
@@ -1,15 +1,10 @@
 #include "common/schlick.hlsl"
 
-#include "fakeCube.hlsl"
-#include "sphericalHarmonics.hlsl"
-#include "../color/tonemap.hlsl"
-
 /*
 contributors: Patricio Gonzalez Vivo
 description: Resolve fresnel coeficient
 use:
-    - <float3> fresnel(const <float3> f0, <float> LoH)
-    - <float3> fresnel(<float3> _R, <float3> _f0, <float> _NoV)
+    - <float3> fresnel(const <float3> f0, <float> NoV)
 license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
@@ -18,22 +13,25 @@ license:
 #ifndef FNC_FRESNEL
 #define FNC_FRESNEL
 
-float3 fresnel(const float3 f0, float LoH) {
+float3 fresnel(const in float3 f0, float3 normal, float3 view)
+{
+    return schlick(f0, 1.0, dot(view, normal));
+}
+
+float3 fresnel(const float3 f0, float NoV)
+{
 #if defined(TARGET_MOBILE) || defined(PLATFORM_RPI)
-    return schlick(f0, 1.0, LoH);
+    return schlick(f0, 1.0, NoV);
 #else
     float f90 = saturate(dot(f0, float3(50.0, 50.0, 50.0) * 0.33));
-    return schlick(f0, f90, LoH);
+    return schlick(f0, f90, NoV);
 #endif
 }
 
-// float fresnelf(float3 V, float3 N, float R0) {
-//     float cosAngle = 1.0-max(dot(V, N), 0.0);
-//     float result = cosAngle * cosAngle;
-//     result = result * result;
-//     result = result * cosAngle;
-//     result = clamp(result * (1.0 - R0) + R0, 0.0, 1.0);
-//     return result;
-// }
+float fresnel(const in float f0, const in float NoV)
+{
+    return schlick(f0, 1.0, NoV);
+}
+
 
 #endif
