965241fde1ead7fc8e1002a3729323644481f51c
shadielhajj <shadi.elhajj@gmail.com>
2024-07-06 21:45:48 +0100

    Cleaned-up obsolete functions in point.hlsl and directional.hlsl.

--- lighting/light/directional.hlsl
+++ lighting/light/directional.hlsl
@@ -17,27 +17,6 @@ license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
 */
 
-#ifndef LIGHT_POSITION
-#if defined(UNITY_COMPILER_HLSL)
-#define LIGHT_POSITION _WorldSpaceLightPos0.xyz
-#else
-#define LIGHT_POSITION  float3(0.0, 10.0, -50.0)
-#endif
-#endif
-
-#ifndef LIGHT_COLOR
-#if defined(UNITY_COMPILER_HLSL)
-#include <UnityLightingCommon.cginc>
-#define LIGHT_COLOR     _LightColor0.rgb
-#else
-#define LIGHT_COLOR     float3(0.5, 0.5, 0.5)
-#endif
-#endif
-
-#ifndef LIGHT_INTENSITY
-#define LIGHT_INTENSITY 1.0
-#endif
-
 #ifndef STR_LIGHT_DIRECTIONAL
 #define STR_LIGHT_DIRECTIONAL
 struct LightDirectional
@@ -50,23 +29,6 @@ struct LightDirectional
 
 #ifndef FNC_LIGHT_DIRECTIONAL
 #define FNC_LIGHT_DIRECTIONAL
-
-void lightDirectional(float3 _diffuseColor, float3 _specularColor, float3 _N, float3 _V, float _NoV, float _roughness, float _f0, float _shadow, inout float3 _diffuse, inout float3 _specular) {
-    #ifdef LIGHT_DIRECTION
-    float3 s = normalize(LIGHT_DIRECTION);
-    #else 
-    float3 s = normalize(LIGHT_POSITION);
-    #endif
-    float NoL = dot(_N, s);
-    float dif = diffuseOrenNayar(s, _N, _V, _NoV, NoL, _roughness);
-    float spec = specularCookTorrance(s, _N, _V, _NoV, NoL, _roughness, _f0);
-    _diffuse += max(0.0, LIGHT_INTENSITY * (_diffuseColor * LIGHT_COLOR * dif) * _shadow);
-    _specular += max(0.0, LIGHT_INTENSITY * (_specularColor * LIGHT_COLOR * spec) * _shadow);
-}
-
-// void lightDirectional(float3 _diffuseColor, float3 _specularColor, float3 _N, float3 _V, float _NoV, float _roughness, float _f0, inout float3 _diffuse, inout float3 _specular) {
-//     return lightDirectional(_diffuseColor, _specularColor, _N, _V, _NoV, _roughness, _f0, 1.0, _diffuse, _specular);
-// }
 
 void lightDirectional(
     const in float3 _diffuseColor, const in float3 _specularColor,
