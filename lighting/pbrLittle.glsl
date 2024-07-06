ba7dad5da0750ed4c495b07d2df5f125aee0e912
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-07-06 16:34:46 -0400

    fixing gooch and removing SPH tweak on pbrLittle

--- lighting/pbrLittle.glsl
+++ lighting/pbrLittle.glsl
@@ -66,9 +66,9 @@ vec4 pbrLittle( vec4 _albedo, vec3 _position, vec3 _normal, float _roughness, fl
     float spec = specular(L, N, V, _roughness) * shadow;
 
     _albedo.rgb = _albedo.rgb * diff;
-#ifdef SCENE_SH_ARRAY
-    _albedo.rgb = _albedo.rgb + tonemapReinhard( sphericalHarmonics(N) ) * 0.25;
-#endif
+// #ifdef SCENE_SH_ARRAY
+    // _albedo.rgb = _albedo.rgb + tonemapReinhard( sphericalHarmonics(N) ) * 0.25;
+// #endif
 
     float NoV = dot(N, V); 
 
