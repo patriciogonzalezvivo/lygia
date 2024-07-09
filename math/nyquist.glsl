81757774c92c05124806f4b21f8261262d5b41ae
shadielhajj <shadi.elhajj@gmail.com>
2024-07-09 19:53:17 +0100

    Added nyquist low-pass filter (hlsl and glsl).

---
+++ math/nyquist.glsl
@@ -0,0 +1,26 @@
+/*
+contributors: Shadi El Hajj
+description: A simple low-pass filter which attenuates high-frequencies.
+use: nyquist(<float> value, <float> fwidth)
+use: nyquist(<float> value, <float> width, <float> strength)
+*/
+
+#ifndef FNC_NYQUIST
+#define FNC_NYQUIST
+
+#ifndef NYQUIST_FILTER_CENTER
+#define NYQUIST_FILTER_CENTER 0.5
+#endif
+
+#ifndef NYQUIST_FILTER_WIDTH
+#define NYQUIST_FILTER_WIDTH 0.25
+#endif
+
+float nyquist(float x, float width){
+    float cutoffStart = NYQUIST_FILTER_CENTER - NYQUIST_FILTER_WIDTH;
+    float cutoffEnd = NYQUIST_FILTER_CENTER + NYQUIST_FILTER_WIDTH;
+    float f = smoothstep(cutoffEnd, cutoffStart, width);
+    return mix(0.5, x, f);
+}
+
+#endif
