dbf4ca1d1a3aac5288891d46eb08fe7c6eb8c909
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-07-11 15:48:06 -0400

    int to float on dist

--- draw/circle.glsl
+++ draw/circle.glsl
@@ -7,7 +7,7 @@
 /*
 contributors: Patricio Gonzalez Vivo
 description: Draw a circle filled or not.
-use: circle(<vec2> st, <float> size [, <float> width])
+use: <float> circle(<vec2> st, <float> size [, <float> width])
 license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
