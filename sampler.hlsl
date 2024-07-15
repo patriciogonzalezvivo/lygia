38b441cc6fce96b58f796f976655cb74167132c2
shadielhajj <shadi.elhajj@gmail.com>
2024-07-15 23:08:03 +0100

    Updated to DirectX 11 syntax

--- sampler.hlsl
+++ sampler.hlsl
@@ -5,10 +5,17 @@ license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
 */
+
+// https://docs.unity3d.com/Manual/SL-SamplerStates.html
+#ifndef DEFAULT_SAMPLER_STATE
+#define DEFAULT_SAMPLER_STATE defaultLinearClampSampler
+SamplerState DEFAULT_SAMPLER_STATE;
+#endif
+
 #ifndef SAMPLER_FNC
-#define SAMPLER_FNC(TEX, UV) tex2D(TEX, UV)
+#define SAMPLER_FNC(TEX, UV) TEX.Sample(DEFAULT_SAMPLER_STATE, UV)
 #endif
 
 #ifndef SAMPLER_TYPE
-#define SAMPLER_TYPE sampler2D
+#define SAMPLER_TYPE Texture2D
 #endif
