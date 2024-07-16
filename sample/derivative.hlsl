d7148aa9506d1a48a61e92d77dc3a849f4726844
shadielhajj <shadi.elhajj@gmail.com>
2024-07-17 00:45:20 +0100

    Fix derivative.hlsl.

--- sample/derivative.hlsl
+++ sample/derivative.hlsl
@@ -19,26 +19,26 @@ license:
 #ifndef FNC_SAMPLEDERIVATIVE
 #define FNC_SAMPLEDERIVATIVE
 float2 sampleDerivative(in SAMPLER_TYPE tex, in float2 st, float2 pixel) { 
-    float p = SAMPLERDERIVATIVE_FNC(st); 
+    float p = SAMPLERDERIVATIVE_FNC(tex, st); 
 
     #if defined(SAMPLEDERRIVATIVE_DD)
     return -float2(ddx(p), ddy(p));
 
     #elif defined(SAMPLEDERRIVATIVE_FAST)
-    float h1 = SAMPLERDERIVATIVE_FNC(st + float2(pixel.x,0.0));
-    float v1 = SAMPLERDERIVATIVE_FNC(st + float2(0.0,pixel.y));
+    float h1 = SAMPLERDERIVATIVE_FNC(tex, st + float2(pixel.x,0.0));
+    float v1 = SAMPLERDERIVATIVE_FNC(tex, st + float2(0.0,pixel.y));
     return (p - float2(h1, v1));
 
     #else
-    float center      = SAMPLERDERIVATIVE_FNC(st);
-    float topLeft     = SAMPLERDERIVATIVE_FNC(st - pixel);
-    float left        = SAMPLERDERIVATIVE_FNC(st - float2(pixel.x, .0));
-    float bottomLeft  = SAMPLERDERIVATIVE_FNC(st + float2(-pixel.x, pixel.y));
-    float top         = SAMPLERDERIVATIVE_FNC(st - float2(.0, pixel.y));
-    float bottom      = SAMPLERDERIVATIVE_FNC(st + float2(.0, pixel.y));
-    float topRight    = SAMPLERDERIVATIVE_FNC(st + float2(pixel.x, -pixel.y));
-    float right       = SAMPLERDERIVATIVE_FNC(st + float2(pixel.x, .0));
-    float bottomRight = SAMPLERDERIVATIVE_FNC(st + pixel);
+    float center      = SAMPLERDERIVATIVE_FNC(tex, st);
+    float topLeft     = SAMPLERDERIVATIVE_FNC(tex, st - pixel);
+    float left        = SAMPLERDERIVATIVE_FNC(tex, st - float2(pixel.x, .0));
+    float bottomLeft  = SAMPLERDERIVATIVE_FNC(tex, st + float2(-pixel.x, pixel.y));
+    float top         = SAMPLERDERIVATIVE_FNC(tex, st - float2(.0, pixel.y));
+    float bottom      = SAMPLERDERIVATIVE_FNC(tex, st + float2(.0, pixel.y));
+    float topRight    = SAMPLERDERIVATIVE_FNC(tex, st + float2(pixel.x, -pixel.y));
+    float right       = SAMPLERDERIVATIVE_FNC(tex, st + float2(pixel.x, .0));
+    float bottomRight = SAMPLERDERIVATIVE_FNC(tex, st + pixel);
     
     float dX = topRight + 2. * right + bottomRight - topLeft - 2. * left - bottomLeft;
     float dY = bottomLeft + 2. * bottom + bottomRight - topLeft - 2. * top - topRight;
