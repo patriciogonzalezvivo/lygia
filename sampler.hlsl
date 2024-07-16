6f0d7753c2692a8880ff772e9381c477b5b9dc1d
shadielhajj <shadi.elhajj@gmail.com>
2024-07-16 10:37:53 +0100

    Added version macros to sampler for backwards compatibility.

--- sampler.hlsl
+++ sampler.hlsl
@@ -6,21 +6,35 @@ license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
 */
 
-// https://docs.unity3d.com/Manual/SL-SamplerStates.html
-#ifndef DEFAULT_SAMPLER_STATE
-#define DEFAULT_SAMPLER_STATE defaultLinearClampSampler
-SamplerState DEFAULT_SAMPLER_STATE
-{
-    Filter = MIN_MAG_MIP_LINEAR;
-    AddressU = Clamp;
-    AddressV = Clamp;
-};
-#endif
+#if defined(__SHADER_TARGET_MAJOR) && __SHADER_TARGET_MAJOR < 4
 
-#ifndef SAMPLER_FNC
-#define SAMPLER_FNC(TEX, UV) TEX.Sample(DEFAULT_SAMPLER_STATE, UV)
-#endif
+    #ifndef SAMPLER_FNC
+    #define SAMPLER_FNC(TEX, UV) tex2D(TEX, UV)
+    #endif
 
-#ifndef SAMPLER_TYPE
-#define SAMPLER_TYPE Texture2D
+    #ifndef SAMPLER_TYPE
+    #define SAMPLER_TYPE sampler2D
+    #endif
+
+#else
+
+    // https://docs.unity3d.com/Manual/SL-SamplerStates.html
+    #ifndef DEFAULT_SAMPLER_STATE
+    #define DEFAULT_SAMPLER_STATE defaultLinearClampSampler
+    SamplerState DEFAULT_SAMPLER_STATE
+    {
+        Filter = MIN_MAG_MIP_LINEAR;
+        AddressU = Clamp;
+        AddressV = Clamp;
+    };
+    #endif
+
+    #ifndef SAMPLER_FNC
+    #define SAMPLER_FNC(TEX, UV) TEX.Sample(DEFAULT_SAMPLER_STATE, UV)
+    #endif
+
+    #ifndef SAMPLER_TYPE
+    #define SAMPLER_TYPE Texture2D
+    #endif
+
 #endif
