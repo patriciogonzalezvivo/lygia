3619e90a9d94f1224cf60d86365c487c6883e1ff
shadielhajj <shadi.elhajj@gmail.com>
2024-07-17 01:42:13 +0100

    Fixed raymarching camera matrix transposition.

--- lighting/raymarch/camera.hlsl
+++ lighting/raymarch/camera.hlsl
@@ -9,10 +9,9 @@ use: <float3x3> raymarchCamera(in <float3> ro, [in <float3> ta [, in <float3> up
 
 float3x3 raymarchCamera( in float3 ro, in float3 ta, in float3 up ) {
     float3 cw = normalize(ta-ro);
-    float3 cp = up;
-    float3 cu = normalize( cross(cw,cp) );
+    float3 cu = normalize( cross(cw,up) );
     float3 cv = normalize( cross(cu,cw) );
-    return float3x3( cu, cv, cw );
+    return transpose(float3x3(cu, cv, cw));
 }
 
 float3x3 raymarchCamera( in float3 ro, in float3 ta, float cr ) {
@@ -20,7 +19,7 @@ float3x3 raymarchCamera( in float3 ro, in float3 ta, float cr ) {
     float3 cp = float3(sin(cr), cos(cr),0.0);
     float3 cu = normalize( cross(cw,cp) );
     float3 cv =          ( cross(cu,cw) );
-    return float3x3( cu, cv, cw );
+    return transpose(float3x3(cu, cv, cw));
 }
 
 float3x3 raymarchCamera( in float3 ro, in float3 ta ) {
