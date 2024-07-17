ac5e8bdbe81f92b1ae26cac882fb39fff3cfeb11
shadielhajj <shadi.elhajj@gmail.com>
2024-07-17 21:31:18 +0100

    Added comment

--- lighting/raymarch/camera.hlsl
+++ lighting/raymarch/camera.hlsl
@@ -11,6 +11,9 @@ float3x3 raymarchCamera( in float3 ro, in float3 ta, in float3 up ) {
     float3 cw = normalize(ta-ro);
     float3 cu = normalize( cross(cw,up) );
     float3 cv = normalize( cross(cu,cw) );
+    // glsl is row-major anh hlsl column major.
+    // float3x3 is initialised with row vectors, but cu, cv and cw are columns vector
+    // thus, we need to transpose after initialization
     return transpose(float3x3(cu, cv, cw));
 }
 
