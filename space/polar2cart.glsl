c7ca2a4f67c74ae894da7ea0b128b24ee6474a5a
shadielhajj <shadi.elhajj@gmail.com>
2024-07-26 10:46:11 +0100

    Fixed cart2polar, added polar2cart

--- space/polar2cart.glsl
+++ space/polar2cart.glsl
@@ -1,10 +1,22 @@
 /*
-contributors: Ivan Dianov
+contributors: [Ivan Dianov, Shadi El Hajj]
 description: polar to cartesian conversion.
 use: polar2cart(<vec2> polar)
 */
 
 #ifndef FNC_POLAR2CART
 #define FNC_POLAR2CART
-vec2 polar2cart(in vec2 polar) { return vec2(cos(polar.x), sin(polar.x)) * polar.y; }
+
+vec2 polar2cart(in vec2 polar) {
+    return vec2(cos(polar.x), sin(polar.x)) * polar.y;
+}
+
+// https://mathworld.wolfram.com/SphericalCoordinates.html
+vec3 polar2cart( in float r, in float phi, in float theta) {
+    float x = r * cos(theta) * sin(phi);
+    float y = r * sin(theta) * sin(phi);
+    float z = r * cos(phi);
+    return vec3(x, y, z);
+}
+
 #endif
