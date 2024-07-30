9e230d9a426ec354c2d903c058d2fe586bc62e63
shadielhajj <shadi.elhajj@gmail.com>
2024-07-30 23:23:10 +0100

    Refactored RAYMARCH_MATERIAL_FNC.

--- lighting/raymarch/render.hlsl
+++ lighting/raymarch/render.hlsl
@@ -12,6 +12,10 @@ use: <vec4> raymarchDefaultRender( in <float3> rayOriging, in <float3> rayDirect
     out <float3> eyeDepth, out <float3> worldPosition, out <float3> worldNormal ) 
 */
 
+#ifndef RAYMARCH_BACKGROUND
+#define RAYMARCH_BACKGROUND float3(0.0, 0.0, 0.0)
+#endif
+
 #ifndef FNC_RAYMARCHDEFAULT
 #define FNC_RAYMARCHDEFAULT
 
@@ -19,14 +23,20 @@ float4 raymarchDefaultRender(
     in float3 rayOrigin, in float3 rayDirection, float3 cameraForward,
     out float eyeDepth, out float3 worldPos, out float3 worldNormal ) { 
 
-    float4 color = float4(0.0, 0.0, 0.0, 0.0);
     
     Material res = raymarchCast(rayOrigin, rayDirection);
     float t = res.sdf;
 
     worldPos = rayOrigin + t * rayDirection;
     worldNormal = raymarchNormal( worldPos );
-    color = RAYMARCH_MATERIAL_FNC(rayDirection, worldPos, worldNormal, res).albedo;
+
+    float4 color = float4(RAYMARCH_BACKGROUND, 0.0);
+    if (res.valid) {
+        res.position = worldPos;
+        res.normal = worldNormal;
+        res.V = rayDirection;
+        color = RAYMARCH_MATERIAL_FNC(res);
+    }
     color.rgb = raymarchFog(color.rgb, t, rayOrigin, rayDirection);
 
     // Eye-space depth. See https://www.shadertoy.com/view/4tByz3
