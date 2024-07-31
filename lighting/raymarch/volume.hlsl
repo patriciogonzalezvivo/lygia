fa92560895e644fca2e7e39f8f04bf34ce2251aa
shadielhajj <shadi.elhajj@gmail.com>
2024-07-31 22:00:28 +0100

    AOV output for volume.hlsl.

--- lighting/raymarch/volume.hlsl
+++ lighting/raymarch/volume.hlsl
@@ -1,11 +1,11 @@
 /*
 contributors:  Inigo Quiles
 description: Default raymarching renderer
-use: <float4> raymarchDefaultRender( in  float3> ro, in  float3> rd ) 
+use: <vec4> raymarchDefaultRender( in <float3> rayOriging, in <float3> rayDirection, in <float3> cameraForward,
+    out <float3> eyeDepth, out <float3> worldPosition, out <float3> worldNormal ) 
 options:
     - RAYMARCH_MATERIAL_FNC(RGB) float3(RGB)
     - RAYMARCH_BACKGROUND float3(0.0)
-    - RAYMARCH_AMBIENT float3(1.0)
     - LIGHT_COLOR     float3(0.5)
     - LIGHT_POSITION  float3(0.0, 10.0, -50.0)
 */
@@ -16,10 +16,6 @@ options:
 #else
 #define LIGHT_COLOR float3(0.5, 0.5, 0.5)
 #endif
-#endif
-
-#ifndef RAYMARCH_AMBIENT
-#define RAYMARCH_AMBIENT float3(1.0, 1.0, 1.0)
 #endif
 
 #ifndef RAYMARCH_BACKGROUND
@@ -49,7 +45,8 @@ options:
 #ifndef FNC_RAYMARCHVOLUMERENDER
 #define FNC_RAYMARCHVOLUMERENDER
 
-float4 raymarchVolume( in float3 ro, in float3 rd ) {
+vec4 raymarchVolume( in vec3 rayOrigin, in vec3 rayDirection, vec3 cameraForward,
+                     out float eyeDepth, out vec3 worldPos, out vec3 worldNormal) {
 
     const float tmin        = RAYMARCH_MIN_DIST;
     const float tmax        = RAYMARCH_MAX_DIST;
@@ -67,7 +64,7 @@ float4 raymarchVolume( in float3 ro, in float3 rd ) {
     float T = 1.;
     float t = tmin;
     float3 col = float3(0.0, 0.0, 0.0);
-    float3 pos = ro;
+    float3 pos = rayOrigin;
     for(int i = 0; i < RAYMARCH_SAMPLES; i++) {
         float4 res    = RAYMARCH_MAP_FNC(pos);
         float density = (0.1 - res.a);
@@ -92,10 +89,14 @@ float4 raymarchVolume( in float3 ro, in float3 rd ) {
             col += LIGHT_COLOR * 80. * tmp * T * Tl;
             #endif
         }
-        pos += rd * tstep;
+        pos += rayDirection * tstep;
     }
 
-    return float4(saturate(col), t);
+    worldPos = rayOrigin + t * rayDirection;
+    worldNormal = raymarchNormal( worldPos );
+    eyeDepth = t * dot(rayDirection, cameraForward);
+
+    return float4(col, t);
 }
 
 #endif
