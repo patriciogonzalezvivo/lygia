3c5118ae1ef5bc2d17e80a7f756cfe5750e106c0
shadielhajj <shadi.elhajj@gmail.com>
2024-08-02 10:19:08 +0100

    Fixed and Refactored lookAt (GLSL only).

--- space/lookAt.hlsl
+++ space/lookAt.hlsl
@@ -1,10 +1,10 @@
 /*
 contributors: Patricio Gonzalez Vivo
-description: create a look up matrix
+description: create a look at matrix
 use:
     - <float3x3> lookAt(<float3> forward, <float3> up)
     - <float3x3> lookAt(<float3> target, <float3> eye, <float3> up)
-    - <float3x3> lookAt(<float3> target, <float3> eye, <float> rolle)
+    - <float3x3> lookAt(<float3> target, <float3> eye, <float> roll)
 license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
@@ -23,7 +23,7 @@ float3x3 lookAt(float3 forward, float3 up) {
 float3x3 lookAt(float3 eye, float3 target, float3 up) {
     float3 zaxis = normalize(target - eye);
     float3 xaxis = normalize(cross(zaxis, up));
-    float3 yaxis = cross(zaxis, xaxis);
+    float3 yaxis = normalize(cross(xaxis, zaxis));
     return float3x3(xaxis, yaxis, zaxis);
 }
 
