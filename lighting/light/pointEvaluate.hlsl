5d56d3bc4dd485cb377030ac42b936fe7f90d973
shadielhajj <shadi.elhajj@gmail.com>
2024-08-31 23:44:21 +0100

    Don't multiply the specular lobe by the specular colo.

--- lighting/light/pointEvaluate.hlsl
+++ lighting/light/pointEvaluate.hlsl
@@ -44,7 +44,7 @@ void lightPointEvaluate(LightPoint L, Material mat, inout ShadingData shadingDat
         lightContribution *= falloff(Ldist, L.falloff);
 
     shadingData.diffuse  += max(float3(0.0, 0.0, 0.0), shadingData.diffuseColor * lightContribution * dif);
-    shadingData.specular += max(float3(0.0, 0.0, 0.0), shadingData.specularColor * lightContribution * spec);
+    shadingData.specular += max(float3(0.0, 0.0, 0.0), lightContribution * spec);
 
     // TODO:
     // - make sure that the shadow use a perspective projection
