48839777a0c28e83ad96da9c273a699a8b7672c8
shadielhajj <shadi.elhajj@gmail.com>
2024-09-01 00:00:29 +0100

    Implemented importance sampling (HLSL)

--- lighting/diffuse/importanceSampling.hlsl
+++ lighting/diffuse/importanceSampling.hlsl
@@ -1,8 +1,5 @@
 #include "../../math/hammersley.hlsl"
 #include "../../space/tbn.hlsl"
-#include "../common/ggx.hlsl"
-#include "../common/smithGGXCorrelated.hlsl"
-#include "../fresnel.hlsl"
 #include "../common/preFilteredImportanceSampling.hlsl"
 
 /*
@@ -16,11 +13,11 @@ license: MIT License (MIT) Copyright (c) 2024 Shadi EL Hajj
 #define IBL_IMPORTANCE_SAMPLING_SAMPLES  64
 #endif
 
-#ifndef FNC_SPECULAR_IMPORTANCE_SAMPLING
-#define FNC_SPECULAR_IMPORTANCE_SAMPLING
+#ifndef FNC_DIFFUSE_IMPORTANCE_SAMPLING
+#define FNC_DIFFUSE_IMPORTANCE_SAMPLING
 
 
-float3 specularImportanceSampling(float roughness, float3 f0, const float3 n, const float3 v, const float3 r, const float NoV) {
+float3 diffuseImportanceSampling(float roughness, const float3 n, const float3 v, const float3 r) {
     const int numSamples = IBL_IMPORTANCE_SAMPLING_SAMPLES;
     const float invNumSamples = 1.0 / float(IBL_IMPORTANCE_SAMPLING_SAMPLES);
     const float3 up = float3(0.0, 0.0, 1.0);
@@ -30,32 +27,22 @@ float3 specularImportanceSampling(float roughness, float3 f0, const float3 n, co
     SCENE_CUBEMAP.GetDimensions(0, width, height, levels);
     float omegaP = (4.0 * PI) / (6.0 * width * width);
 
-    float3 indirectSpecular = float3(0.0, 0.0, 0.0);
+    float3 indirectDiffuse = float3(0.0, 0.0, 0.0);
     for (int i = 0; i < numSamples; i++) {
         float2 u = hammersley(i, numSamples);
-        float3 h = mul(T, importanceSamplingGGX(u, roughness));
-        float3 l = lerp(r, n, roughness * roughness);
+        float3 h = mul(T, hemisphereCosSample(u));
 
+        float3 l = n;
         float NoL = saturate(dot(n, l));
         if (NoL > 0.0) {
-            float NoH = dot(n, h);
-            float LoH = saturate(dot(l, h));
-
-            float D = GGX(n, h, NoH, roughness);
-            float V = smithGGXCorrelated_Fast(roughness, NoV, NoL);
-            float3 F = fresnel(f0, LoH);
-
-            float ipdf = (4.0 * LoH) / (D * NoH);
-            float mipLevel = prefilteredImportanceSampling(ipdf, omegaP, numSamples);
+            float ipdf = PI / NoL;
+            float mipLevel = prefilteredImportanceSampling(ipdf, omegaP, numSamples) + 1.0;
             float3 L = SCENE_CUBEMAP.SampleLevel(SAMPLER_TRILINEAR_CLAMP, l, mipLevel).rgb;
-
-            float3 Fr = F * (D * V * NoL * ipdf * invNumSamples);
-
-            indirectSpecular += (Fr * L);
+            indirectDiffuse += L;
         }
     }
 
-    return indirectSpecular;
+    return indirectDiffuse * invNumSamples;
 }
 
 #endif
