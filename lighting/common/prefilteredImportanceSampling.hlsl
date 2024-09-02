efd9e1c0c5668eeaf35b6ac864bbbb7fa83227f2
shadielhajj <shadi.elhajj@gmail.com>
2024-09-02 09:03:37 +0100

    Fixed const initialisation.

--- lighting/common/prefilteredImportanceSampling.hlsl
+++ lighting/common/prefilteredImportanceSampling.hlsl
@@ -2,9 +2,8 @@
 #define FNC_PRE_FILTERED_IMPORTANCE_SAMPLING
 
 float prefilteredImportanceSampling(float ipdf, float omegaP, int numSamples) {
-    const float invNumSamples = 1.0 / float(numSamples);
     const float K = 4.0;
-    float omegaS = invNumSamples * ipdf;
+    float omegaS = ipdf / float(numSamples);
     float mipLevel = log2(K * omegaS / omegaP) * 0.5; // log4
     return mipLevel;
 }
