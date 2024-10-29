1f43af70abfa44093c29a45590bbb9443aa74284
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-10-29 07:39:41 -0400

    solving ThreeJS luminance issue

--- color/luminance.glsl
+++ color/luminance.glsl
@@ -1,6 +1,8 @@
 /*
 contributor: nan
-description: Computes the luminance of the specified linear RGB color using the luminance coefficients from Rec. 709.
+description: |
+    Computes the luminance of the specified linear RGB color using the luminance coefficients from Rec. 709.
+    Note, ThreeJS seems to inject this in all their shaders. Which could lead to issues
 use: luminance(<vec3|vec4> color)
 license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
@@ -9,6 +11,6 @@ license:
 
 #ifndef FNC_LUMINANCE 
 #define FNC_LUMINANCE
-float luminance(vec3 v) { return dot(v, vec3(0.21250175, 0.71537574, 0.07212251)); }
-float luminance(vec4 v) { return luminance( v.rgb ); }
+float luminance(in vec3 linear) { return dot(linear, vec3(0.21250175, 0.71537574, 0.07212251)); }
+float luminance(in vec4 linear) { return luminance( linear.rgb ); }
 #endif
