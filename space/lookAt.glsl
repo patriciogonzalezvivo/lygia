5708da485d6a403a73c4504317df17a646499af5
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-11-06 22:49:37 -0500

    normalizing and undoing lookAt matrix

--- space/lookAt.glsl
+++ space/lookAt.glsl
@@ -5,8 +5,10 @@ use:
     - <mat3> lookAt(<vec3> forward, <vec3> up)
     - <mat3> lookAt(<vec3> eye, <vec3> target, <vec3> up)
     - <mat3> lookAt(<vec3> eye, <vec3> target, <float> roll)
+    - <mat3> lookAt(<vec3> forward)
 options:
     - LOOK_AT_LEFT_HANDED: assume a left-handed coordinate system
+    - LOOK_AT_RIGHT_HANDED: assume a right-handed coordinate system
 license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
@@ -16,13 +18,13 @@ license:
 #define FNC_LOOKAT
 
 mat3 lookAt(vec3 forward, vec3 up) {
-    vec3 zaxis = forward;
-#if defined(LOOK_AT_LEFT_HANDED)
-    vec3 xaxis = normalize(cross(up, zaxis));
-    vec3 yaxis = normalize(cross(zaxis, xaxis));
-#else
+    vec3 zaxis = normalize(forward);
+#if defined(LOOK_AT_RIGHT_HANDED)
     vec3 xaxis = normalize(cross(zaxis, up));
-    vec3 yaxis = normalize(cross(xaxis, zaxis));
+    vec3 yaxis = cross(xaxis, zaxis);
+#else
+    vec3 xaxis = normalize(cross(up, zaxis));
+    vec3 yaxis = cross(zaxis, xaxis);
 #endif
     return mat3(xaxis, yaxis, zaxis);
 }
