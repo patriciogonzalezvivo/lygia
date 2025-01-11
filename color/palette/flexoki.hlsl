970440d467f9cfc25eb083d02943eb0b4f78559c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2025-01-11 11:11:13 -0500

    addking flexoki palette

---
+++ color/palette/flexoki.hlsl
@@ -0,0 +1,500 @@
+/*
+contributors: Steph Ango
+description: |
+    Flexoki is an inky color scheme for prose and code. Flexoki is designed for reading and writing on digital screens. It is inspired by analog printing inks and warm shades of paper.
+    Learn more at stephango.com/flexoki
+license:
+    - MIT License, Copyright (c) 2023 Steph Ango
+*/
+
+#ifndef FLEXOKI_PAPER
+#define FLEXOKI_PAPER float3(1.0, 0.988, 0.941)
+#endif
+
+#ifndef FLEXOKI_BASE_50
+#define FLEXOKI_BASE_50 float3(0.949, 0.941, 0.898)
+#endif
+
+#ifndef FLEXOKI_BASE_100
+#define FLEXOKI_BASE_100 float3(0.902, 0.894, 0.851)
+#endif
+
+#ifndef FLEXOKI_BASE_150
+#define FLEXOKI_BASE_150 float3(0.855, 0.847, 0.804)
+#endif
+
+#ifndef FLEXOKI_BASE_200
+#define FLEXOKI_BASE_200 float3(0.808, 0.804, 0.765)
+#endif
+
+#ifndef FLEXOKI_BASE_300
+#define FLEXOKI_BASE_300 float3(0.718, 0.706, 0.675)
+#endif
+
+#ifndef FLEXOKI_BASE_400
+#define FLEXOKI_BASE_400 float3(0.624, 0.616, 0.588)
+#endif
+
+#ifndef FLEXOKI_BASE_500
+#define FLEXOKI_BASE_500 float3(0.525, 0.522, 0.502)
+#endif
+
+#ifndef FLEXOKI_BASE_600
+#define FLEXOKI_BASE_600 float3(0.435, 0.431, 0.412)
+#endif
+
+#ifndef FLEXOKI_BASE_700
+#define FLEXOKI_BASE_700 float3(0.345, 0.337, 0.325)
+#endif
+
+#ifndef FLEXOKI_BASE_800
+#define FLEXOKI_BASE_800 float3(0.251, 0.243, 0.235)
+#endif
+
+#ifndef FLEXOKI_BASE_850
+#define FLEXOKI_BASE_850 float3(0.204, 0.192, 0.192)
+#endif
+
+#ifndef FLEXOKI_BASE_900
+#define FLEXOKI_BASE_900 float3(0.157, 0.153, 0.149)
+#endif
+
+#ifndef FLEXOKI_BASE_950
+#define FLEXOKI_BASE_950 float3(0.110, 0.105, 0.102)
+#endif
+
+#ifndef FLEXOKI_BLACK
+#define FLEXOKI_BLACK float3(0.063, 0.059, 0.059)
+#endif
+
+// Red
+
+#ifndef FLEXOKI_RED_50
+#define FLEXOKI_RED_50 float3(1.0, 0.886, 0.835)
+#endif
+
+#ifndef FLEXOKI_RED_100
+#define FLEXOKI_RED_100 float3(1.0, 0.792, 0.733)
+#endif
+
+#ifndef FLEXOKI_RED_150
+#define FLEXOKI_RED_150 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_RED_200
+#define FLEXOKI_RED_200 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_RED_300
+#define FLEXOKI_RED_300 float3(0.918, 0.439, 0.384)
+#endif
+
+#ifndef FLEXOKI_RED_400
+#define FLEXOKI_RED_400 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_RED_500
+#define FLEXOKI_RED_500 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_RED_600
+#define FLEXOKI_RED_600 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_RED_700
+#define FLEXOKI_RED_700 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_RED_800
+#define FLEXOKI_RED_800 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_RED_850
+#define FLEXOKI_RED_850 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_RED_900
+#define FLEXOKI_RED_900 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_RED_950
+#define FLEXOKI_RED_950 float3(0.824, 0.302, 0.255)
+#endif
+
+// Orange
+
+#ifndef FLEXOKI_ORANGE_50
+#define FLEXOKI_ORANGE_50 float3(1.0, 0.898, 0.812)
+#endif
+
+#ifndef FLEXOKI_ORANGE_100
+#define FLEXOKI_ORANGE_100 float3(0.996, 0.827, 0.686)
+#endif
+
+#ifndef FLEXOKI_ORANGE_150
+#define FLEXOKI_ORANGE_150 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_ORANGE_200
+#define FLEXOKI_ORANGE_200 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_ORANGE_300
+#define FLEXOKI_ORANGE_300 float3(0.918, 0.439, 0.384)
+#endif
+
+#ifndef FLEXOKI_ORANGE_400
+#define FLEXOKI_ORANGE_400 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_ORANGE_500
+#define FLEXOKI_ORANGE_500 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_ORANGE_600
+#define FLEXOKI_ORANGE_600 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_ORANGE_700
+#define FLEXOKI_ORANGE_700 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_ORANGE_800
+#define FLEXOKI_ORANGE_800 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_ORANGE_850
+#define FLEXOKI_ORANGE_850 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_ORANGE_900
+#define FLEXOKI_ORANGE_900 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_ORANGE_950
+#define FLEXOKI_ORANGE_950 float3(0.824, 0.302, 0.255)
+#endif
+
+// Yellow
+
+#ifndef FLEXOKI_YELLOW_50
+#define FLEXOKI_YELLOW_50 float3(1.0, 0.898, 0.812)
+#endif
+
+#ifndef FLEXOKI_YELLOW_100
+#define FLEXOKI_YELLOW_100 float3(0.996, 0.827, 0.686)
+#endif
+
+#ifndef FLEXOKI_YELLOW_150
+#define FLEXOKI_YELLOW_150 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_YELLOW_200
+#define FLEXOKI_YELLOW_200 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_YELLOW_300
+#define FLEXOKI_YELLOW_300 float3(0.918, 0.439, 0.384)
+#endif
+
+#ifndef FLEXOKI_YELLOW_400
+#define FLEXOKI_YELLOW_400 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_YELLOW_500
+#define FLEXOKI_YELLOW_500 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_YELLOW_600
+#define FLEXOKI_YELLOW_600 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_YELLOW_700
+#define FLEXOKI_YELLOW_700 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_YELLOW_800
+#define FLEXOKI_YELLOW_800 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_YELLOW_850
+#define FLEXOKI_YELLOW_850 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_YELLOW_900
+#define FLEXOKI_YELLOW_900 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_YELLOW_950
+#define FLEXOKI_YELLOW_950 float3(0.824, 0.302, 0.255)
+#endif
+
+// Green 
+
+#ifndef FLEXOKI_GREEN_50
+#define FLEXOKI_GREEN_50 float3(0.949, 0.941, 0.898)
+#endif
+
+#ifndef FLEXOKI_GREEN_100
+#define FLEXOKI_GREEN_100 float3(0.949, 0.941, 0.898)
+#endif
+
+#ifndef FLEXOKI_GREEN_150
+#define FLEXOKI_GREEN_150 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_GREEN_200
+#define FLEXOKI_GREEN_200 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_GREEN_300
+#define FLEXOKI_GREEN_300 float3(0.918, 0.439, 0.384)
+#endif
+
+#ifndef FLEXOKI_GREEN_400
+#define FLEXOKI_GREEN_400 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_GREEN_500
+#define FLEXOKI_GREEN_500 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_GREEN_600
+#define FLEXOKI_GREEN_600 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_GREEN_700
+#define FLEXOKI_GREEN_700 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_GREEN_800
+#define FLEXOKI_GREEN_800 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_GREEN_850
+#define FLEXOKI_GREEN_850 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_GREEN_900
+#define FLEXOKI_GREEN_900 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_GREEN_950
+#define FLEXOKI_GREEN_950 float3(0.824, 0.302, 0.255)
+#endif
+
+// Cyan
+
+#ifndef FLEXOKI_CYAN_50
+#define FLEXOKI_CYAN_50 float3(0.886, 0.918, 0.918)
+#endif
+
+#ifndef FLEXOKI_CYAN_100
+#define FLEXOKI_CYAN_100 float3(0.996, 0.827, 0.686)
+#endif
+
+#ifndef FLEXOKI_CYAN_150
+#define FLEXOKI_CYAN_150 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_CYAN_200
+#define FLEXOKI_CYAN_200 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_CYAN_300
+#define FLEXOKI_CYAN_300 float3(0.918, 0.439, 0.384)
+#endif
+
+#ifndef FLEXOKI_CYAN_400
+#define FLEXOKI_CYAN_400 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_CYAN_500
+#define FLEXOKI_CYAN_500 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_CYAN_600
+#define FLEXOKI_CYAN_600 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_CYAN_700
+#define FLEXOKI_CYAN_700 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_CYAN_800
+#define FLEXOKI_CYAN_800 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_CYAN_850
+#define FLEXOKI_CYAN_850 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_CYAN_900
+#define FLEXOKI_CYAN_900 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_CYAN_950
+#define FLEXOKI_CYAN_950 float3(0.824, 0.302, 0.255)
+#endif
+
+
+// Blue
+
+#ifndef FLEXOKI_BLUE_50
+#define FLEXOKI_BLUE_50 float3(0.886, 0.918, 0.918)
+#endif
+
+#ifndef FLEXOKI_BLUE_100
+#define FLEXOKI_BLUE_100 float3(0.996, 0.827, 0.686)
+#endif
+
+#ifndef FLEXOKI_BLUE_150
+#define FLEXOKI_BLUE_150 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_BLUE_200
+#define FLEXOKI_BLUE_200 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_BLUE_300
+#define FLEXOKI_BLUE_300 float3(0.918, 0.439, 0.384)
+#endif
+
+#ifndef FLEXOKI_BLUE_400
+#define FLEXOKI_BLUE_400 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_BLUE_500
+#define FLEXOKI_BLUE_500 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_BLUE_600
+#define FLEXOKI_BLUE_600 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_BLUE_700
+#define FLEXOKI_BLUE_700 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_BLUE_800
+#define FLEXOKI_BLUE_800 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_BLUE_850
+#define FLEXOKI_BLUE_850 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_BLUE_900
+#define FLEXOKI_BLUE_900 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_BLUE_950
+#define FLEXOKI_BLUE_950 float3(0.824, 0.302, 0.255)
+#endif
+
+// Purple
+#ifndef FLEXOKI_PURPLE_50
+#define FLEXOKI_PURPLE_50 float3(0.886, 0.918, 0.918)
+#endif
+
+#ifndef FLEXOKI_PURPLE_100
+#define FLEXOKI_PURPLE_100 float3(0.996, 0.827, 0.686)
+#endif
+
+#ifndef FLEXOKI_PURPLE_150
+#define FLEXOKI_PURPLE_150 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_PURPLE_200
+#define FLEXOKI_PURPLE_200 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_PURPLE_300
+#define FLEXOKI_PURPLE_300 float3(0.918, 0.439, 0.384)
+#endif
+
+#ifndef FLEXOKI_PURPLE_400
+#define FLEXOKI_PURPLE_400 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_PURPLE_500
+#define FLEXOKI_PURPLE_500 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_PURPLE_600
+#define FLEXOKI_PURPLE_600 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_PURPLE_700
+#define FLEXOKI_PURPLE_700 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_PURPLE_800
+#define FLEXOKI_PURPLE_800 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_PURPLE_850
+#define FLEXOKI_PURPLE_850 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_PURPLE_900
+#define FLEXOKI_PURPLE_900 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_PURPLE_950
+#define FLEXOKI_PURPLE_950 float3(0.824, 0.302, 0.255)
+#endif
+
+// Magenta
+
+#ifndef FLEXOKI_MAGENTA_50
+#define FLEXOKI_MAGENTA_50 float3(0.996, 0.827, 0.686)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_100
+#define FLEXOKI_MAGENTA_100 float3(0.996, 0.827, 0.686)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_150
+#define FLEXOKI_MAGENTA_150 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_200
+#define FLEXOKI_MAGENTA_200 float3(0.949, 0.702, 0.635)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_300
+#define FLEXOKI_MAGENTA_300 float3(0.918, 0.439, 0.384)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_400
+#define FLEXOKI_MAGENTA_400 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_500
+#define FLEXOKI_MAGENTA_500 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_600
+#define FLEXOKI_MAGENTA_600 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_700
+#define FLEXOKI_MAGENTA_700 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_800
+#define FLEXOKI_MAGENTA_800 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_850
+#define FLEXOKI_MAGENTA_850 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_900
+#define FLEXOKI_MAGENTA_900 float3(0.824, 0.302, 0.255)
+#endif
+
+#ifndef FLEXOKI_MAGENTA_950
+#define FLEXOKI_MAGENTA_950 float3(0.824, 0.302, 0.255)
+#endif
