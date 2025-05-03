ceb7787626b0c170e78c928e6dc2b1470a32c40e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2025-05-03 14:53:11 -0400

    use __VERSION__

--- math/modi.glsl
+++ math/modi.glsl
@@ -8,7 +8,7 @@ use: <int> modi(<int> x, <int> y)
 #ifndef FNC_MODI
 #define FNC_MODI
 int modi(int x, int y) {
-#if version >= 130
+#if __VERSION__ >= 130
     return x % y;
 #else
     return x - y * int(floor(float(x) / float(y)));
