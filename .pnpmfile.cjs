11cddc2c925bc45c6aed255901b67054ab63b67f
lee mighdoll <lee@underneath.ca>
2026-03-22 17:20:23 -0700

    for local wesl-js devs, update path

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,7 +2,7 @@ const path = require("node:path");
 
 function readPackage(pkg) {
   if (process.env.LOCAL_DEPS) {
-    const toolsPkgs = path.resolve(__dirname, "../wesl-js/tools/packages");
+    const toolsPkgs = path.resolve(__dirname, "../wesl-js/packages");
     pkg.dependencies = {
       ...pkg.dependencies,
       "wesl-link": `link:${toolsPkgs}/wesl-link`,
